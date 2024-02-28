6088f887b2304d5b3d51696c6370a8a9fbae1c82
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-02-28 19:08:05 +0900

    #21: 디자인 폰트 가이드라인에 따른 Pretendard 폰트 유스케이스 추가
    - 피그마 디자인 폰트 가이드라인에 따라 Pretendard 폰트 유스케이스를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIResource/UIFont+ToDoGarden.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIResource/UIFont+ToDoGarden.swift
@@ -6,3 +6,83 @@
 //
 
 import UIKit.UIFont
+
+extension UIFont {
+	
+	// MARK: - Head font style
+	
+	public static let pretendardHeadBold: UIFont = UIFont(
+		name: PretendardFont.bold.name,
+		size: 18
+	) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
+	
+	public static let pretendardHeadSemiBold: UIFont = UIFont(
+		name: PretendardFont.semibold.name,
+		size: 16
+	) ?? UIFont.systemFont(ofSize: 16, weight: .semibold)
+	
+	public static let pretendardHeadLight75: UIFont = UIFont(
+		name: PretendardFont.light.name,
+		size: 75
+	) ?? UIFont.systemFont(ofSize: 75, weight: .light)
+	
+	public static let pretendardHeadBold55: UIFont = UIFont(
+		name: PretendardFont.bold.name,
+		size: 55
+	) ?? UIFont.systemFont(ofSize: 55, weight: .bold)
+	
+	// MARK: - Body font style
+	
+	public static let pretendardBodyBold: UIFont = UIFont(
+		name: PretendardFont.bold.name,
+		size: 12
+	) ?? UIFont.systemFont(ofSize: 12, weight: .bold)
+	
+	public static let pretendardBodySemiBold: UIFont = UIFont(
+		name: PretendardFont.semibold.name,
+		size: 13
+	) ?? UIFont.systemFont(ofSize: 13, weight: .semibold)
+	
+	public static let pretendardBodySemiBold15: UIFont = UIFont(
+		name: PretendardFont.semibold.name,
+		size: 15
+	) ?? UIFont.systemFont(ofSize: 15, weight: .semibold)
+	
+	public static let pretendardBodyMedium: UIFont = UIFont(
+		name: PretendardFont.medium.name,
+		size: 13
+	) ?? UIFont.systemFont(ofSize: 13, weight: .medium)
+	
+	public static let pretendardBodyRegular: UIFont = UIFont(
+		name: PretendardFont.regular.name,
+		size: 16
+	) ?? UIFont.systemFont(ofSize: 16, weight: .regular)
+	
+	// MARK: - Detail font style
+	
+	public static let pretendardDetailRegular: UIFont = UIFont(
+		name: PretendardFont.regular.name,
+		size: 15
+	) ?? UIFont.systemFont(ofSize: 15, weight: .regular)
+	
+	public static let pretendardDetailLight: UIFont = UIFont(
+		name: PretendardFont.light.name,
+		size: 12
+	) ?? UIFont.systemFont(ofSize: 12, weight: .light)
+}
+
+public enum PretendardFont: String, CaseIterable {
+	case black = "Pretendard-Black"
+	case bold = "Pretendard-Bold"
+	case extraBold = "Pretendard-ExtraBold"
+	case extraLight = "Pretendard-ExtraLight"
+	case light = "Pretendard-Light"
+	case medium = "Pretendard-Medium"
+	case regular = "Pretendard-Regular"
+	case semibold = "Pretendard-SemiBold"
+	case thin = "Pretendard-Thin"
+	
+	var name: String {
+		return self.rawValue
+	}
+}
