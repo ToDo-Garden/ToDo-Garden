e54968d4dcdcff0e3d00a74957231542dc214b6d
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-02-28 20:06:08 +0900

    #23: TimerControlButton destructive style 추가

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+TimerControlButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+TimerControlButton.swift
@@ -7,3 +7,34 @@
 
 import UIKit
 
+import ToDoGardenUIResource
+
+extension UIButton {
+	public func timerControlButtonDestructiveStyle(with title: String) {
+		self.setupDestructiveStyleBackgroundImage()
+		self.setupTitleForTimerControlButton(with: title)
+	}
+}
+
+// MARK: - private functions
+
+extension UIButton {
+	private func setupDestructiveStyleBackgroundImage() {
+		self.setBackgroundImage(
+			UIImage.timerControlButtonDestructiveBackground,
+			for: .normal
+		)
+	}
+	
+	private func setupTitleForTimerControlButton(with title: String) {
+		let attributedTitle = NSAttributedString(
+			string: title,
+			attributes: [
+				NSAttributedString.Key.font: UIFont.pretendardBodySemiBold15,
+				NSAttributedString.Key.foregroundColor: UIColor.white
+			]
+		)
+		
+		self.setAttributedTitle(attributedTitle, for: .normal)
+	}
+}
