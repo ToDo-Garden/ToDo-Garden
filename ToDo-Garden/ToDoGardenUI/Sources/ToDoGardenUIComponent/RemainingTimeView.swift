8ca5ca967be0474454890054794c054fd7a049cf
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-03-01 19:26:20 +0900

    #30: RemainingTimeLabel 추가
    - 남은시간을 나타내는 label을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RemainingTimeView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RemainingTimeView.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 public final class RemainingTimeView: UIView {
+	private let remainingTimeLabel: UILabel
+	
 	public init() {
+		self.remainingTimeLabel = UILabel()
 		super.init(frame: CGRect.zero)
 	}
 	
 	public required init?(coder: NSCoder) {
+		self.remainingTimeLabel = UILabel()
 		super.init(coder: coder)
 	}
 }
