77266aabeb792218c99d00c2705957232dcdfb83
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-03-01 15:53:44 +0900

    #31: 오토레이아웃 설정 관련 extension 추가
    - 오토레이아웃을 사용하는 view에서 AutoresizingMask를 사용한 Constraints 변환을 하지 않는 함수를 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Extension
+//
+//
+//  Created by Noah on 2/29/24.
+//
+
+import UIKit.UIView
+
+extension UIView {
+	func usingAutolayout() {
+		self.translatesAutoresizingMaskIntoConstraints = false
+	}
+}
