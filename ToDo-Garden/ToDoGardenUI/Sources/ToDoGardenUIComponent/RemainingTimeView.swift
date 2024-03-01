36c897a89d6010f1c34de0e56c444b77b40079fe
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-03-01 19:23:15 +0900

    #30: RemainingTimeView 타입 추가
    - 남은 시간을 나타내는 뷰를 표현하는 타입을 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RemainingTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  RemainingTimeView.swift
+//
+//
+//  Created by Noah on 3/1/24.
+//
+
+import UIKit
+
+public final class RemainingTimeView: UIView {
+	public init() {
+		super.init(frame: CGRect.zero)
+	}
+	
+	public required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+}
