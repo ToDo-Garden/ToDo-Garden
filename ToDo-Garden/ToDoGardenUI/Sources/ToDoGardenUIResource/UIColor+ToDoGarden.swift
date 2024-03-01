022d21f1904408e43459e8472789449c97bab579
SONG <geun2121@gmail.com>
2024-03-01 16:34:43 +0900

    #28: UIColor Extension 추가
    - 컬러 리소스를 손쉽게 사용할 수 있게 static변수로 선언했습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIResource/UIColor+ToDoGarden.swift
@@ -0,0 +1,52 @@
+//
+//  UIColor+ToDoGarden.swift
+//
+//
+//  Created by SONG on 3/1/24.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+  public static let toDoGardenRed = UIColor(resource: .toDoGardenRed)
+  
+  public static let toDoGardenOrange = UIColor(resource: .toDoGardenOrange)
+  
+  public static let toDoGardenYellow = UIColor(resource: .toDoGardenYellow)
+  
+  public static let toDoGardenLeaf = UIColor(resource: .toDoGardenLeaf)
+  
+  public static let toDoGardenOlive = UIColor(resource: .toDoGardenOlive)
+  
+  public static let toDoGardenMint = UIColor(resource: .toDoGardenMint)
+  
+  public static let toDoGardenBlue = UIColor(resource: .toDoGardenBlue)
+  
+  public static let toDoGardenPink = UIColor(resource: .toDoGardenPink)
+  
+  public static let toDoGardenPurple = UIColor(resource: .toDoGardenPurple)
+  
+  public static let toDoGardenBrown = UIColor(resource: .toDoGardenBrown)
+  
+  public static let toDoGardenBlack = UIColor(resource: .toDoGardenBlack)
+  
+  public static let toDoGardenGray = UIColor(resource: .toDoGardenGray)
+  
+  public static let toDoGardenGreenDark = UIColor(resource: .toDoGardenGreenDark)
+  
+  public static let toDoGardenGreenGray = UIColor(resource: .toDoGardenGreenGray)
+  
+  public static let toDoGardenGreenBackground = UIColor(resource: .toDoGardenGreenBackground)
+  
+  public static let toDoGardenGray1 = UIColor(resource: .toDoGardenGray1)
+  
+  public static let toDoGardenGray2 = UIColor(resource: .toDoGardenGray2)
+  
+  public static let toDoGardenGray3 = UIColor(resource: .toDoGardenGray3)
+  
+  public static let toDoGardenEditButtonRed = UIColor(resource: .toDoGardenEditButtonRed)
+  
+  public static let toDoGardenEditButtonBlue = UIColor(resource: .toDoGardenEditButtonBlue)
+  
+  public static let toDoGardenEditButtonYellow = UIColor(resource: .toDoGardenEditButtonYellow)
+}
