36f3d7b366306adeb28d6977442fefd54d7abd37
dpfdallfm <dpfdlalfm@naver.com>
2024-03-04 19:58:52 +0900

    #38: AddButton 스타일 추가 함수 구현
    - AddButton 스타일을 적용하는 함수를 구현하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+AddButton.swift
@@ -0,0 +1,14 @@
+//
+//  UIButton+addButton.swift
+//
+//
+//  Created by Wood on 3/4/24.
+//
+
+import ToDoGardenUIResource
+import UIKit.UIButton
+
+extension UIButton {
+  public func addButtonStyle(with title: String) {
+  }
+}
