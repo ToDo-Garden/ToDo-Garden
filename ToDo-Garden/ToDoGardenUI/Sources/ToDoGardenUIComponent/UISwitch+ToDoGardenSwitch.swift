c3267c630773f06009a772d464575bd3d2517cf3
SONG <geun2121@gmail.com>
2024-03-05 14:21:26 +0900

    #19: UISwitch extension 추가
    - on/off 상태 색상을 변경하기 위한 extension의 스켈레톤 코드를 작성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UISwitch+ToDoGardenSwitch.swift
@@ -0,0 +1,24 @@
+//
+//  UISwitch+ToDoGardenSwitch.swift
+//
+//
+//  Created by SONG on 3/1/24.
+//
+
+import UIKit.UISwitch
+import ToDoGardenUIResource
+
+extension UISwitch {
+  public func ToDoGardenSwitchStyle() {
+  }
+}
+
+// MARK: - private functions
+
+extension UISwitch {
+  private func setupOnTintColor() {
+  }
+  
+  private func setupOffTintColor() {
+  }
+}
