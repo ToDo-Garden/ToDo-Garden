a7d8e58d0d1f77abc9f7beb233d9e56b6895ed1a
SONG <geun2121@gmail.com>
2024-03-20 17:14:32 +0900

    #43: 초기화문 작성
    - ToDoGardenBoxButton의 이니셜라이저를 작성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenBoxButton.swift
@@ -7,6 +7,23 @@
 
 import UIKit.UIButton
 
+import ToDoGardenUIConstant
+
 public class ToDoGardenBoxButton: UIButton {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+  
+  public convenience init(
+    isRoundRect: Bool,
+    text: String,
+    sizeType: ToDoGardenBoxButtonConstant.Size
+  ) {
+    self.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
   
 }
