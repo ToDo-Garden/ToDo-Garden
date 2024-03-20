05114662ddf275df3bc91f956caeb7663df6e02b
SONG <geun2121@gmail.com>
2024-03-20 17:01:20 +0900

    #43: Alpha ,Size 정의
    - ToDoGardenBoxButtonConstant에 2가지 Alpha값에 대한 상수를 정의하였습니다.
    -ToDoGardenBoxButtonConstant에 3가지 Size에 대한 상수를 정의하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/ToDoGardenBoxButtonConstant.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/ToDoGardenBoxButtonConstant.swift
@@ -8,5 +8,56 @@
 import Foundation
 
 public enum ToDoGardenBoxButtonConstant {
+  public enum Alpha {
+    case highlighted
+    case normal
+    
+    public var value: CGFloat {
+      switch self {
+      case ToDoGardenBoxButtonConstant.Alpha.highlighted:
+        return 0.7
+      case ToDoGardenBoxButtonConstant.Alpha.normal:
+        return 1.0
+      }
+    }
+  }
   
+  public enum Size {
+    case primary
+    case secondary
+    case tertiary
+    
+    public var width: CGFloat {
+      switch self {
+      case ToDoGardenBoxButtonConstant.Size.primary:
+        return 302.0
+      case ToDoGardenBoxButtonConstant.Size.secondary:
+        return 287.0
+      case ToDoGardenBoxButtonConstant.Size.tertiary:
+        return 288.0
+      }
+    }
+    
+    public var height: CGFloat {
+      switch self {
+      case ToDoGardenBoxButtonConstant.Size.primary:
+        return 49.0
+      case ToDoGardenBoxButtonConstant.Size.secondary:
+        return 55.0
+      case ToDoGardenBoxButtonConstant.Size.tertiary:
+        return 49.0
+      }
+    }
+    
+    public var cornerRadius: CGFloat {
+      switch self {
+      case ToDoGardenBoxButtonConstant.Size.primary:
+        return 49.0 / 2
+      case ToDoGardenBoxButtonConstant.Size.secondary:
+        return 55.0 / 2
+      case ToDoGardenBoxButtonConstant.Size.tertiary:
+        return 49.0 / 2
+      }
+    }
+  }
 }
