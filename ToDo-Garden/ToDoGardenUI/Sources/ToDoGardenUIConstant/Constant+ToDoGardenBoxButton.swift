acd8899774c27fe75ed60fe2d3c57d68b54fc314
SONG <geun2121@gmail.com>
2024-03-21 15:50:16 +0900

    #49: Size constant 정의
    - 세가지 Size에 대한 width,height,cornerRadius Constant를 정의하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenBoxButton.swift
@@ -12,4 +12,43 @@ extension Constant.ToDoGardenBoxButton {
     public static let normal: CGFloat = 1.0
     public static let highlighted: CGFloat = 0.7
   }
+  
+  public enum Size {
+    case primary
+    case secondary
+    case tertiary
+    
+    public var width: CGFloat {
+      switch self {
+      case Constant.ToDoGardenBoxButton.Size.primary:
+        return 302.0
+      case Constant.ToDoGardenBoxButton.Size.secondary:
+        return 287.0
+      case Constant.ToDoGardenBoxButton.Size.tertiary:
+        return 288.0
+      }
+    }
+    
+    public var height: CGFloat {
+      switch self {
+      case Constant.ToDoGardenBoxButton.Size.primary:
+        return 49.0
+      case Constant.ToDoGardenBoxButton.Size.secondary:
+        return 55.0
+      case Constant.ToDoGardenBoxButton.Size.tertiary:
+        return 49.0
+      }
+    }
+    
+    public var cornerRadius: CGFloat {
+      switch self {
+      case Constant.ToDoGardenBoxButton.Size.primary:
+        return 49.0 / 2
+      case Constant.ToDoGardenBoxButton.Size.secondary:
+        return 55.0 / 2
+      case Constant.ToDoGardenBoxButton.Size.tertiary:
+        return 49.0 / 2
+      }
+    }
+  }
 }
