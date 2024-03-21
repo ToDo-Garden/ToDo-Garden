1e5df2155325226b4810bbbbad87eefcb03222b6
SONG <geun2121@gmail.com>
2024-03-21 15:49:04 +0900

    #49: Alpha constant 정의
    - ToDoGardenBoxButton의 alpha constant를 정의하였습니다.
    - CGFloat을 사용하기 떄문에 CoreGraphics를 import 하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenBoxButton.swift
@@ -5,6 +5,11 @@
 //  Created by SONG on 3/21/24.
 //
 
+import CoreGraphics
+
 extension Constant.ToDoGardenBoxButton {
-  
+  public enum Alpha {
+    public static let normal: CGFloat = 1.0
+    public static let highlighted: CGFloat = 0.7
+  }
 }
