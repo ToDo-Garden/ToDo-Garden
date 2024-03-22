599ba6625026209ccdaf641e17c641ea41873570
dpfdallfm <dpfdlalfm@naver.com>
2024-03-22 15:21:34 +0900

    #47: Layout 타입 선언
    - AddButton 타입을 확장하여 레이아웃 관련 상수값들을 담는 타입을 선언하였습니다.
    - AddButton 내부 이미지에 대한 패딩 상수를 선언하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+AddButton.swift
@@ -0,0 +1,16 @@
+//
+//  Constant+AddButton.swift
+//
+//
+//  Created by Wood on 3/22/24.
+//
+
+import Foundation
+
+extension Constant.AddButton {
+  public enum Layout {}
+}
+
+extension Constant.AddButton.Layout {
+  public static let imagePadding: CGFloat = 4.0
+}
