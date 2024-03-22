d30719414dc0c3f5f1914b7a7036177866222f75
dpfdallfm <dpfdlalfm@naver.com>
2024-03-22 15:23:12 +0900

    #47: Alpha 타입 선언
    - AddButton 타입을 확장하여 버튼의 투명도를 담는 Alpha 타입을 선언하였습니다.
    - 버튼이 Highlighted 상태일 때 적용할 투명도 상수를 선언하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+AddButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+AddButton.swift
@@ -9,8 +9,13 @@ import Foundation
 
 extension Constant.AddButton {
   public enum Layout {}
+  public enum Alpha {}
 }
 
 extension Constant.AddButton.Layout {
   public static let imagePadding: CGFloat = 4.0
+}
+
+extension Constant.AddButton.Alpha {
+  public static let highlighted: CGFloat = 0.7
 }
