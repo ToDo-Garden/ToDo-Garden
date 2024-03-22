fdbfcfd7e5cf36c29cf382d198787059573dc204
SONG <geun2121@gmail.com>
2024-03-22 15:36:11 +0900

    #43: 초기화문 선언
    - 비어있는 초기화문을 선언하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenBoxButton.swift
@@ -8,4 +8,19 @@
 import UIKit.UIButton
 
 public final class ToDoGardenBoxButton: UIButton {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+  
+  public convenience init(
+    isRoundRect: Bool,
+    text: String,
+    sizeType: CGSize
+  ) {
+    self.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
 }
