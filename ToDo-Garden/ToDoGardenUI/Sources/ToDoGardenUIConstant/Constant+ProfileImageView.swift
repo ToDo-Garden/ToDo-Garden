7fbf499fe9779edf093acf70585d9f370ab3306d
wood0203 <dpfdlalfm@naver.com>
2024-04-08 20:36:30 +0900

    #60: Size 열거형 타입 선언
    - ProfileImageView의 크기를 나타내는 타입을 선언하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ProfileImageView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ProfileImageView.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Constant.ProfileImageView {
+  public enum Size {
+    public static let small  = CGSize(width: 36, height: 36)
+    public static let medium = CGSize(width: 56, height: 56)
+    public static let large  = CGSize(width: 86, height: 86)
+    public static let xLarge = CGSize(width: 241, height: 241)
+  }
+}
