85a45e03c2c4eedb46d7f9652b84e8103fe76a77
SONG <geun2121@gmail.com>
2024-04-09 15:57:09 +0900

    #55: 프로퍼티 이름 수정
    - 명확한 의미전달을 위해 segment -> item 으로수정했습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+PeriodSegmentedControl.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+PeriodSegmentedControl.swift
@@ -14,12 +14,12 @@ extension Constant.PeriodSegmentedControl {
 
 extension Constant.PeriodSegmentedControl.Layout {
   public static let innerPadding: CGFloat = 5.0
-  public static let segmentWidth: CGFloat = 24.0
-  public static let firstSegmentCenterXPosition: CGFloat = 17.0
+  public static let itemWidth: CGFloat = 24.0
+  public static let firstItemCenterXPosition: CGFloat = 17.0
   public static let height: CGFloat = 26.0
   public static let indicatorViewWidth: CGFloat = 23.0
 }
 
 extension Constant.PeriodSegmentedControl.Content {
-  public static let defaultSegments: [String] = ["일", "주", "월"]
+  public static let defaultItems: [String] = ["일", "주", "월"]
 }
