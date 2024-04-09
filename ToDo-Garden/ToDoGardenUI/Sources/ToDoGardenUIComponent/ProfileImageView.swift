59a81da86dcb4ae77830567a46b2825ee0ef5638
wood0203 <dpfdlalfm@naver.com>
2024-04-09 23:11:54 +0900

    #66: ProfileImageView 파일 생성
    - ProfileImageView 파일을 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ProfileImageView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileImageView.swift
+//
+//
+//  Created by Wood on 4/7/24.
+//
+
+import UIKit.UIImageView
+
+public final class ProfileImageView: UIImageView {
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
