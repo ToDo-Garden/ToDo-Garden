1bf41f95cc57255a36870ec586a49143acfd1061
wood0203 <dpfdlalfm@naver.com>
2024-04-10 16:25:01 +0900

    #66: ProfileImageViewStyle contentMode 변경 메서드 추가
    - ProfileImageViewStyle에 이미지를 보여주는 방식을 적용하는 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ProfileImageView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ProfileImageView.swift
@@ -13,22 +13,30 @@ public final class ProfileImageView: UIImageView {
   public init(size: CGSize) {
     self.size = size
     super.init(frame: CGRect.zero)
+    ProfileImageViewStyle.apply(to: self, with: self.size)
   }
 
   required init?(coder: NSCoder) {
     self.size = CGSize()
     super.init(coder: coder)
+    ProfileImageViewStyle.apply(to: self, with: self.size)
   }
 }
 
 private enum ProfileImageViewStyle {
   fileprivate static func apply(to imageView: UIImageView, with size: CGSize) {
     self.setupRoundedCorner(to: imageView, with: size)
+    self.setupContentAppearnce(to: imageView)
   }
 }
 
 extension ProfileImageViewStyle {
   private static func setupRoundedCorner(to imageView: UIImageView, with size: CGSize) {
     imageView.layer.cornerRadius = size.height / 2
+  }
+  
+  private static func setupContentAppearnce(to imageView: UIImageView) {
+    imageView.contentMode = UIView.ContentMode.scaleAspectFill
+    imageView.clipsToBounds = true
   }
 }
