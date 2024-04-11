84d186a7299a7fc47f1a99e195660214d8a460fb
SONG <geun2121@gmail.com>
2024-04-11 14:19:11 +0900

    #65: indicatorView 설정
    - 현재 활성화된 item을 표시하는 indicatorView를 추가합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControlBaseView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControlBaseView.swift
@@ -7,11 +7,14 @@
 
 import UIKit.UIView
 
+import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 final class PeriodSegmentedControlBaseView: UIImageView {
+  var indicatorView: UIImageView
   
   init() {
+    self.indicatorView = UIImageView(image: UIImage.periodSegmentedControlIndicator)
     super.init(frame: CGRect.zero)
   }
   
@@ -21,6 +24,7 @@ final class PeriodSegmentedControlBaseView: UIImageView {
   }
   
   required init?(coder: NSCoder) {
+    self.indicatorView = UIImageView(image: UIImage.periodSegmentedControlIndicator)
     super.init(coder: coder)
   }
 }
@@ -29,11 +33,26 @@ final class PeriodSegmentedControlBaseView: UIImageView {
 extension PeriodSegmentedControlBaseView {
   private func setup(with items: [String]) {
     self.setupBackgroundView()
+    self.setupIndicatorView()
   }
   
   private func setupBackgroundView() {
     self.image = UIImage.periodSegmentedControlBackground
     self.isUserInteractionEnabled = true
     self.contentMode = ContentMode.scaleToFill
+  }
+  
+  private func setupIndicatorView() {
+    let indicatorViewWidth = Constant.PeriodSegmentedControl.Layout.indicatorViewWidth
+    self.addSubview(self.indicatorView)
+    
+    self.indicatorView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate(
+      [
+        self.indicatorView.widthAnchor.constraint(equalToConstant: indicatorViewWidth),
+        self.indicatorView.heightAnchor.constraint(equalToConstant: indicatorViewWidth),
+        self.indicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+      ]
+    )
   }
 }
