fe00bf3ca723e912a650d0868096838e765c69f0
SONG <geun2121@gmail.com>
2024-04-12 14:38:19 +0900

    #72: items 프로퍼티 선언
    - items 프로퍼티를 선언하였습니다.
    - items 프로퍼티에 초기화문 내부에서 값을 할당합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControl.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControl.swift
@@ -11,12 +11,15 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 public final class PeriodSegmentedControl: UIControl {
+  private let items: [String]
   
   public init(items: [String] = Constant.PeriodSegmentedControl.Content.defaultItems) {
+    self.items = items
     super.init(frame: CGRect.zero)
   }
   
   required init?(coder: NSCoder) {
+    self.items = Constant.PeriodSegmentedControl.Content.defaultItems
     super.init(coder: coder)
   }
 }
