80b4ffabe8d3649131137c586ac51fa6ab215117
SONG <geun2121@gmail.com>
2024-04-13 20:07:11 +0900

    #72: periodSegmentedControlAppearance 사용
    - periodSegmentedControlAppearance를 생성합니다.
    - periodSegmentedControlAppearance를 통해 UI객체를 가져와서 레이아웃을 설정합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControl.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControl.swift
@@ -11,20 +11,42 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 public final class PeriodSegmentedControl: UIControl {
+  private let periodSegmentedControlAppearance: PeriodSegmentedControlAppearance
   private let items: [String]
   
   public init(items: [String] = Constant.PeriodSegmentedControl.Content.defaultItems) {
     self.items = items
+    self.periodSegmentedControlAppearance = PeriodSegmentedControlAppearance(with: self.items)
     super.init(frame: CGRect.zero)
+    self.setup()
   }
   
   required init?(coder: NSCoder) {
     self.items = Constant.PeriodSegmentedControl.Content.defaultItems
+    self.periodSegmentedControlAppearance = PeriodSegmentedControlAppearance(with: self.items)
     super.init(coder: coder)
+    self.setup()
   }
 }
 
 //MARK: - private functions
 extension PeriodSegmentedControl {
+  private func setup() {
+    self.setupAppearance()
+  }
   
+  private func setupAppearance() {
+    let appearance = self.periodSegmentedControlAppearance.getAssembledView()
+    self.addSubview(appearance)
+    
+    appearance.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate(
+      [
+        appearance.widthAnchor.constraint(equalTo: self.widthAnchor),
+        appearance.heightAnchor.constraint(equalTo: self.heightAnchor),
+        appearance.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+        appearance.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+      ]
+    )
+  }
 }
