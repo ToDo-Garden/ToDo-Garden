8ad350747a8988326da96a07919bc8b13a550e36
SONG <geun2121@gmail.com>
2024-04-17 15:03:34 +0900

    #73: 초기화메소드 정의
    - init() 메소드를 정의하였습니다.
    - init() 내부에서 프로퍼티를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControlGestureRecognizer.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControlGestureRecognizer.swift
@@ -5,8 +5,17 @@
 //  Created by SONG on 4/17/24.
 //
 
-import Foundation
+import UIKit
 
 final class PeriodSegmentedControlGestureRecognizer {
+  private let panRecognizer: UIPanGestureRecognizer
+  private let tapRecognizer: UITapGestureRecognizer
+  private let longpressRecognizer: UILongPressGestureRecognizer
   
+  init(target: UIControl, panAction: Selector?, tapAction: Selector?, longpressAction: Selector?) {
+    self.panRecognizer = UIPanGestureRecognizer(target: target, action: panAction)
+    self.tapRecognizer = UITapGestureRecognizer(target: target, action: tapAction)
+    self.longpressRecognizer = UILongPressGestureRecognizer(target: target, action: longpressAction)
+    
+  }
 }
