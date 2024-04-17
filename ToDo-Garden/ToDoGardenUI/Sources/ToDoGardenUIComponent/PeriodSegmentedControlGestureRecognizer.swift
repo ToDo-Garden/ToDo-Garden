49e103ba136e695872483052053dad1fb43f14fc
SONG <geun2121@gmail.com>
2024-04-17 15:00:26 +0900

    #73: 파일 추가
    - PeriodSegmentedControlGestureRecognizer 파일을 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/PeriodSegmentedControlGestureRecognizer.swift
@@ -0,0 +1,12 @@
+//
+//  PeriodSegmentedControlGestureRecognizer.swift
+//
+//
+//  Created by SONG on 4/17/24.
+//
+
+import Foundation
+
+final class PeriodSegmentedControlGestureRecognizer {
+  
+}
