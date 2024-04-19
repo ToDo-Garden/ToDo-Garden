840315593707ae89a5cc68a4355ae92a36c67f2c
dpfdallfm <dpfdlalfm@naver.com>
2024-04-19 21:55:21 +0900

    #81: SearchGardenButton 스타일 적용 함수 추가
    - UIButton을 확장하여 SearchGardenButton 스타일을 적용해주는 함수를 구현하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+SearchGardenButton.swift
@@ -0,0 +1,14 @@
+//
+//  UIButton+SearchGardenButton.swift
+//
+//
+//  Created by Wood on 4/19/24.
+//
+
+import UIKit.UIButton
+
+extension UIButton {
+  public func searchGardenButtonStyle() {
+    
+  }
+}
