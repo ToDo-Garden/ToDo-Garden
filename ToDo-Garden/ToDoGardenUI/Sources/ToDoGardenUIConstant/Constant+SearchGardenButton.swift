5239a15ea8b73de5d9ceef7570f256c9cb89be9b
dpfdallfm <dpfdlalfm@naver.com>
2024-04-19 14:57:58 +0900

    #78: ImageView trailing 제약조건 추가
    - Figma 디자인을 참고하여 검색 아이콘 이미지에 적용할 제약조건을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SearchGardenButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SearchGardenButton.swift
@@ -9,8 +9,13 @@ import Foundation
 
 extension Constant.SearchGardenButton {
   public enum Layout {}
+  public enum ImageView {}
 }
 
 extension Constant.SearchGardenButton.Layout {
   public static let cornerRadius: CGFloat = 12
+}
+
+extension Constant.SearchGardenButton.ImageView {
+  public static let trailingConstraint: CGFloat = -12.5
 }
