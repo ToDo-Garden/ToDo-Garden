baffad708b3618d5b60bbeefce3be7b6516b3962
dpfdallfm <dpfdlalfm@naver.com>
2024-04-24 16:10:22 +0900

    #89: ToDoGardenPlainButton 스타일 적용 함수 추가
    - ToDoGardenPlainButton 스타일을 적용할 수 있는 함수를 구현하였습니다.
    - 이미지만 설정하면 되기에 UIButton을 확장하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+ToDoGardenPlainButton.swift
@@ -0,0 +1,15 @@
+//
+//  UIButton+ToDoGardenPlainButton.swift
+//
+//
+//  Created by Wood on 4/18/24.
+//
+
+import UIKit
+
+extension UIButton { 
+  public func toDoGardenPlainButtonStyle() {
+    
+  }
+}
+
