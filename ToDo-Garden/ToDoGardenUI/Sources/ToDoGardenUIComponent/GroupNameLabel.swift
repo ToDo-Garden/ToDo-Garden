b6a30684a2f6282a22ec52f3cb8fdb0a01ee88cd
dpfdallfm <dpfdlalfm@naver.com>
2024-04-26 13:39:29 +0900

    #92: GroupNameLabel 객체 생성
    - 그룹명을 표시할 때 사용하는 레이블 객체를 생성하였습니다.
    - 텍스트에 패딩값을 부여하기 위해 커스텀 클래스로 구현하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
@@ -0,0 +1,18 @@
+//
+//  GroupNameLabel.swift
+//
+//
+//  Created by Wood on 4/26/24.
+//
+
+import UIKit
+
+public final class GroupNameLabel: UILabel {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+  
+  public required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
