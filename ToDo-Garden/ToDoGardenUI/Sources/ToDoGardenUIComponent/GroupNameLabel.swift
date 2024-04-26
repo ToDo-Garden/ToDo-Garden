43e39c53a6a435cbfa6dd9e40c2ebc371f8ece00
dpfdallfm <dpfdlalfm@naver.com>
2024-04-26 13:57:48 +0900

    #92: 텍스트 패팅 설정 함수 추가
    - 텍스트에 패딩 값을 부여할 때 사용하는 저장 프로퍼티를 추가하였습니다.
    - drawText 메서드에서 재사용되기 때문에 별도의 프로퍼티로 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
@@ -11,12 +11,16 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 public final class GroupNameLabel: UILabel {
+  private var textPadding: UIEdgeInsets
+
   public init() {
+    self.textPadding = UIEdgeInsets()
     super.init(frame: CGRect.zero)
     self.setupUI()
   }
   
   public required init?(coder: NSCoder) {
+    self.textPadding = UIEdgeInsets()
     super.init(coder: coder)
     self.setupUI()
   }
@@ -24,11 +28,21 @@ public final class GroupNameLabel: UILabel {
 
 extension GroupNameLabel {
   private func setupUI() {
+    self.setupTextPadding()
     self.setupTextStyle()
     self.setupBackgroundColor()
     self.setupRoundedCorner()
   }
-  
+
+  private func setupTextPadding() {
+    self.textPadding = UIEdgeInsets(
+      top: Constant.GroupNameLabel.Layout.TextPadding.top,
+      left: Constant.GroupNameLabel.Layout.TextPadding.left,
+      bottom: Constant.GroupNameLabel.Layout.TextPadding.bottom,
+      right: Constant.GroupNameLabel.Layout.TextPadding.right
+    )
+  }
+
   private func setupTextStyle() {
     self.font = UIFont.pretendardBodyBold
     self.textColor = UIColor.toDoGardenGreenDark
