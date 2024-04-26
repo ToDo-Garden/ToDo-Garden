6f97ade4e794ecd5597565d8e0d3375625a1cd7a
dpfdallfm <dpfdlalfm@naver.com>
2024-04-26 13:52:12 +0900

    #92: UI 설정 추상화 함수 추가
    - UI에 대한 모든 설정 함수들을 호출하는 추상화된 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
@@ -10,9 +10,17 @@ import UIKit
 public final class GroupNameLabel: UILabel {
   public init() {
     super.init(frame: CGRect.zero)
+    self.setupUI()
   }
   
   public required init?(coder: NSCoder) {
     super.init(coder: coder)
+    self.setupUI()
+  }
+}
+
+extension GroupNameLabel {
+  private func setupUI() {
+    
   }
 }
