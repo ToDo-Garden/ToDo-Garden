301adf178455da8daf7689f021be74bd12aa0e64
dpfdallfm <dpfdlalfm@naver.com>
2024-04-28 12:06:37 +0900

    #92: GroupNameModel 추가
    - 그룹명을 표시할 때 필요한 레이아웃 값들을 갖는 GroupNameModel을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
@@ -32,7 +32,7 @@ extension Styled {
     }
 
     public required init?(coder: NSCoder) {
-      self.configuration = Styled.UILabel.Configuration.groupName(Configuration.GroupNameModel.defaultPrimaryModel)
+      self.configuration = Styled.UILabel.Configuration.groupName(Configuration.GroupNameModel.defaultGroupModel)
       super.init(coder: coder)
       self.setupUI()
     }
@@ -57,6 +57,39 @@ extension Styled {
   }
 }
 
+// MARK: private functions
+
+extension Styled.UILabel {
+  private func setupUI() {
+    self.setupBackgroundColor()
+    self.setupText()
+    self.setupRoundedCorner()
+  }
+
+  private func setupBackgroundColor() {
+    switch self.configuration {
+    case let .groupName(model):
+      self.backgroundColor = model.backgroundColor
+    }
+  }
+
+  private func setupText() {
+    switch self.configuration {
+    case let .groupName(model):
+      self.font = model.font
+      self.textColor = model.textColor
+    }
+  }
+
+  private func setupRoundedCorner() {
+    switch self.configuration {
+    case let .groupName(model):
+      self.clipsToBounds = true
+      self.layer.cornerRadius = model.cornerRadius
+    }
+  }
+}
+
 // MARK: configuration
 
 extension Styled.UILabel {
@@ -65,15 +98,29 @@ extension Styled.UILabel {
       if case let Styled.UILabel.Configuration.groupName(model) = self {
         return model
       }
-      return Styled.UILabel.Configuration.GroupNameModel.defaultPrimaryModel
+      return Styled.UILabel.Configuration.GroupNameModel.defaultGroupModel
     }
 
     case groupName(Styled.UILabel.Configuration.GroupNameModel)
   }
 }
 
+// MARK: models
+
 extension Styled.UILabel.Configuration {
   struct GroupNameModel {
-    
+    let cornerRadius: CGFloat
+    let textPadding: UIEdgeInsets
+    let font: UIFont
+    let textColor: UIColor
+    let backgroundColor: UIColor
+
+    static let defaultGroupModel = Styled.UILabel.Configuration.GroupNameModel(
+      cornerRadius: 12,
+      textPadding: UIEdgeInsets(top: 5, left: 12, bottom: 5, right: 12),
+      font: UIFont.pretendardBodyBold,
+      textColor: UIColor.toDoGardenGreenDark,
+      backgroundColor: UIColor.toDoGardenGreenBackground
+    )
   }
 }
