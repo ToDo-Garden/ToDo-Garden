ba16e262343d8535621ea5935f3db686637b39e6
dpfdallfm <dpfdlalfm@naver.com>
2024-04-29 13:32:47 +0900

    #92: GroupNameLabel 커스텀 타입으로 수정
    - 오버라이딩이 필요하기에 UILabel 스타일을 확장하는 대신 별도의 커스텀 타입으로 생성할 수 있게 수정하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupNameLabel.swift
@@ -9,91 +9,79 @@ import UIKit
 
 import ToDoGardenUIResource
 
-public enum Styled {}
-
-extension Styled.UILabel {
-  public static let groupName = Styled.UILabel(
-    configuration: Configuration.groupName(
-      Configuration.GroupNameModel.defaultGroupModel
-    )
-  )
-}
-
-extension Styled {
-  open class UILabel: UIKit.UILabel {
-    private var configuration: Styled.UILabel.Configuration
-
-    public override var intrinsicContentSize: CGSize {
-      var contentSize = super.intrinsicContentSize
-
-      switch self.configuration {
-      case let .groupName(groupNameModel):
-        contentSize.width += (groupNameModel.textPadding.left + groupNameModel.textPadding.right)
-        contentSize.height += (groupNameModel.textPadding.top + groupNameModel.textPadding.bottom)
-      }
-
-      return contentSize
+public final class GroupNameLabel: UILabel {
+  private var configuration: GroupNameLabel.Configuration
+  
+  public override var intrinsicContentSize: CGSize {
+    var contentSize = super.intrinsicContentSize
+    
+    switch self.configuration {
+    case let .primary(model):
+      contentSize.width += (model.textPadding.left + model.textPadding.right)
+      contentSize.height += (model.textPadding.top + model.textPadding.bottom)
     }
+    
+    return contentSize
+  }
+  
+  init(configuration: GroupNameLabel.Configuration) {
+    self.configuration = configuration
+    super.init(frame: CGRect.zero)
+    self.setupUI()
+  }
 
-    init(configuration: Styled.UILabel.Configuration) {
-      self.configuration = configuration
-      super.init(frame: CGRect.zero)
-      self.setupUI()
+  public required init?(coder: NSCoder) {
+    self.configuration = GroupNameLabel.Configuration.primary(Configuration.PrimaryModel.defaultPrimaryModel)
+    super.init(coder: coder)
+    self.setupUI()
+  }
+  
+  public override func drawText(in rect: CGRect) {
+    let textPadding: UIEdgeInsets
+    switch self.configuration {
+    case let .primary(model):
+      textPadding = model.textPadding
     }
-
-    public required init?(coder: NSCoder) {
-      self.configuration = Styled.UILabel.Configuration.groupName(Configuration.GroupNameModel.defaultGroupModel)
-      super.init(coder: coder)
-      self.setupUI()
-    }
-
-    public override func drawText(in rect: CGRect) {
-      let textPadding: UIEdgeInsets
-      switch self.configuration {
-      case let .groupName(groupNameModel):
-        textPadding = groupNameModel.textPadding
-      }
-
-      super.drawText(
-        in: rect.inset(
-          by: textPadding
-        )
+    
+    super.drawText(
+      in: rect.inset(
+        by: textPadding
       )
-    }
-
-    public func updateText(with text: String) {
-      self.text = text
-    }
+    )
+  }
+  
+  public func updateText(with text: String) {
+    self.text = text
   }
 }
 
 // MARK: private functions
 
-extension Styled.UILabel {
+extension GroupNameLabel {
   private func setupUI() {
     self.setupBackgroundColor()
     self.setupText()
     self.setupRoundedCorner()
   }
-
+  
   private func setupBackgroundColor() {
     switch self.configuration {
-    case let .groupName(model):
+    case let .primary(model):
       self.backgroundColor = model.backgroundColor
     }
   }
 
   private func setupText() {
     switch self.configuration {
-    case let .groupName(model):
+    case let .primary(model):
       self.font = model.font
       self.textColor = model.textColor
     }
   }
-
+  
   private func setupRoundedCorner() {
     switch self.configuration {
-    case let .groupName(model):
+    case let .primary(model):
       self.clipsToBounds = true
       self.layer.cornerRadius = model.cornerRadius
     }
@@ -102,30 +90,30 @@ extension Styled.UILabel {
 
 // MARK: configuration
 
-extension Styled.UILabel {
+extension GroupNameLabel {
   public enum Configuration {
-    var groupNameModel: Styled.UILabel.Configuration.GroupNameModel {
-      if case let Styled.UILabel.Configuration.groupName(model) = self {
+    var primaryModel: GroupNameLabel.Configuration.PrimaryModel {
+      if case let GroupNameLabel.Configuration.primary(model) = self {
         return model
       }
-      return Styled.UILabel.Configuration.GroupNameModel.defaultGroupModel
+      return .defaultPrimaryModel
     }
-
-    case groupName(Styled.UILabel.Configuration.GroupNameModel)
+    
+    case primary(GroupNameLabel.Configuration.PrimaryModel)
   }
 }
 
 // MARK: models
 
-extension Styled.UILabel.Configuration {
-  public struct GroupNameModel {
+extension GroupNameLabel.Configuration {
+  public struct PrimaryModel {
     let cornerRadius: CGFloat
     let textPadding: UIEdgeInsets
     let font: UIFont
     let textColor: UIColor
     let backgroundColor: UIColor
-
-    static let defaultGroupModel = Styled.UILabel.Configuration.GroupNameModel(
+    
+    static let defaultPrimaryModel = GroupNameLabel.Configuration.PrimaryModel(
       cornerRadius: 12,
       textPadding: UIEdgeInsets(top: 5, left: 12, bottom: 5, right: 12),
       font: UIFont.pretendardBodyBold,
@@ -134,3 +122,49 @@ extension Styled.UILabel.Configuration {
     )
   }
 }
+
+extension GroupNameLabel {
+  public static let primary = GroupNameLabel(
+    configuration: Configuration.primary(
+      Configuration.PrimaryModel.defaultPrimaryModel
+    )
+  )
+}
+
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+
+    // MARK: - UIViewRepresentable
+
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+#endif
+// MyYellowButton
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct MyYellowButtonPreview: PreviewProvider{
+    static var previews: some View {
+        UIViewPreview {
+          let label = GroupNameLabel.primary
+          label.updateText(with: "iOS 인터뷰 스터디")
+          return label
+        }.previewLayout(.sizeThatFits)
+    }
+}
+#endif
