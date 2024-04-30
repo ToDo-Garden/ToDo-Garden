3536178b675129d3e4462a4ff08fa42f3cb22149
GangWoon <gangwoon2018@gmail.com>
2024-04-30 11:57:00 +0900

    #94: UIStackView Extension 수정

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIStackView+Extension.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIStackView+Extension.swift
@@ -1,16 +1,18 @@
 import UIKit
 
 extension UIStackView {
-  public func addSpacing(_ value: CGFloat) {
+  public func addSpacing(_ value: CGFloat? = nil) {
     let spacing = UIView()
     spacing.usingAutolayout()
-    switch self.axis {
-    case .vertical:
-      spacing.heightAnchor.constraint(equalToConstant: value).isActive = true
-    case .horizontal:
-      spacing.widthAnchor.constraint(equalToConstant: value).isActive = true
-    @unknown default:
-      break
+    if let value {
+      switch self.axis {
+      case .vertical:
+        spacing.heightAnchor.constraint(equalToConstant: value).isActive = true
+      case .horizontal:
+        spacing.widthAnchor.constraint(equalToConstant: value).isActive = true
+      @unknown default:
+        break
+      }
     }
     self.addArrangedSubview(spacing)
   }
