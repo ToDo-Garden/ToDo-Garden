0699e27f55a44b3b8df23e95d4ab2b67d16f9004
SONG <geun2121@gmail.com>
2024-04-30 16:23:45 +0900

    #95: Alpha 정의
    - Alpha 설정에 필요한 스태틱 상수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenAlertView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenAlertView.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 extension Constant.ToDoGardenAlertView {
+  public enum Alpha {}
+}
 
+extension Constant.ToDoGardenAlertView.Alpha {
+  public static let touchedAlpha = 0.7
+  public static let normalAlpha = 1.0
 }
