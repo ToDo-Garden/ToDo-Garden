f6f9a251a4ce7c69c577533c3aafc117fdf52392
SONG <geun2121@gmail.com>
2024-04-30 16:33:27 +0900

    #95: Constant 컨테이너 모델 정의
    - 각 UI에 들어갈 불변값들을 모아서 한번에 전달하기 위한 컨테이너 모델을 구현하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenAlertView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoGardenAlertView.swift
@@ -25,3 +25,64 @@ extension Constant.ToDoGardenAlertView.Alpha {
   public static let touchedAlpha = 0.7
   public static let normalAlpha = 1.0
 }
+
+extension Constant.ToDoGardenAlertView.Content {
+  public struct ViewState {
+    public let backPlane: BackPlaneState
+    public let title: TitleViewState
+    public let description: DescriptionViewState
+    public let buttons: [ButtonLabelState]
+    public let stackView: StackViewState
+  }
+  
+  public struct BackPlaneState {
+    public let width: CGFloat
+    public let height: CGFloat
+    public let cornerRadius: CGFloat
+  }
+  
+  public struct TitleViewState {
+    public let text: String
+    public let topMargin: CGFloat
+  }
+  
+  public struct DescriptionViewState {
+    public let text: String
+    public let topMargin: CGFloat
+    public let numberOfLines: Int
+    
+    init(text: String, topMargin: CGFloat, numberOfLines: Int = 2) {
+      self.text = text
+      self.topMargin = topMargin
+      self.numberOfLines = numberOfLines
+    }
+  }
+  
+  public struct ButtonLabelState {
+    public let text: String
+    public let isRed: Bool
+    public let buttonActionType: ButtonActionType
+  }
+  
+  public struct StackViewState {
+    public let isHorizontal: Bool
+    public let height: CGFloat
+    public let dividerThickness: CGFloat
+    
+    init(isHorizontal: Bool, height: CGFloat, dividerThickness: CGFloat = 1.0) {
+      self.isHorizontal = isHorizontal
+      self.height = height
+      self.dividerThickness = dividerThickness
+    }
+  }
+  
+  public enum ButtonActionType {
+    case cancel
+    case keepConcentration
+    case goHome
+    case delete
+    case unsubscribe
+    case logout
+    case stopConcentration
+  }
+}
