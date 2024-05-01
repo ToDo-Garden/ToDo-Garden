d144e648a93623ba4a708df19c46f07b34abb02e
dpfdallfm <dpfdlalfm@naver.com>
2024-05-01 15:52:09 +0900

    #104: 애니메이션 관련 속성 상수 값 추가
    - 투두 완료 애니메이션의 속성과 관련된 상수 값들을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoCheckBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoCheckBoxButton.swift
@@ -10,6 +10,7 @@ import Foundation
 extension Constant.ToDoCheckBoxButton {
   public enum Layout { }
   public enum Action { }
+  public enum Animation { }
 }
 
 extension Constant.ToDoCheckBoxButton.Layout {
@@ -19,4 +20,12 @@ extension Constant.ToDoCheckBoxButton.Layout {
 
 extension Constant.ToDoCheckBoxButton.Action {
   public static let impactIntesity: CGFloat = 0.6
+}
+
+extension Constant.ToDoCheckBoxButton.Animation {
+  public static let lineWidth: CGFloat = 1.0
+  public static let keyPath: String = "strokeEnd"
+  public static let duration: CGFloat = 0.4
+  public static let fromValue: CGFloat = 0.0
+  public static let toValue: CGFloat = 1.0
 }
