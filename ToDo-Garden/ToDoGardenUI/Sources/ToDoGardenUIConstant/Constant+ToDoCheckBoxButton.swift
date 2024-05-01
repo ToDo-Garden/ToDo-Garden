ef5ccc42eca4404848cb2d03fe16f7324309a1c2
dpfdallfm <dpfdlalfm@naver.com>
2024-05-01 15:47:04 +0900

    #104: 테두리 레이아웃 관련 상수 추가
    - 테두리 레이아웃 설정에 관련된 상수 값들을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoCheckBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+ToDoCheckBoxButton.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 extension Constant.ToDoCheckBoxButton {
-  
+  public enum Layout { }
+}
+
+extension Constant.ToDoCheckBoxButton.Layout {
+  public static let borderWidth: CGFloat = 0.6
+  public static let cornerRadius: CGFloat = 3.0
 }
