e6794b974ae91c8bf2731b13af990a665977809a
dpfdallfm <dpfdlalfm@naver.com>
2024-05-02 12:23:31 +0900

    #107: PrimaryModel 타입 추가
    - 버튼에 필요한 설정값들을 입력값들을 담는 PrimaryModel 타입을 선언하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CreateToDoButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CreateToDoButton.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+import ToDoGardenUIResource
+
 public final class CreateToDoButton: UIButton {
-  
-  public init() {
+  private var primaryModel: CreateToDoButton.PrimaryModel
+
+  public init(primaryModel: CreateToDoButton.PrimaryModel) {
+    self.primaryModel = primaryModel
     super.init(frame: CGRect.zero)
   }
 
+  @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Primary Model
+
+extension CreateToDoButton {
+  public struct PrimaryModel {
+    var title: String
+    var imagePadding: CGFloat
+    var contentInsets: NSDirectionalEdgeInsets
+    var image: UIImage
+
+    public init(
+      title: String,
+      imagePadding: CGFloat = Constant.CreateToDoButton.Layout.Primary.imagePadding,
+      contentInsets: NSDirectionalEdgeInsets = Constant.CreateToDoButton.Layout.Primary.contentInsets,
+      image: UIImage = UIImage.createToDoButtonImage
+    ) {
+      self.title = title
+      self.imagePadding = imagePadding
+      self.contentInsets = contentInsets
+      self.image = image
+    }
   }
 }
