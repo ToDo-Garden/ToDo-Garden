ab5f3445f5eebb41fc4f1f713543ff56b91d2242
dpfdallfm <dpfdlalfm@naver.com>
2024-05-02 11:51:15 +0900

    #107: CreateToDoButton 객체 생성
    - CreateToDoButton 컴포넌트 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CreateToDoButton.swift
@@ -0,0 +1,19 @@
+//
+//  CreateToDoButton.swift
+//
+//
+//  Created by Wood on 5/2/24.
+//
+
+import UIKit
+
+public final class CreateToDoButton: UIButton {
+  
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
