757f92d9f25bfb1433cf3ac4d87fc286e412b9c7
dpfdallfm <dpfdlalfm@naver.com>
2024-05-02 13:23:30 +0900

    #46: ToDoCheckBoxButton 객체 생성
    - 투두를 완료했을 때 사용하는 체크박스 버튼 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoCheckBoxButton.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoCheckBoxButton.swift
+//
+//
+//  Created by Wood on 5/2/24.
+//
+
+import UIKit
+
+import ToDoGardenUIResource
+import ToDoGardenUIConstant
+
+public final class ToDoCheckBoxButton: UIButton {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
