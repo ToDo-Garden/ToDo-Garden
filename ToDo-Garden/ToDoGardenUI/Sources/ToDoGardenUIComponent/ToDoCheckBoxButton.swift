7b003bbb86fcfcd65c9d9e3ca6a946823cfd4318
dpfdallfm <dpfdlalfm@naver.com>
2024-05-02 13:25:26 +0900

    #46: CheckBoxModel 타입 추가
    - 버튼의 상태 설정에 필요한 값들을 담는 타입인 CheckBoxModel을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoCheckBoxButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoCheckBoxButton.swift
@@ -11,12 +11,38 @@ import ToDoGardenUIResource
 import ToDoGardenUIConstant
 
 public final class ToDoCheckBoxButton: UIButton {
-  public init() {
+  private var checkBoxModel: ToDoCheckBoxButton.CheckBoxModel
+
+  public init(checkBoxModel: ToDoCheckBoxButton.CheckBoxModel) {
+    self.checkBoxModel = checkBoxModel
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   public required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: CheckBox Model
+
+extension ToDoCheckBoxButton {
+  public struct CheckBoxModel {
+    var isToDoDone: Bool
+    var groupColor: UIColor
+    var borderWidth: CGFloat
+    var cornerRadius: CGFloat
+
+    public init(
+      isToDoDone: Bool,
+      groupColor: UIColor,
+      borderWidth: CGFloat = Constant.ToDoCheckBoxButton.Layout.borderWidth,
+      cornerRadius: CGFloat = Constant.ToDoCheckBoxButton.Layout.cornerRadius
+    ) {
+      self.isToDoDone = isToDoDone
+      self.groupColor = groupColor
+      self.borderWidth = borderWidth
+      self.cornerRadius = cornerRadius
+    }
   }
 }
