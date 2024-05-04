8375474dfdcbb83b0c54f6e87598d655f87e4d4d
SONG <geun2121@gmail.com>
2024-05-04 16:32:03 +0900

    #110: 파일 추가 및 스켈레톤 코드 작성
    - ToDoGardenAlertView.swift 파일을 생성하였습니다.
    - 초기화 문을 비롯한 스켈레톤 코드를 작성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenAlertView.swift
@@ -0,0 +1,42 @@
+//
+//  ToDoGardenAlertView.swift
+//
+//
+//  Created by SONG on 5/4/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+
+final public class ToDoGardenAlertView: UIView {
+  private var configuration: Configuration
+  
+  init(configuration: Configuration) {
+    self.configuration = configuration
+    super.init(frame: CGRect.zero)
+    self.build()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func build() {
+    
+  }
+  
+}
+
+extension ToDoGardenAlertView {
+  public struct Configuration {
+    let contents: Constant.ToDoGardenAlertView.Content.ViewState
+    
+    public init(
+      contents: Constant.ToDoGardenAlertView.Content.ViewState
+    ) {
+      self.contents = contents
+    }
+  }
+}
