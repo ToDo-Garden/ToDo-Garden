ee3538bf5a6d9446325b680b7361df08ce045bb3
dpfdallfm <dpfdlalfm@naver.com>
2024-05-04 23:17:42 +0900

    #114: 텍스트 패딩 값 추가
    - Constant에 레이블 텍스트 패딩 값을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupNameLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupNameLabel.swift
@@ -10,3 +10,18 @@ import Foundation
 extension Constant.GroupNameLabel {
   public enum Layout {}
 }
+
+extension Constant.GroupNameLabel.Layout {
+  public enum TextPadding {}
+}
+
+extension Constant.GroupNameLabel.Layout {
+  public static let cornerRadius: CGFloat = 11.5
+}
+
+extension Constant.GroupNameLabel.Layout.TextPadding {
+  public static let top: CGFloat = 5
+  public static let left: CGFloat = 11
+  public static let bottom: CGFloat = 4
+  public static let right: CGFloat = 11
+}
