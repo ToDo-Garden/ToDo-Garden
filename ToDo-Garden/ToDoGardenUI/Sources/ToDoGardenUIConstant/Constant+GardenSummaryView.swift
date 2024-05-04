8c3f144ff31ffc808f78466811f6a62724e57140
SONG <geun2121@gmail.com>
2024-05-04 19:03:19 +0900

    #112: Layout 관련 상수 선언
    - Layout 관련 상수들을 선언했습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GardenSummaryView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GardenSummaryView.swift
@@ -12,4 +12,20 @@ extension Constant.GardenSummaryView {
     static let averageTimeTitle: String = "평균 집중 시간"
     static let averageCompleteTitle: String = "평균 완료 수"
   }
+  
+  private enum Layout {
+    static let backPlaneWidth: CGFloat = 331.0
+    static let backPlaneHeight: CGFloat = 103.0
+    static let cornerRadius: CGFloat = 10.0
+    static let borderWidth: CGFloat = 1.0
+    
+    static let titleLeftMargin: CGFloat = 10.0
+    static let titleTopMargin: CGFloat = 11.0
+    
+    static let decriptionRightMargin: CGFloat = -30.0
+    static let decriptionBottomMargin: CGFloat = -20.0
+    
+    static let lineWidth: CGFloat = 2.0
+    static let lineHeight: CGFloat = 56.5
+  }
 }
