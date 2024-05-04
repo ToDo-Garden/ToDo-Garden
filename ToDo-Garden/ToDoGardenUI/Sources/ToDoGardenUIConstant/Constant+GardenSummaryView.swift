22fc2129561f304e89172fdf5aa7b74166734a3f
SONG <geun2121@gmail.com>
2024-05-04 18:54:50 +0900

    #112: String 관련 상수 선언
    - Constant.GardenSummaryView.Content 내부에, String관련 상수를 선언하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GardenSummaryView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GardenSummaryView.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 extension Constant.GardenSummaryView {
-
+  public enum Content {
+    static let averageTimeTitle: String = "평균 집중 시간"
+    static let averageCompleteTitle: String = "평균 완료 수"
+  }
 }
