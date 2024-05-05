f0ce37df294269e66ad5d4d09be07e6f0636c724
SONG <geun2121@gmail.com>
2024-05-05 15:57:01 +0900

    #118: 파일생성 및 스켈레톤 코드 작성
    - ToDoGardenAlertController를 생성하여 추가했습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenAlertController.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoGardenAlertController.swift
+//
+//
+//  Created by SONG on 5/5/24.
+//
+
+import UIKit.UIViewController
+
+import ToDoGardenUIConstant
+
+public final class ToDoGardenAlertController: UIViewController {
+  private var alertView: ToDoGardenAlertView
+  
+  public init(for alertType: ToDoGardenAlertView.Configuration) {
+    self.alertView = ToDoGardenAlertView(configuration: alertType)
+    super.init(nibName: nil, bundle: nil)
+    self.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    self.layout()
+  }
+  
+  private func layout() {
+    
+  }
+}
