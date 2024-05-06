f51ff7384e6ebc31e44f802cc590e172bb090568
dpfdallfm <dpfdlalfm@naver.com>
2024-05-06 16:25:13 +0900

    #124: PrimaryModel Constant 추가
    - PrimaryModel 생성시 레이아웃 관련 상수들의 기본값을 Constant에 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+CreateToDoButton.swift
@@ -0,0 +1,22 @@
+//
+//  Constant+CreateToDoButton.swift
+//
+//
+//  Created by Wood on 5/6/24.
+//
+
+import Foundation
+
+extension Constant.CreateToDoButton {
+  public enum Layout {}
+}
+
+extension Constant.CreateToDoButton.Layout {
+  public enum Primary {
+    public static let imagePadding: CGFloat = 5
+    public static let topMargin: CGFloat = 5
+    public static let leadingMargin: CGFloat = 12
+    public static let bottomMargin: CGFloat = 4
+    public static let trailingMargin: CGFloat = 8
+  }
+}
