3b110fbfb3071750af5b136824b5ab95033f02a8
dpfdallfm <dpfdlalfm@naver.com>
2024-05-07 15:58:24 +0900

    #130: AlarmTimeView Constant 추가
    - AlarmTimeView의 레이아웃 관련 상수들을 선언할 수 있도록 Constant 파일을 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+AlarmTimeView.swift
@@ -0,0 +1,22 @@
+//
+//  Constant+AlarmTimeView.swift
+//  
+//
+//  Created by Wood on 5/7/24.
+//
+
+import Foundation
+
+extension Constant.AlarmTimeView {
+  public enum Layout {}
+}
+
+extension Constant.AlarmTimeView.Layout {
+  public enum TimeLabel {}
+  public enum AlarmSettingButton {}
+}
+
+extension Constant.AlarmTimeView.Layout {
+  public static let borderWidth: CGFloat = 1.0
+  public static let cornerRadius: CGFloat = 15.5
+}
