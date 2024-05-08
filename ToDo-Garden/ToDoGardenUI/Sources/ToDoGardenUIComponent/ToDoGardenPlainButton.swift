45dbf74031a2b22accbd19ec4a00edb15da4bba5
dpfdallfm <dpfdlalfm@naver.com>
2024-05-08 13:33:24 +0900

    #89: Model 객체 생성
    - PlainButton에 필요한 데이터를 담는 Model 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenPlainButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenPlainButton.swift
@@ -8,12 +8,25 @@
 import UIKit.UIButton
 
 public final class ToDoGardenPlainButton: UIButton {
-  public init() {
+  private var model: Model
+
+  public init(model: Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension ToDoGardenPlainButton {
+  public struct Model {
+    let image: UIImage
+
+    init(image: UIImage) {
+      self.image = image
+    }
   }
 }
