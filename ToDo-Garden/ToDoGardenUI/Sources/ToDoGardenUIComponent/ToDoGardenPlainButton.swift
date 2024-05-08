e0802e81462b72ea1b9a6a1ef5e658a124b1e8b7
dpfdallfm <dpfdlalfm@naver.com>
2024-05-08 13:13:57 +0900

    #89: ToDoGardenPlainButton 객체 생성
    - 기본 버튼으로 사용하는 ToDoGardenPlainButton 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoGardenPlainButton.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoGardenPlainButton.swift
+//
+//
+//  Created by Wood on 4/18/24.
+//
+
+import UIKit.UIButton
+
+public final class ToDoGardenPlainButton: UIButton {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
