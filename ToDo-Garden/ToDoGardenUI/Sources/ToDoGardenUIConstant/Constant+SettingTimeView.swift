31fb85fec7122132c905e7ae3cab87d24365081d
SONG <geun2121@gmail.com>
2024-05-09 13:29:41 +0900

    #133: Content 추가
    - String 관련 상수

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SettingTimeView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SettingTimeView.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 extension Constant.SettingTimeView {
+  private enum Content {
+    static let focusTimeTitle: String = "집중시간 설정"
+    static let breakTimeTitle: String = "휴식시간 설정"
+    static let notificationTimeTitle: String = "시간 설정"
+  }
   
 }
