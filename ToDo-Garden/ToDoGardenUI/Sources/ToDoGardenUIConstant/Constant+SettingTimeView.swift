a682caa963755039a9c9e6805ce1fd0fde354544
SONG <geun2121@gmail.com>
2024-05-09 13:37:59 +0900

    #133: TimePicker.DataStore 정의
    - TimePicker.DataStore를 정의했습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SettingTimeView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SettingTimeView.swift
@@ -39,13 +39,88 @@ extension Constant.SettingTimeView {
     public var dataStore: TimePicker.DataStore {
       switch self {
       case TimePicker.alarmTime:
-        return TimePicker.DataStore()
+        return TimePicker.DataStore(
+          highlightedView: HighlightedView(
+            leading: 40.0,
+            trailing: -20.0,
+            height: 33.0
+          ),
+          hourUnitLabel: HourUnitLabel(
+            text: Content.hour,
+            leading: 34.0
+          ),
+          minuteUnitLabel: MinuteUnitLabel(
+            text: Content.minute,
+            leading: 15.0
+          ),
+          secondUnitLabel: nil,
+          pickerView: PickerView(
+            topMargin: 95.0,
+            width: 214.0,
+            height: 216.0,
+            rowHeight: 43.0,
+            numberOfComponents: 2,
+            numberOfRowsInComponent: [24, 60]
+          )
+        )
         
       case TimePicker.focusTime:
-        return TimePicker.DataStore()
+        return TimePicker.DataStore(
+          highlightedView: HighlightedView(
+            leading: 40.0,
+            trailing: -20.0,
+            height: 33.0
+          ),
+          hourUnitLabel: HourUnitLabel(
+            text: Content.hour,
+            leading: 34.0
+          ),
+          minuteUnitLabel: MinuteUnitLabel(
+            text: Content.minute,
+            leading: 15.0
+          ),
+          secondUnitLabel: SecondUnitLabel(
+            text: Content.second,
+            leading: -7.0
+          ),
+          pickerView: PickerView(
+            topMargin: 95.0,
+            width: 320.0,
+            height: 216.0,
+            rowHeight: 43.0,
+            numberOfComponents: 3,
+            numberOfRowsInComponent: [24, 60, 60]
+          )
+        )
         
       case TimePicker.breakTime:
-        return TimePicker.DataStore()
+        return TimePicker.DataStore(
+          highlightedView: HighlightedView(
+            leading: 40.0,
+            trailing: -20.0,
+            height: 33.0
+          ),
+          hourUnitLabel: HourUnitLabel(
+            text: Content.hour,
+            leading: 34.0
+          ),
+          minuteUnitLabel: MinuteUnitLabel(
+            text: Content.minute,
+            leading: 15.0
+          ),
+          secondUnitLabel: SecondUnitLabel(
+            text: Content.second,
+            leading: -7.0
+          ),
+          pickerView: PickerView(
+            topMargin: 95.0,
+            width: 320.0,
+            height: 216.0,
+            rowHeight: 43.0,
+            numberOfComponents: 3,
+            numberOfRowsInComponent: [2, 60, 60]
+          )
+        )
       }
     }
   }
@@ -56,5 +131,47 @@ extension Constant.SettingTimeView {
 }
 
 extension Constant.SettingTimeView.TimePicker {
-  public struct DataStore {}
+  private enum Content {
+    static let hour = "시간"
+    static let minute = "분"
+    static let second = "초"
+  }
+  
+  public struct DataStore {
+    public let highlightedView: HighlightedView
+    public let hourUnitLabel: HourUnitLabel
+    public let minuteUnitLabel: MinuteUnitLabel
+    public let secondUnitLabel: SecondUnitLabel?
+    public let pickerView: PickerView
+  }
+  
+  public struct HighlightedView {
+    public let leading: CGFloat
+    public let trailing: CGFloat
+    public let height: CGFloat
+  }
+  
+  public struct HourUnitLabel {
+    public let text: String
+    public let leading: CGFloat
+  }
+  
+  public struct MinuteUnitLabel {
+    public let text: String
+    public let leading: CGFloat
+  }
+  
+  public struct SecondUnitLabel {
+    public let text: String
+    public let leading: CGFloat
+  }
+  
+  public struct PickerView {
+    public let topMargin: CGFloat
+    public let width: CGFloat
+    public let height: CGFloat
+    public let rowHeight: CGFloat
+    public let numberOfComponents: Int
+    public let numberOfRowsInComponent: [Int]
+  }
 }
