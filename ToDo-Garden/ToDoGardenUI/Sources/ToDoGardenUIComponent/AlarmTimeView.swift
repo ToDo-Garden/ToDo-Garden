2a870d6dabdc3257572ef808e62881dcac1fc923
dpfdallfm <dpfdlalfm@naver.com>
2024-05-10 20:05:35 +0900

    #132: AlarmTimeView 객체 생성
    - 투두 알림 시간을 설정할 때 사용하는 AlarmTimeView 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AlarmTimeView.swift
@@ -0,0 +1,22 @@
+//
+//  AlarmTimeView.swift
+//
+//
+//  Created by Wood on 5/10/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+import ToDoGardenUIResource
+
+public final class AlarmTimeView: UIView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
