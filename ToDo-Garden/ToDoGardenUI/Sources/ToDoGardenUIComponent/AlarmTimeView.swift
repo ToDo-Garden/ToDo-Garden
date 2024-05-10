4f1823133149369674cc94aacc51af48dd197b16
dpfdallfm <dpfdlalfm@naver.com>
2024-05-10 20:24:10 +0900

    #132: 알람 설정 버튼 추가
    - 알람을 설정할 수 있는 버튼을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AlarmTimeView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AlarmTimeView.swift
@@ -12,15 +12,80 @@ import ToDoGardenUIResource
 
 public final class AlarmTimeView: UIView {
   private var model: AlarmTimeView.Model
+  private var timeLabel: UILabel
+  private var alarmSettingButton: UIButton
 
   public init(model: AlarmTimeView.Model) {
     self.model = model
+    self.timeLabel = UILabel()
+    self.alarmSettingButton = UIButton()
     super.init(frame: CGRect.zero)
+    self.setup()
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+
+extension AlarmTimeView {
+  private func setup() {
+    self.setupBorder()
+    self.setupTimeLabel()
+    self.setupAlarmSettingButton()
+  }
+
+  private func setupBorder() {
+    self.layer.cornerRadius = self.model.cornerRadius
+    self.layer.borderWidth = self.model.borderWidth
+    self.layer.borderColor = UIColor.toDoGardenGreenDark.cgColor
+  }
+
+  private func setupTimeLabel() {
+    self.timeLabel.text = self.model.labelText
+    self.timeLabel.font = UIFont.pretendardBodySemiBold
+    self.timeLabel.textColor = UIColor.toDoGardenGreenDark
+  }
+
+  private func setupAlarmSettingButton() {
+    self.setupAlarmSettingButtonConfiguration()
+    self.setupAlarmSettingButtonUpdateHandler()
+    self.setupAlarmSettingButtonTitle(with: self.model.alarmTime)
+  }
+
+  private func setupAlarmSettingButtonConfiguration() {
+    var configuration = UIButton.Configuration.filled()
+    configuration.contentInsets = model.contentInsets
+    configuration.baseBackgroundColor = UIColor.toDoGardenGreenBackground
+    self.alarmSettingButton.configuration = configuration
+  }
+
+  private func setupAlarmSettingButtonUpdateHandler() {
+    self.alarmSettingButton.configurationUpdateHandler = { button in
+      switch button.state {
+      case UIControl.State.normal:
+        button.configuration?.attributedTitle?.foregroundColor = UIColor.toDoGardenGreenDark
+        button.configuration?.baseBackgroundColor = UIColor.toDoGardenGreenBackground
+      case UIControl.State.disabled:
+        button.configuration?.attributedTitle?.foregroundColor = UIColor.toDoGardenGray3
+      default:
+        break
+      }
+    }
+  }
+
+  private func setupAlarmSettingButtonTitle(with text: String) {
+    let attributes = AttributeContainer(
+      [
+        NSAttributedString.Key.font: UIFont.pretendardBodyMedium,
+        NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark
+      ]
+    )
+    let attributedTitle = AttributedString(text, attributes: attributes)
+    self.alarmSettingButton.configuration?.attributedTitle = attributedTitle
   }
 }
 
