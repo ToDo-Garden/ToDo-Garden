0b2aec05538ccd3dd5320ff9a45f56570287a42b
dpfdallfm <dpfdlalfm@naver.com>
2024-05-10 20:06:21 +0900

    #132: Model 객체 생성
    - 다양한 UI 스타일을 지정할 수 있도록 Model 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AlarmTimeView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AlarmTimeView.swift
@@ -11,12 +11,40 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 public final class AlarmTimeView: UIView {
-  public init() {
+  private var model: AlarmTimeView.Model
+
+  public init(model: AlarmTimeView.Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Model
+
+extension AlarmTimeView {
+  public struct Model {
+    let labelText: String
+    let alarmTime: String
+    let borderWidth: CGFloat
+    let cornerRadius: CGFloat
+    let contentInsets: NSDirectionalEdgeInsets
+
+    public static let primary = Self(
+      labelText: Constant.AlarmTimeView.StringLiteral.TimeLabel.defaultText,
+      alarmTime: Constant.AlarmTimeView.StringLiteral.AlarmSettingButton.defaultTimeText,
+      borderWidth: Constant.AlarmTimeView.Layout.borderWidth,
+      cornerRadius: Constant.AlarmTimeView.Layout.cornerRadius,
+      contentInsets: NSDirectionalEdgeInsets(
+        top: Constant.AlarmTimeView.Layout.AlarmSettingButton.ContentInsets.top,
+        leading: Constant.AlarmTimeView.Layout.AlarmSettingButton.ContentInsets.leading,
+        bottom: Constant.AlarmTimeView.Layout.AlarmSettingButton.ContentInsets.bottom,
+        trailing: Constant.AlarmTimeView.Layout.AlarmSettingButton.ContentInsets.trailing
+      )
+    )
   }
 }
