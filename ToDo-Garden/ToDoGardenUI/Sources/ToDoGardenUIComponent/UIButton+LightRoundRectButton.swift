e75191ca40252e8352d5d5be6d01964fb346d359
SONG <geun2121@gmail.com>
2024-05-17 17:27:55 +0900

    #152: applyLightRoundRectButtonStyle()
    - LightRoundRectButton를 만드는 메서드를 정의했습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+LightRoundRectButton.swift
@@ -0,0 +1,33 @@
+//
+//  UIButton+LightRoundRectButton.swift
+//
+//
+//  Created by SONG on 5/17/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+
+extension UIButton {
+  public func applyLightRoundRectButtonStyle(title: String) {
+    LightRoundRectButton.apply(to: self, title: title)
+  }
+}
+
+private enum LightRoundRectButton {
+  fileprivate static func apply(to button: UIButton, title: String) {
+    var buttonConfiguration = UIButton.Configuration.filled()
+    buttonConfiguration.contentInsets = Constant.LightRoundRectButton.contentsInsets
+    buttonConfiguration.baseForegroundColor = .toDoGardenGreenDark
+    buttonConfiguration.baseBackgroundColor = .toDoGardenGreenBackground
+    
+    let attributedString = NSAttributedString(
+      string: title,
+      attributes: [NSAttributedString.Key.font: UIFont.pretendardBodySemiBold]
+    )
+
+    button.configuration = buttonConfiguration
+    button.setAttributedTitle(attributedString, for: UIControl.State.normal)
+  }
+}
