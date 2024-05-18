820655da0d30d5f7dc09b6cb6ad1f8f06f8da6ea
SONG <geun2121@gmail.com>
2024-05-18 16:33:25 +0900

    #152: applyRingToggleButtonStyle()
    - RingToggleButton을 만드는 메서드를 정의하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/UIButton+RingToggleButton.swift
@@ -0,0 +1,33 @@
+//
+//  RingToggleButton.swift
+//
+//
+//  Created by SONG on 5/17/24.
+//
+
+import UIKit
+
+extension UIButton {
+  public func applyRingToggleButtonStyle() {
+    RingToggleButton.apply(to: self)
+  }
+}
+
+private enum RingToggleButton {
+  fileprivate static func apply(to button: UIButton) {
+    button.setBackgroundImage(UIImage.ringToggleOff, for: UIControl.State.normal)
+    button.setBackgroundImage(UIImage.ringToggleOn, for: UIControl.State.selected)
+    button.setBackgroundImage(UIImage.ringToggleHighlighted, for: UIControl.State.highlighted)
+    button.setBackgroundImage(
+      UIImage.ringToggleHighlighted,
+      for: [
+        UIControl.State.selected,
+        UIControl.State.highlighted
+      ]
+    )
+
+    button.addAction(
+      UIAction { [weak button] _ in button?.isSelected.toggle() },
+      for: UIControl.Event.touchUpInside)
+  }
+}
