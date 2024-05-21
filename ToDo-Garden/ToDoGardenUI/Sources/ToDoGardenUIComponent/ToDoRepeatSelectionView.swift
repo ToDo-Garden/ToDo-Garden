caf2289b24df3288b57d82722ece04b5c4fe26c2
dpfdallfm <dpfdlalfm@naver.com>
2024-05-21 12:56:46 +0900

    #158: Model 객체 생성
    - UI를 커스텀할 수도록 모델 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoRepeatSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoRepeatSelectionView.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+import ToDoGardenUIConstant
+import ToDoGardenUIResource
+
 public class ToDoRepeatSelectionView: UIView {
-  public init() {
+  let model: Model
+
+  public init(model: Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Model
+
+extension ToDoRepeatSelectionView {
+  public struct Model {
+    let title: String
+    let borderWidth: CGFloat
+    let cornerRadius: CGFloat
+    var selectedStateImage: UIImage?
+
+    public init(
+      title: String,
+      borderWidth: CGFloat,
+      cornerRadius: CGFloat,
+      selectedStateImage: UIImage? = nil
+    ) {
+      self.title = title
+      self.borderWidth = borderWidth
+      self.cornerRadius = cornerRadius
+      self.selectedStateImage = selectedStateImage
+    }
+
+    public static let onlyToday = Self(
+      title: Constant.ToDoRepeatSelectionView.StringLiteral.RepetitionLabel.onlyToday,
+      borderWidth: Constant.ToDoRepeatSelectionView.Layout.borderWidth,
+      cornerRadius: Constant.ToDoRepeatSelectionView.Layout.cornerRadius,
+      selectedStateImage: UIImage.checkmarkImage
+    )
+
+    public static let anotherDay = Self(
+      title: Constant.ToDoRepeatSelectionView.StringLiteral.RepetitionLabel.anotherDay,
+      borderWidth: Constant.ToDoRepeatSelectionView.Layout.borderWidth,
+      cornerRadius: Constant.ToDoRepeatSelectionView.Layout.cornerRadius
+    )
   }
 }
