48d99e33ee1e306bd952942efa929d208ee6595b
dpfdallfm <dpfdlalfm@naver.com>
2024-05-21 12:55:38 +0900

    #158: 객체 생성
    - 투두 반복여부를 선택할 수 있는 뷰 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoRepeatSelectionView.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoRepeatSelectionView.swift
+//
+//
+//  Created by Wood on 5/19/24.
+//
+
+import UIKit
+
+public class ToDoRepeatSelectionView: UIView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
