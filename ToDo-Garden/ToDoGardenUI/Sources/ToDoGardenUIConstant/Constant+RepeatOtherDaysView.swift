187c631bc207269804f057fff4dd985c092b1143
SONG <geun2121@gmail.com>
2024-05-28 13:22:17 +0900

    #175: Layout 관련 상수 추가

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+RepeatOtherDaysView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+RepeatOtherDaysView.swift
@@ -12,3 +12,29 @@ extension Constant.RepeatOtherDaysView {
   public enum StringLiteral {}
   public enum AboutAnimation {}
 }
+
+extension Constant.RepeatOtherDaysView.Layout {
+  public static let heightUnselected: CGFloat = 31.0
+  public static let heightSelected: CGFloat = 159.0
+  
+  public enum Title {
+    public static let topMargin: CGFloat = 13.0
+  }
+  public enum InnerStackView {
+    public static let width: CGFloat = 315.0
+    public static let height: CGFloat = 119.0
+  }
+  public enum Divider {
+    public static let width: CGFloat = 286.0
+    public static let height: CGFloat = 1.0
+  }
+  public enum DateButtonSet {
+    public static let width: CGFloat = 140.0
+    public static let trailing: CGFloat = -17.0
+  }
+  
+  public enum CommonMargin {
+    public static let narrow: CGFloat = 10.0
+    public static let broad: CGFloat = 40.0
+  }
+}
