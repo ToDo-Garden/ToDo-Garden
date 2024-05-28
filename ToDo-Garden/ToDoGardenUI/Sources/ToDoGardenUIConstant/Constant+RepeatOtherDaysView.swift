2031d45bd80c9b01727a886065c001135bbc40e7
SONG <geun2121@gmail.com>
2024-05-28 13:19:14 +0900

    #175: enum  추가

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+RepeatOtherDaysView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+RepeatOtherDaysView.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  Constant+RepeatOtherDaysView.swift
+//
 //
 //  Created by SONG on 5/28/24.
 //
 
 import Foundation
+
+extension Constant.RepeatOtherDaysView {
+  public enum Layout {}
+  public enum StringLiteral {}
+  public enum AboutAnimation {}
+}
