2d3ead517320152c6c383d52972782042df384a7
SONG <geun2121@gmail.com>
2024-05-29 17:37:17 +0900

    #171: bindViewModel() 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysView.swift
@@ -49,6 +49,79 @@ public final class RepeatOtherDaysView: ToDoRepeatSelectionView {
 // MARK: - Private functions
 extension RepeatOtherDaysView {
   private func setup() {
+    self.bindViewModel()
+  }
+  
+  private func bindViewModel() {
+    self.bindDateButtons()
+    self.bindSelectionStates()
+    self.bindVisibilityStates()
+    self.bindViewHeights()
+    self.bindMargins()
+  }
+}
+
+// MARK: - About Binding
+extension RepeatOtherDaysView {
+  private func bindDateButtons() {
+    self.viewModel.dateButton.startDate.bind { [weak self] date in
+      self?.dateButtonSet.updateStartDateButton(title: date)
+    }
+    
+    self.viewModel.dateButton.endDate.bind { [weak self] date in
+      self?.dateButtonSet.updateEndDateButton(title: date)
+    }
+  }
+  
+  private func bindSelectionStates() {
+    self.viewModel.isSelected.bind { [weak self] _ in
+      self?.updateUI()
+      self?.updateBackgroundColor()
+    }
+    
+    self.viewModel.ringToggleButton.isSelected.bind { [weak self] isSelected in
+      self?.ringToggleButton.isSelected = isSelected
+      if isSelected == true {
+        self?.dateButtonSet.isSelected = false
+      }
+      self?.updateBackgroundColor()
+    }
+    
+    self.viewModel.dateButton.isSelected.bind { [weak self] isSelected in
+      self?.dateButtonSet.isSelected = isSelected
+      if isSelected == true {
+        self?.ringToggleButton.isSelected = false
+      }
+      self?.updateBackgroundColor()
+    }
+  }
+  
+  private func bindVisibilityStates() {
+    self.viewModel.divider.isHidden.bind { [weak self] isHidden in
+      self?.divider.isHidden = isHidden
+    }
+    
+    self.viewModel.innerStackView.isHidden.bind { [weak self] isHidden in
+      self?.innerStackView.isHidden = isHidden
+    }
+  }
+  
+  private func bindViewHeights() {
+    self.viewModel.height.bind { [weak self] height in
+      self?.updateRepeatOtherDaysViewHeightConstraint(to: height)
+    }
+    
+    self.viewModel.innerStackView.height.bind { [weak self] height in
+      self?.updateInnerStackViewViewHeightConstraint(to: height)
+    }
+  }
+  
+  private func bindMargins() {
+    self.viewModel.title.topMargin.bind { [weak self] margin in
+      self?.updateTitleConstraint(to: margin)
+    }
+  }
+}
   }
 }
 
