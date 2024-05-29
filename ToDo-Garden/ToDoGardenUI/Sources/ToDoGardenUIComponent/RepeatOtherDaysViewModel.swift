5f862da002903bb84126a6d3a89fef523148d8aa
SONG <geun2121@gmail.com>
2024-05-29 16:45:54 +0900

    #171: init() 정의
    -  프로퍼티 초기화 로직을 작성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysViewModel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysViewModel.swift
@@ -10,7 +10,67 @@ import Foundation
 import ToDoGardenUIConstant
 
 public final class RepeatOtherDaysViewModel {
+  private(set) var dateButton: DateButtonState
+  private(set) var ringToggleButton: RingToggleButtonState
+  private(set) var divider: DividerState
+  private(set) var innerStackView: InnerStackViewState
+  private(set) var title: TitleState
   
+  private(set) var isSelected: Observable<Bool>
+  private(set) var height: Observable<CGFloat>
+  
+  init(
+    startDate: String?,
+    endDate: String?
+  ) {
+    let today = RepeatOtherDaysViewModel.currentDateString()
+    
+    self.ringToggleButton = RepeatOtherDaysViewModel.initializeRingToggleButton(
+      startDate: startDate,
+      endDate: endDate
+    )
+    
+    self.dateButton = RepeatOtherDaysViewModel.initializeDateButton(
+      startDate: startDate,
+      endDate: endDate,
+      today: today
+    )
+    
+    self.isSelected = Observable(false)
+    self.divider = DividerState(isHidden: Observable(true))
+    self.innerStackView = InnerStackViewState(
+      isHidden: Observable(true),
+      height: Observable(CGFloat.zero)
+    )
+    self.height = Observable(Constant.RepeatOtherDaysView.Layout.heightUnselected)
+    self.title = TitleState(topMargin: Observable(Constant.ToDoRepeatSelectionView.Layout.RepetitionLabel.topMargin))
+  }
+}
+
+extension RepeatOtherDaysViewModel {
+  
+}
+
+extension RepeatOtherDaysViewModel {
+  private static func currentDateString() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy.MM.dd"
+    return dateFormatter.string(from: Date())
+  }
+  
+  private static func initializeRingToggleButton(startDate: String?, endDate: String?) -> RingToggleButtonState {
+    let isDateUndecided = (startDate == nil) || (endDate == nil)
+    return RingToggleButtonState(isSelected: Observable(isDateUndecided))
+  }
+  
+  private static func initializeDateButton(startDate: String?, endDate: String?, today: String) -> DateButtonState {
+    let isDateUndecided = (startDate == nil) || (endDate == nil)
+    return DateButtonState(
+      startDate: Observable(startDate ?? today),
+      endDate: Observable(endDate ?? today),
+      isSelected: Observable(!isDateUndecided)
+    )
+  }
 }
 
 extension RepeatOtherDaysViewModel {
