6e545efc63d39912be48c24b1d19701234c80828
SONG <geun2121@gmail.com>
2024-05-29 16:37:12 +0900

    #171: Observable 정의
    - 뷰 바인딩을 위한 Observable 클래스를 정의했습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysViewModel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysViewModel.swift
@@ -12,3 +12,21 @@ import ToDoGardenUIConstant
 public final class RepeatOtherDaysViewModel {
   
 }
+class Observable<T> {
+  var value: T {
+    didSet {
+      listener?(value)
+    }
+  }
+  
+  private var listener: ((T) -> Void)?
+  
+  init(_ value: T) {
+    self.value = value
+  }
+  
+  func bind(_ listener: @escaping (T) -> Void) {
+    self.listener = listener
+    listener(value)
+  }
+}
