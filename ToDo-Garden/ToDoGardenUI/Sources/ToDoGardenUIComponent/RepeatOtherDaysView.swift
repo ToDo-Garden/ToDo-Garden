203868a70e59c6ad93e23942ce6b747a5ac9a7bc
SONG <geun2121@gmail.com>
2024-05-29 17:25:09 +0900

    #171: 초기화 및 스켈레톤 코드 작성
    - 프로퍼티 초기화
    - setup() 껍데기 작성

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/RepeatOtherDaysView.swift
@@ -5,10 +5,32 @@
 //  Created by SONG on 5/28/24.
 //
 
-import Foundation
+import UIKit
 
 import ToDoGardenUIConstant
 
 public final class RepeatOtherDaysView: ToDoRepeatSelectionView {
+  public let ringToggleButton: UIButton
+  public let dateButtonSet: DateButtonSet
+  private let divider: UIView
+  private let innerStackView: UIStackView
   
+  private var heightConstraints: [NSLayoutConstraint]
+  private var viewModel: RepeatOtherDaysViewModel
+  
+  public init(startDate: String?, endDate: String?) {
+    self.viewModel = RepeatOtherDaysViewModel(startDate: startDate, endDate: endDate)
+    self.heightConstraints = []
+    self.ringToggleButton = UIButton()
+    self.innerStackView = UIStackView()
+    self.divider = UIView()
+    self.dateButtonSet = DateButtonSet()
+    super.init(model: ToDoRepeatSelectionView.Model.anotherDay)
+    self.setup()
+  }
+}
+
+extension RepeatOtherDaysView {
+  private func setup() {
+  }
 }
