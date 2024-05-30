f5d94dcf77d73f8abf9363a4420d1ece503bed71
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 02:17:40 +0900

    #192: 레이아웃 관련 상수 추가
    - 캘린더에 필요한 레이아웃 관련 상수들을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+CalendarView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+CalendarView.swift
@@ -8,4 +8,38 @@
 import Foundation
 
 extension Constant.CalendarView {
+  public enum Layout {}
+}
+
+extension Constant.CalendarView.Layout {
+  public enum MonthLabel {
+    public static let topMargin: CGFloat = 12
+    public static let leadingMargin: CGFloat = 12
+  }
+
+  public enum BackButton {
+    public static let trailingMargin: CGFloat = 12
+    public static let widthMargin: CGFloat = 24
+    public static let heightMargin: CGFloat = 24
+  }
+
+  public enum ForwardButton {
+    public static let trailingMargin: CGFloat = 6
+    public static let widthMargin: CGFloat = 24
+    public static let heightMargin: CGFloat = 24
+  }
+
+  public enum StackView {
+    public static let topMargin: CGFloat = 26
+    public static let leadingMargin: CGFloat = 22
+    public static let trailingMargin: CGFloat = 19
+  }
+
+  public enum CollectionView {
+    public static let topMargin: CGFloat = 15
+    public static let leadingMargin: CGFloat = 13
+    public static let trailingMargin: CGFloat = 10
+    public static let bottomMargin: CGFloat = 11
+    public static let defaultHeight: CGFloat = 98
+  }
 }
