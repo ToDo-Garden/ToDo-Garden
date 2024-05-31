158301211c6efb717acbeef092bc1bb7f32ac368
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 20:27:37 +0900

    #198: Model 객체 생성
    - 뷰를 커스텀할 수 있도록 Model 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarView.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 public final class CalendarView: UIView {
-  public init() {
+  private var model: Model
+
+  public init(model: Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, deprecated)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension CalendarView {
+  public struct Model {
+    let borderWidth: CGFloat
+    let cornerRadius: CGFloat
+    let collectionView: Model.CollectionView
+
+    public static let primary = Self(
+      borderWidth: Constant.CalendarView.Layout.Primary.borderWidth,
+      cornerRadius: Constant.CalendarView.Layout.Primary.cornerRadius,
+      collectionView: CollectionView(
+        itemSize: Constant.CalendarView.Layout.Primary.itemSize,
+        itemSpacing: Constant.CalendarView.Layout.Primary.itemSpacing,
+        lineSpacing: Constant.CalendarView.Layout.Primary.lineSpacing
+      )
+    )
+  }
+}
+
+extension CalendarView.Model {
+  public struct CollectionView {
+    let itemSize: CGSize
+    let itemSpacing: CGFloat
+    let lineSpacing: CGFloat
   }
 }
