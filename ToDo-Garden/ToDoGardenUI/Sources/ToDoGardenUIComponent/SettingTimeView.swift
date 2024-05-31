ab77256f9ced8a7064d066545a49a5d0fb419605
SONG <geun2121@gmail.com>
2024-05-31 12:54:23 +0900

    #191: 초기화 및 스켈레톤코드 작성

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SettingTimeView.swift
@@ -0,0 +1,47 @@
+//
+//  SettingTimeView.swift
+//
+//
+//  Created by SONG on 5/30/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+
+public class SettingTimeView: UIView {
+  private var configuration: Configuration
+  private let timepicker: ToDoGardenTimePicker
+
+  public init(with button: UIButton, for configuration: Configuration) {
+    self.configuration = configuration
+    self.timepicker = ToDoGardenTimePicker(configuration: self.configuration.dataStore.timePicker)
+    super.init(frame: CGRect.zero)
+    self.build(with: button)
+  }
+  
+  @available(*, unavailable)
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension SettingTimeView {
+  private func build(with button: UIButton) {
+    
+  }
+}
+
+extension SettingTimeView {
+  public struct Configuration {
+    let dataStore: Constant.SettingTimeView.DataStore
+    
+    init(dataStore: Constant.SettingTimeView.DataStore) {
+      self.dataStore = dataStore
+    }
+  }
+}
+
+extension SettingTimeView.Configuration {
+
+}
