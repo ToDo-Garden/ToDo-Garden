c17693d344eca24c458ca4e65b81e548fa68e063
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 21:56:27 +0900

    #198: Cell에 투두 존재여부 표시 View 추가
    - 셀에 해당하는 날짜에 투두가 있는 경우 있음 표시를 하는 View를 추가하였습니다.
    - 오토 레이아웃을 설정하는 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarCollectionViewCell.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarCollectionViewCell.swift
@@ -10,13 +10,61 @@ import UIKit
 import ToDoGardenUIConstant
 
 final class CalendarCollectionViewCell: UICollectionViewCell {
+  private var toDoExistenceView: UIView
 
   override init(frame: CGRect) {
+    self.toDoExistenceView = UIView()
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+
+extension CalendarCollectionViewCell {
+  private func setup() {
+    self.setupToDoExistenceView()
+    self.addSubviews()
+    self.setupSubviewsLayout()
+  }
+
+  private func setupToDoExistenceView() {
+    self.toDoExistenceView.backgroundColor = UIColor.toDoGardenGray3
+    let cornerRadius = Constant.CalendarView.Layout.CollectionViewCell.ToDoExistenceView.cornerRadius
+    self.toDoExistenceView.layer.cornerRadius = cornerRadius
+    self.toDoExistenceView.isHidden = true
+  }
+}
+
+// MARK: Set AutoLayout
+
+extension CalendarCollectionViewCell {
+  private func addSubviews() {
+    self.addSubview(self.toDoExistenceView)
+  }
+
+  private func setupSubviewsLayout() {
+    self.setupToDoExistenceViewLayout()
+  }
+
+  private func setupToDoExistenceViewLayout() {
+    self.toDoExistenceView.usingAutolayout()
+
+    NSLayoutConstraint.activate(
+      [
+        self.toDoExistenceView.leadingAnchor.constraint(equalTo: self.dayLabel.leadingAnchor),
+        self.toDoExistenceView.bottomAnchor.constraint(equalTo: self.dayLabel.topAnchor),
+        self.toDoExistenceView.widthAnchor.constraint(
+          equalToConstant: Constant.CalendarView.Layout.CollectionViewCell.ToDoExistenceView.widthMargin
+        ),
+        self.toDoExistenceView.heightAnchor.constraint(
+          equalToConstant: Constant.CalendarView.Layout.CollectionViewCell.ToDoExistenceView.heightMargin
+        )
+      ]
+    )
   }
 }
