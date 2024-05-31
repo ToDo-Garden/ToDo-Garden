c9dd41f21513af04962c7f68b1b0817d2cdbe412
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 12:40:14 +0900

    #194: 현재 달 스냅샷 적용 함수 추가
    - 캘린더에서 최초에 보여줄 데이터들을 로드하여 스냅샷을 적용하는 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarViewDelegate.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarViewDelegate.swift
@@ -12,11 +12,16 @@ import ToDoGardenUIConstant
 final class CalendarViewDelegate: NSObject {
   private var collectionView: UICollectionView
   private var collectionViewDataSource: UICollectionViewDiffableDataSource<CalendarSection, CalendarItem>!
+  private var calendarDataGenerator: CalendarDataGeneratable
   private var dateFormatter: DateFormatter
 
   init(collectionView: UICollectionView) {
     self.collectionView = collectionView
     self.dateFormatter = DateFormatter()
+    self.calendarDataGenerator = CalendarDataGenerator(calendar: Calendar.localeUpdated)
+    super.init()
+    self.setupDateFormatter()
+    self.loadInitialMonthSnapshot()
   }
 }
 
@@ -38,5 +43,27 @@ extension CalendarViewDelegate {
   private func setupCollectionViewDataSource() {
     self.collectionViewDataSource = self.makeDiffableDataSource(self.collectionView, with: self.dateFormatter)
     self.collectionView.dataSource = self.collectionViewDataSource
+  }
+
+  private func loadInitialMonthSnapshot() {
+    var snapshot = self.collectionViewDataSource.snapshot()
+    let currentDate = Date()
+    let dateRange = (-3...3)
+    dateRange.forEach { (addValue: Int) in
+      guard let monthData = try? self.calendarDataGenerator.fetchMonthData(from: currentDate, add: addValue)
+      else { return }
+
+      let section = CalendarSection(firstDay: monthData.firstDayOfMonth)
+      snapshot.appendSections([section])
+
+      let newItems = monthData.dates.map { (day: MonthData.Day) in
+        let date = day.date
+        let isThisMonth = day.isThisMonth
+        return CalendarItem(date: date, isThisMonth: isThisMonth)
+      }
+      snapshot.appendItems(newItems, toSection: section)
+    }
+
+    self.collectionViewDataSource.apply(snapshot)
   }
 }
