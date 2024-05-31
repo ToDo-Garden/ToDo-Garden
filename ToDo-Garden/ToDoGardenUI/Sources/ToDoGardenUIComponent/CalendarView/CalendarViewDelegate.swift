481bf079d153ea3527d060817563742e3dcb9407
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 12:40:37 +0900

    #194: 캘린더 스크롤 함수 추가
    - CalendarView 캘린더 스크롤시 호출할 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarViewDelegate.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarViewDelegate.swift
@@ -13,15 +13,34 @@ final class CalendarViewDelegate: NSObject {
   private var collectionView: UICollectionView
   private var collectionViewDataSource: UICollectionViewDiffableDataSource<CalendarSection, CalendarItem>!
   private var calendarDataGenerator: CalendarDataGeneratable
+  private var currentIndexPath: IndexPath
+  private var collectionViewModel: CalendarView.Model.CollectionView
   private var dateFormatter: DateFormatter
 
   init(collectionView: UICollectionView) {
     self.collectionView = collectionView
-    self.dateFormatter = DateFormatter()
     self.calendarDataGenerator = CalendarDataGenerator(calendar: Calendar.localeUpdated)
+    self.currentIndexPath = IndexPath(item: 0, section: 3)
+    self.dateFormatter = DateFormatter()
     super.init()
     self.setupDateFormatter()
     self.loadInitialMonthSnapshot()
+  }
+
+  func scrollCalendar(to scrollDirection: CalendarScrollDirection, animated: Bool) {
+    self.currentIndexPath.section += scrollDirection.rawValue
+    self.collectionView.scrollToItem(
+      at: self.currentIndexPath,
+      at: UICollectionView.ScrollPosition.left,
+      animated: animated
+    )
+  }
+
+  func getCollectionViewHeight() -> CGFloat {
+    let snapshot = self.collectionViewDataSource.snapshot()
+    let currentSection = snapshot.sectionIdentifiers[self.currentIndexPath.section]
+    let itemCount = snapshot.itemIdentifiers(inSection: currentSection).count
+    return self.calculateHeight(items: itemCount)
   }
 }
 
@@ -34,6 +53,16 @@ extension CalendarViewDelegate {
       self.dateFormatter.locale = Locale.autoupdatingCurrent
     }
     self.dateFormatter.dateFormat = Constant.CalendarView.StringLiteral.dateFormat
+  }
+
+  private func calculateHeight(items count: Int) -> CGFloat {
+    let numberOfRows = CGFloat(count / 7)
+    let totalItemHeight = self.collectionViewModel.itemSize.height * numberOfRows
+    let totalInsets = self.collectionViewModel.lineSpacing * (numberOfRows - 1)
+    let collectionViewHeight = totalItemHeight + totalInsets
+    let defaultHeight: CGFloat = Constant.CalendarView.Layout.CollectionView.defaultHeight
+
+    return defaultHeight + collectionViewHeight
   }
 }
 
@@ -66,4 +95,10 @@ extension CalendarViewDelegate {
 
     self.collectionViewDataSource.apply(snapshot)
   }
+}
+
+enum CalendarScrollDirection: Int {
+  case left  = -1
+  case current = 0
+  case right = 1
 }
