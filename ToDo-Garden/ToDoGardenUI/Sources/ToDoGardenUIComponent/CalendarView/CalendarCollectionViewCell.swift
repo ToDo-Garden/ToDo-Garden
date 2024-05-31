3485f665e5253ea780cdae138e94f6d8e1727bb2
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 21:53:34 +0900

    #198: CalendarCollectionViewCell 추가
    - 캘린더에서 사용하는 Cell 객체를 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CalendarCollectionViewCell.swift
+//
+//
+//  Created by Wood on 5/31/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+
+final class CalendarCollectionViewCell: UICollectionViewCell {
+
+  override init(frame: CGRect) {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
