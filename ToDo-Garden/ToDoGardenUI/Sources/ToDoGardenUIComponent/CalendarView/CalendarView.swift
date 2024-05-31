27e7a390bc2b435026ecb4d2c2dbc10bcafebaf0
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 20:26:56 +0900

    #198: CalendarView 객체 생성
    - CalendarView 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarView.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarView.swift
+//
+//
+//  Created by Wood on 5/31/24.
+//
+
+import UIKit
+
+public final class CalendarView: UIView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, deprecated)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
