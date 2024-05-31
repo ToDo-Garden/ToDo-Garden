b15a895bdf552b60d8ffece7d35d7b8020b12080
dpfdallfm <dpfdlalfm@naver.com>
2024-05-31 12:39:11 +0900

    #194: CalendarViewDelegate 파일 생성
    - 캘린더 스크롤 및 날짜 표시에 필요한 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/CalendarView/CalendarViewDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarViewDelegate.swift
+//
+//
+//  Created by Wood on 5/31/24.
+//
+
+import UIKit
+
+import ToDoGardenUIConstant
+
+final class CalendarViewDelegate: NSObject {
+  private var collectionView: UICollectionView
+
+  init(collectionView: UICollectionView) {
+    self.collectionView = collectionView
+  }
+}
