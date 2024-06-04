ae6efbcf029db4397202655f5097ef8244559473
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-04 20:56:08 +0900

    #202: EditMode 정의
    - 투두를 수정할 수 있는 모드를 정의하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/EditToDoSegmentedControl.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/EditToDoSegmentedControl.swift
@@ -5,4 +5,11 @@
 //  Created by Noah on 6/3/24.
 //
 
-import Foundation
+import UIKit
+
+public final class EditToDoSegmentedControl: UISegmentedControl {
+  public enum EditMode: Int {
+    case notification = 0
+    case todo = 1
+  }
+}
