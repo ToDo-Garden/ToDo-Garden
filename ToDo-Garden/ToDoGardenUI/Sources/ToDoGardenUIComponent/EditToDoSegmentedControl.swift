50bf908edbe2311fe43697c8130d361ce394c4e7
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-04 20:58:02 +0900

    #202: SegmentedControl 값에 따른 EditMode 설정

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/EditToDoSegmentedControl.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/EditToDoSegmentedControl.swift
@@ -12,4 +12,46 @@ public final class EditToDoSegmentedControl: UISegmentedControl {
     case notification = 0
     case todo = 1
   }
+  
+  public var editMode: EditMode?
+  
+  public init() {
+    super.init(frame: CGRect.zero)
+    self.setup()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Setup
+
+extension EditToDoSegmentedControl {
+  private func setup() {
+    self.setupAction()
+  }
+}
+
+// MARK: - Setup action
+
+extension EditToDoSegmentedControl {
+  private func setupAction() {
+    self.addTarget(
+      self,
+      action: #selector(self.editModeDidChange(_:)),
+      for: UIControl.Event.valueChanged
+    )
+  }
+  
+  @objc private func editModeDidChange(_ sender: UISegmentedControl) {
+    guard let selectedMode = EditMode(rawValue: sender.selectedSegmentIndex)
+    else {
+      self.editMode = nil
+      return
+    }
+    
+    self.editMode = selectedMode
+  }
 }
