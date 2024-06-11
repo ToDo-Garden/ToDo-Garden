2ae382d82afc204da4865b613d7940c99d0f2fab
SONG <geun2121@gmail.com>
2024-06-11 14:24:10 +0900

    #220: 의존성 추가 및 들여쓰기 수정
    - 내용이 바뀐것은 ToDoGardenUIAPI의 dependencies입니다.

--- ToDo-Garden/ToDoGardenUI/Package.swift
+++ ToDo-Garden/ToDoGardenUI/Package.swift
@@ -4,46 +4,51 @@
 import PackageDescription
 
 let package = Package(
-	name: "ToDoGardenUI",
-	platforms: [.iOS(.v15)],
-	products: [
-		.library(
-			name: "ToDoGardenUIAPI",
-			targets: ["ToDoGardenUIAPI"]
-		),
-		.library(
-			name: "ToDoGardenUIComponent",
-			targets: ["ToDoGardenUIComponent"]
-		),
-		.library(
-			name: "ToDoGardenUIResource",
-			targets: ["ToDoGardenUIResource"]
-		),
+  name: "ToDoGardenUI",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "ToDoGardenUIAPI",
+      targets: ["ToDoGardenUIAPI"]
+    ),
+    .library(
+      name: "ToDoGardenUIComponent",
+      targets: ["ToDoGardenUIComponent"]
+    ),
+    .library(
+      name: "ToDoGardenUIResource",
+      targets: ["ToDoGardenUIResource"]
+    ),
     .library(
       name: "ToDoGardenUIConstant",
       targets: ["ToDoGardenUIConstant"]
     )
-	],
-	targets: [
-		.target(name: "ToDoGardenUIAPI"),
-		.target(
-			name: "ToDoGardenUIComponent",
-			dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "ToDoGardenUIAPI",
+      dependencies: [
+        "ToDoGardenUIConstant"
+      ]
+    ),
+    .target(
+      name: "ToDoGardenUIComponent",
+      dependencies: [
         "ToDoGardenUIResource",
         "ToDoGardenUIConstant",
         "CombineExtension",
         "FoundationExtension",
         "ToDoGardenUIAPI"
       ]
-		),
-		.target(
-			name: "ToDoGardenUIResource",
-			resources: [
-				.process("Fonts")
-			]
-		),
+    ),
+    .target(
+      name: "ToDoGardenUIResource",
+      resources: [
+        .process("Fonts")
+      ]
+    ),
     .target(name: "ToDoGardenUIConstant"),
     .target(name: "CombineExtension"),
     .target(name: "FoundationExtension")
-	]
+  ]
 )
