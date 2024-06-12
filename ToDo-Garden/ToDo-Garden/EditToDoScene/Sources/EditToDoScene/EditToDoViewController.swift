62c57db98c0c72549f42efe3ed65e4ecf7024206
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-12 20:03:17 +0900

    #228: EditToDoViewController Constant 추가
    - EditToDoViewController에 필요한 Constant들을 선언하는 타입을 추가하였습니다.
    - Constant를 EditToDoViewController 확장부에 선언하기 위해 EditToDoViewController를 생성하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoViewController.swift
@@ -0,0 +1,10 @@
+//
+//  EditToDoViewController.swift
+//  
+//
+//  Created by Wood on 6/12/24.
+//  Copyright (c) 2024 ToDoGarden. All rights reserved.
+
+import UIKit
+
+final class EditToDoViewController: UIViewController {}
