922a16b6b2422cf120a332d3f6fb5949a41d53a4
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-12 20:17:10 +0900

    #228: 수정 완료 버튼 레이아웃 추가
    - 수정 완료버튼에 필요한 레이아웃 상수를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoViewController+Constant.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoViewController+Constant.swift
@@ -1,3 +1,17 @@
+import Foundation
+
 extension EditToDoViewController {
   enum Constant {}
+}
+
+extension EditToDoViewController.Constant {
+  enum Layout {}
+}
+
+extension EditToDoViewController.Constant.Layout {
+  enum CompleteEditingButton {}
+}
+
+extension EditToDoViewController.Constant.Layout.CompleteEditingButton {
+  static let bottomMargin: CGFloat = 13
 }
