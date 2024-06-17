1ddc7d102b996c42a4a3fcaab657ea38163e72ee
GangWoon <gangwoon2018@gmail.com>
2024-06-17 14:58:50 +0900

    #229: Add Timer ViewController

--- ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneViewController.swift
+++ ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneViewController.swift
@@ -2,12 +2,18 @@ import UIKit
 
 import TimerSceneApi
 import TimerSceneEntity
+import ToDoGardenUIComponent
 
 protocol TimerSceneDisplayLogic: AnyObject {
   func displaySomething(viewModel: TimerScene.Something.ViewModel)
 }
 
 public final class TimerSceneViewController: UIViewController, TimerSceneViewControllable {
+  private var circularProgressView: CircularProgressView!
+  private var targetLabel: UILabel!
+  private var timeLabel: UILabel!
+  private var controlButton: UIButton!
+  
   // MARK: - VIP Properties
   var interactor: TimerSceneBusinessLogic?
   var router: (TimerSceneRoutingLogic & TimerSceneDataPassing)?
@@ -26,6 +32,76 @@ public final class TimerSceneViewController: UIViewController, TimerSceneViewCon
   public override func viewDidLoad() {
     super.viewDidLoad()
     self.doSomething()
+    build()
+  }
+  
+  private func build() {
+    let vStack = UIStackView()
+    vStack.axis = .vertical
+    vStack.addSpacing(94)
+    
+    circularProgressView = buildCircularProgressView()
+    layoutCircularProgressView(vStack)
+    vStack.addSpacing(55)
+    
+    targetLabel = buildTargetLabel()
+    vStack.addArrangedSubview(targetLabel)
+    vStack.addSpacing(9)
+    
+    timeLabel = buildTimeLabel()
+    vStack.addArrangedSubview(timeLabel)
+    vStack.addSpacing(21)
+    
+    controlButton = buildSetTimerButton()
+    vStack.addArrangedSubview(controlButton)
+    
+    vStack.addSpacing()
+    view.addSubview(vStack)
+    vStack.equalToParent()
+  }
+  
+  private func buildCircularProgressView() -> CircularProgressView {
+    let alarm = CircularProgressView(
+      progressColor: .toDoGardenRed,
+      backgroundColor: .toDoGardenPink,
+      lineWidth: 9
+    )
+    
+    return alarm
+  }
+  
+  private func layoutCircularProgressView(_ container: UIStackView) {
+    let stack = UIStackView(
+      arrangedSubviews: [
+        UIView(),
+        circularProgressView,
+        UIView()
+      ]
+    )
+    stack.distribution = .equalSpacing
+    circularProgressView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    circularProgressView.widthAnchor.constraint(equalToConstant: 236).isActive = true
+    circularProgressView.heightAnchor.constraint(equalToConstant: 236).isActive = true
+    circularProgressView.startAnimation(duration: 3, from: 0, to: 1)
+    container.addArrangedSubview(stack)
+  }
+  
+  private func buildTargetLabel() -> UILabel {
+    let label = UILabel()
+    label.text = "집중시간 40분"
+    return label
+  }
+  
+  private func buildTimeLabel() -> UILabel {
+    let timeLabel = UILabel()
+    timeLabel.text = "08:26"
+    return timeLabel
+  }
+  
+  private func buildSetTimerButton() -> UIButton {
+    let button = UIButton()
+    button.applyRingToggleButtonStyle()
+    return button
   }
 }
 
@@ -43,3 +119,12 @@ extension TimerSceneViewController {
     self.interactor?.doSomething(request: request)
   }
 }
+
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview {
+  let viewController = TimerSceneViewController()
+  
+  return viewController
+}
+#endif
