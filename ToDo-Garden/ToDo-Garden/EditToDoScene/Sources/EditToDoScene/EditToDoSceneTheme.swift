87c4dff15888ef0faf4463de474325ec5ab2efd3
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-19 12:36:59 +0900

    #236: Constant Namespace Depth 수정
    - 레이아웃 및 StinrgLiteral 관련 상수들을 한눈에 보기 쉽게 Depth를 변경하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoSceneTheme.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoSceneTheme.swift
@@ -10,77 +10,59 @@ extension EditToDoSceneTheme.Constant {
 }
 
 extension EditToDoSceneTheme.Constant.Layout {
-  enum CompleteEditingButton {}
-  enum EditModeSegmentedControl {}
-  enum ToDoScheduleView {}
-}
+  enum CompleteEditingButton {
+    static let bottomMargin: CGFloat = 13
+  }
 
-extension EditToDoSceneTheme.Constant.Layout.CompleteEditingButton {
-  static let bottomMargin: CGFloat = 13
-}
+  enum EditModeSegmentedControl {
+    static let topMargin: CGFloat = 10
+    static let leadingMargin: CGFloat = 12
+    static let trailingMargin: CGFloat = 13
+    static let height: CGFloat = 49
+  }
 
-extension EditToDoSceneTheme.Constant.Layout.EditModeSegmentedControl {
-  static let topMargin: CGFloat = 10
-  static let leadingMargin: CGFloat = 12
-  static let trailingMargin: CGFloat = 13
-  static let height: CGFloat = 49
-}
+  enum ToDoScheduleView {
+    enum AlarmTimeSettingView {
+      static let topMargin: CGFloat = 7
+      static let height: CGFloat = 53
+    }
 
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView {
-  static let topMargin: CGFloat = 40
-  static let leadingMargin: CGFloat = 30
-  static let traillingMargin: CGFloat = 30
-  static let bottomMargin: CGFloat = 150
-}
+    enum RepetitionLabel {
+      static let topMargin: CGFloat = 50
+      static let leadingMargin: CGFloat = 3
+    }
 
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView {
-  enum AlarmTimeSettingView {}
-  enum RepetitionLabel {}
-  enum RepeatOnlyTodayButton {}
-  enum RepeatOtherDaysView {}
-}
+    enum RepeatOnlyTodayButton {
+      static let topMargin: CGFloat = 10
+    }
 
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView.AlarmTimeSettingView {
-  static let topMargin: CGFloat = 7
-  static let height: CGFloat = 53
-}
+    enum RepeatOtherDaysView {
+      static let topMargin: CGFloat = 11
+    }
 
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView.RepetitionLabel {
-  static let topMargin: CGFloat = 50
-  static let leadingMargin: CGFloat = 3
-}
-
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView.RepeatOnlyTodayButton {
-  static let topMargin: CGFloat = 10
-}
-
-extension EditToDoSceneTheme.Constant.Layout.ToDoScheduleView.RepeatOtherDaysView {
-  static let topMargin: CGFloat = 11
+    static let topMargin: CGFloat = 40
+    static let leadingMargin: CGFloat = 30
+    static let traillingMargin: CGFloat = 30
+    static let bottomMargin: CGFloat = 150
+  }
 }
 
 extension EditToDoSceneTheme.Constant.StringLiteral {
-  enum CompleteEditingButton {}
-  enum EditToDoViewController {}
-  enum ToDoScheduleView {}
-}
+  enum CompleteEditingButton {
+    static let title = "ToDo 편집"
+  }
 
-extension EditToDoSceneTheme.Constant.StringLiteral.CompleteEditingButton {
-  static let title = "완료"
-}
+  enum EditToDoViewController {
+    static let title = "완료"
+  }
 
-extension EditToDoSceneTheme.Constant.StringLiteral.EditToDoViewController {
-  static let title = "ToDo 편집"
-}
+  enum ToDoScheduleView {
+    enum AlarmLabel {
+      static let text = "알림"
+    }
 
-extension EditToDoSceneTheme.Constant.StringLiteral.ToDoScheduleView {
-  enum AlarmLabel {}
-  enum RepetitionLabel {}
-}
-
-extension EditToDoSceneTheme.Constant.StringLiteral.ToDoScheduleView.AlarmLabel {
-  static let text = "알림"
-}
-
-extension EditToDoSceneTheme.Constant.StringLiteral.ToDoScheduleView.RepetitionLabel {
-  static let text = "반복"
+    enum RepetitionLabel {
+      static let text = "반복"
+    }
+  }
 }
