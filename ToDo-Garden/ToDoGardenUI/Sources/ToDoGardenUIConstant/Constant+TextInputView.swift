0f06dd993327a0bacad26cac29a2ba156cc3ad06
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-22 22:03:51 +0900

    #240: Placeholder Label 레이아웃 상수 추가
    - PlaceholderLabel에 필요한 레이아웃 상수를 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+TextInputView.swift
@@ -0,0 +1,21 @@
+//
+//  Constant+TextInputView.swift
+//
+//
+//  Created by Wood on 6/22/24.
+//
+
+import Foundation
+
+extension Constant.TextInputView {
+  public enum Layout {}
+}
+
+extension Constant.TextInputView.Layout {
+  public enum PlaceholderLabel {}
+}
+
+extension Constant.TextInputView.Layout.PlaceholderLabel {
+  public static let defaultHeight: CGFloat = 18
+  public static let bottomMargin: CGFloat = 2
+}
