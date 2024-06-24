f87d8b0253a59c76ad6b9ba2ddc78c9dedccc856
SONG <geun2121@gmail.com>
2024-06-24 17:43:23 +0900

    #247: 인터페이스 추가
    - ManageGroupTableViewCellAPI 추가

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIAPI/ManageGroupTableViewCellAPI.swift
@@ -0,0 +1,34 @@
+//
+//  ManageGroupTableViewCellAPI.swift
+//
+//
+//  Created by SONG on 6/24/24.
+//
+
+import UIKit.UIColor
+
+public protocol ManageGroupTableViewCellAPI {
+  func startAnimation()
+  func enterEditingMode()
+  func leaveEditingMode()
+  
+  func applyModelPrimary(
+    id: String,
+    groupName: String,
+    progressColor: UIColor,
+    progressRate: Float
+  )
+  
+  func applyModelSecondary(
+    id: String,
+    groupName: String,
+    progressColor: UIColor,
+    progressRate: Float
+  )
+  
+  func update(color: UIColor?, progressRate: Float?, groupName: String?)
+
+  func setupRightButtonAction(handler: @escaping (UIColor, String) -> Void)
+  
+  func getIdentifier() -> String
+}
