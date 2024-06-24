e86aa967fae42344ae8e84260439e007dada7935
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-24 12:44:53 +0900

    #242: TextInputView 객체 파일 생성
    - TextInputView 객체를 선언한 파일을 생성하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TextInputView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+import ToDoGardenUIConstant
+
+public final class TextInputView: UIView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
