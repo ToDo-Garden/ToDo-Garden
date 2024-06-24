e6a59d059363a2bce65685a485087218891e7d6f
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-06-24 12:48:32 +0900

    #242: Model 생성
    - TextInputView 생성시 커스텀 할 수 있도록 Model 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TextInputView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TextInputView.swift
@@ -3,12 +3,33 @@ import UIKit
 import ToDoGardenUIConstant
 
 public final class TextInputView: UIView {
-  public init() {
+  private let model: Model
+
+  public init(model: Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension TextInputView {
+  public struct Model {
+    let inputText: String
+    let shrinkScale: CGFloat
+
+    public init(inputText: String, shrinkScale: CGFloat = 0.8) {
+      self.inputText = inputText
+      self.shrinkScale = shrinkScale
+    }
+
+    public static let toDoName = Self(inputText: "할 일")
+    public static let groupName = Self(inputText: "그룹명")
+    public static let userNickname = Self(inputText: "닉네임")
+    public static let userId = Self(inputText: "아이디")
+    public static let userDescription = Self(inputText: "소개")
   }
 }
