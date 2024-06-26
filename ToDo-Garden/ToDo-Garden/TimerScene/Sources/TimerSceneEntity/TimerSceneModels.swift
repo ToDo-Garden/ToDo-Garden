5b37059250d4e178cf1ae740e500b8aa4303c536
GangWoon <gangwoon2018@gmail.com>
2024-06-26 15:19:26 +0900

    #229: Add CircularProgressRange

--- ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerSceneEntity/TimerSceneModels.swift
+++ ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerSceneEntity/TimerSceneModels.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 public enum TimerScene {
+  public enum CircularProgressRange {
+    case oneThird
+    case twoThirds
+    case whole
+    
+    public init(_ percent: Double) {
+      switch percent {
+      case 0.0..<0.3:
+        self = .oneThird
+      case 0.3..<0.6:
+        self = .twoThirds
+      default:
+        self = .whole
+      }
+    }
+  }
+  
   public enum Something {
     public struct Request {
       public init() { }
