e92511eb9ebfb271f298e3e8c020f0da1cd820f9
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-26 14:38:21 +0900

    #260: TDUtility 패키지 추가

---
+++ ToDo-Garden/TDUtility/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "TDUtility",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "TDUtility",
+      targets: ["TDUtility"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "TDUtility",
+    ),
+)
