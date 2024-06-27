f0d50f3646d396e7b42ef12ac6279860558a4161
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-27 19:40:21 +0900

    #213: GardenView Tag 추가
    - Test report에서 분류를 쉽게하기 위해 Tag를 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Tests/GardenViewTests/PomodoroRecordCellItemTests.swift
@@ -0,0 +1,15 @@
+//
+//  PomodoroRecordCellItemTests.swift
+//
+//
+//  Created by Noah on 6/27/24.
+//
+
+@testable import ToDoGardenUIComponent
+
+import Foundation
+import Testing
+
+extension Tag {
+  @Tag static let gardenView: Self
+}
