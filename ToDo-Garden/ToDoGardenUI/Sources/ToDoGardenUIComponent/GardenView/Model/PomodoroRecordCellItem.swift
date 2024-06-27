37904c717176b9bbd8a386f816f5d73a63b1ab8e
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-27 16:26:36 +0900

    #213: groupedPomodoroRecordCellItem 함수 구현
    - 일주일 단위로 그룹화된 PomodoroRecordCellItem을 반환하는 메서드를 구현하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Model/PomodoroRecordCellItem.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Model/PomodoroRecordCellItem.swift
@@ -21,4 +21,41 @@ struct PomodoroRecordCellItem: Hashable {
   func hash(into hasher: inout Hasher) {
     hasher.combine(self.id)
   }
+  
+  /// 그룹화된 `PomodoroRecordCellItem` 배열을 반환합니다.
+  /// - Parameters:
+  ///   - pomodoroDates: Pomodoro 세션이 실행된 날짜 배열
+  ///   - pomodoroLevels: 각 날짜에 해당하는 Pomodoro 레벨 배열
+  /// - Returns: 일주일 단위로 그룹화된 `PomodoroRecordCellItem` 배열을 반환합니다. 날짜와 레벨 배열의 크기가 다르면 `nil`을 반환합니다.
+  static func groupedPomodoroRecordCellItem(
+    pomodoroDates: [Date],
+    pomodoroLevels: [PomodoroLevel]
+  ) -> [PomodoroRecordCellItem]? {
+    guard pomodoroDates.count == pomodoroLevels.count
+    else { return nil }
+    
+    var pomodoroRecordCellItems: [PomodoroRecordCellItem] = []
+    
+    let combined = zip(pomodoroDates, pomodoroLevels)
+    
+    var currentBatchDates: [Date] = []
+    var currentBatchLevels: [PomodoroLevel] = []
+    
+    let daysOfWeek = 7
+    
+    for (date, level) in combined {
+      currentBatchDates.append(date)
+      currentBatchLevels.append(level)
+      
+      if currentBatchDates.count == daysOfWeek {
+        let recordItem = PomodoroRecordCellItem(dates: currentBatchDates, pomodoroLevels: currentBatchLevels)
+        pomodoroRecordCellItems.append(recordItem)
+        
+        currentBatchDates.removeAll()
+        currentBatchLevels.removeAll()
+      }
+    }
+    
+    return pomodoroRecordCellItems
+  }
 }
