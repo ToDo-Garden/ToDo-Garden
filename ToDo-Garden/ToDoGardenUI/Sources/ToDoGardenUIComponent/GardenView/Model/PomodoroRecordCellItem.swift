f982f2a50663f34385ed71494b5451110a23e703
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-06-27 16:55:19 +0900

    #213: formattedFirstDayOfMonth 함수 구현
    - 인스턴스의 날짜 목록에서 해당 월의 첫 번째 날짜를 포매팅된 문자열로 반환하는 함수를 구현하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Model/PomodoroRecordCellItem.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Model/PomodoroRecordCellItem.swift
@@ -22,6 +22,28 @@ struct PomodoroRecordCellItem: Hashable {
     hasher.combine(self.id)
   }
   
+  /// 인스턴스의 날짜 목록에서 해당 월의 첫 번째 날짜를 포매팅된 문자열로 반환합니다.
+  ///
+  /// `PomodoroRecordCellItem`인스턴스의 `dates`를 Set으로 변환한 후 순회하며 각 날짜의 해당 월 첫 번째 날짜가 배열에 포함되어 있는지 확인합니다.
+  /// 배열의 `contains(_:)` 메서드는 O(n) Set의 `contains(_:)`는 O(1)이므로, Set으로 변환하였습니다.
+  /// 만약 그러한 날짜를 찾으면, 이 날짜를 "M/d" 형식의 문자열로 변환하여 반환합니다. 해당 날짜를 찾지 못하면 `nil`을 반환합니다.
+  ///
+  /// - Returns: "M/d" 형식의 문자열로 표현된 해당 월의 첫 번째 날짜, 해당 날짜를 찾지 못한 경우 `nil`을 반환합니다.
+  func formattedFirstDayOfMonth() -> String? {
+    let dateFormatStyle = Date.FormatStyle()
+      .month(Date.FormatStyle.Symbol.Month.defaultDigits)
+      .day(Date.FormatStyle.Symbol.Day.defaultDigits)
+    let dateSet = Set(self.dates)
+    
+    for date in self.dates {
+      if let firstDayOfMonth = self.firstDayOfMonth(from: date), dateSet.contains(firstDayOfMonth) {
+        return firstDayOfMonth.formatted(dateFormatStyle)
+      }
+    }
+    
+    return nil
+  }
+  
   /// 그룹화된 `PomodoroRecordCellItem` 배열을 반환합니다.
   /// - Parameters:
   ///   - pomodoroDates: Pomodoro 세션이 실행된 날짜 배열
@@ -57,5 +79,27 @@ struct PomodoroRecordCellItem: Hashable {
     }
     
     return pomodoroRecordCellItems
+  }
+}
+
+extension PomodoroRecordCellItem {
+  /// 주어진 날짜의 해당 월의 첫 번째 날짜를 반환합니다.
+  ///
+  /// 주어진 날짜에서 연도와 월을 추출하고, 해당 월의 첫 번째 날을 나타내는 날짜를 반환합니다.
+  ///
+  /// - Parameter date: 기준이 되는 날짜.
+  /// - Returns: 해당 월의 첫 번째 날짜, 생성할 수 없는 경우 `nil`을 반환합니다.
+  private func firstDayOfMonth(from date: Date) -> Date? {
+    let calendar = Calendar.current
+    var components = calendar.dateComponents(
+      [
+        Calendar.Component.year,
+        Calendar.Component.month
+      ],
+      from: date
+    )
+    components.day = 1
+    
+    return calendar.date(from: components)
   }
 }
