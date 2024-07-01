d3ba21caff9cb68310551b7e999b07f2b920b48a
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-01 16:18:50 +0900

    #213: DataSource 정의
    - PomodoroLevelCollectionView의 DataSource를 정의하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/PomodoroLevelCollectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/PomodoroLevelCollectionView.swift
@@ -11,6 +11,50 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 final class PomodoroLevelCollectionView: UICollectionView {
+  private var pomodoroLevelCollectionViewDataSource: DataSource?
+  
+  init() {
+    self.setup()
+  }
+  
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Setup
+
+extension PomodoroLevelCollectionView {
+  private func setup() {
+    self.setupDataSource()
+  }
+  
+  private func setupDataSource() {
+    self.pomodoroLevelCollectionViewDataSource = self.makeDataSource()
+  }
+}
+
+extension PomodoroLevelCollectionView {
+  private func makeDataSource() -> DataSource {
+    let cellRegistration = self.createPomodoroLevelCollectionViewCellRegistration()
+    
+    return DataSource(collectionView: self) { pomodoroLevelsCollectionView, indexPath, pomodoroLevelItem in
+      let pomodoroLevelCollectionViewCell = pomodoroLevelsCollectionView.dequeueConfiguredReusableCell(
+        using: cellRegistration,
+        for: indexPath,
+        item: pomodoroLevelItem.pomodoroLevel
+      )
+      
+      return pomodoroLevelCollectionViewCell
+    }
+  }
+  
+  private func createPomodoroLevelCollectionViewCellRegistration() -> CellRegistration {
+    return CellRegistration { pomodoroLevelCollectionViewCell, _, pomodoroLevel in
+      pomodoroLevelCollectionViewCell.configure(with: pomodoroLevel)
+    }
+  }
 }
 
 extension PomodoroLevelCollectionView {
