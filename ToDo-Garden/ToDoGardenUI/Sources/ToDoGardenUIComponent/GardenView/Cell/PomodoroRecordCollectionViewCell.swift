a9a58a66f8948dcabf0959df769d711da92420c6
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-01 16:50:15 +0900

    #213: firstDayOfMonthLabel 추가
    - 셀에 포함된 달의 첫째날을 나타내는 label을 추가하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Cell/PomodoroRecordCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PomodoroRecordCollectionViewCell.swift
+//
+//
+//  Created by Noah on 6/7/24.
+//
+
+import UIKit
+
+import ToDoGardenUIResource
+
+final class PomodoroRecordCollectionViewCell: UICollectionViewCell {
+  private let firstDayOfMonthLabel: UILabel = {
+    let firstDayOfMonthLabel = UILabel()
+    firstDayOfMonthLabel.font = UIFont.pretendardDetailRegular5
+    firstDayOfMonthLabel.textColor = UIColor.black
+    firstDayOfMonthLabel.textAlignment = NSTextAlignment.center
+    
+    return firstDayOfMonthLabel
+  }()
+}
