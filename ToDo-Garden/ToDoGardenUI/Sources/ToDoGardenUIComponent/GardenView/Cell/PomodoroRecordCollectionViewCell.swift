f7ef3bf2d953fa72457966d9f28f334e3cc125cc
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-01 16:54:32 +0900

    #213: layout 설정
    - autolayout 제약설정을 진행하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Cell/PomodoroRecordCollectionViewCell.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GardenView/Cell/PomodoroRecordCollectionViewCell.swift
@@ -37,4 +37,52 @@ final class PomodoroRecordCollectionViewCell: UICollectionViewCell {
     
     return contentStackView
   }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setup()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Setup
+
+extension PomodoroRecordCollectionViewCell {
+  private func setup() {
+    self.addSubviews()
+    self.setupLayoutConstraints()
+  }
+}
+
+// MARK: - Add subviews
+
+extension PomodoroRecordCollectionViewCell {
+  private func addSubviews() {
+    self.contentView.addSubview(self.contentStackView)
+    self.contentStackView.addArrangedSubview(self.pomodoroLevelsView)
+    self.contentStackView.addArrangedSubview(self.firstDayOfMonthLabel)
+  }
+}
+
+// MARK: - Setup layout constraints
+
+extension PomodoroRecordCollectionViewCell {
+  private func setupLayoutConstraints() {
+    self.setupContentsStackViewLayoutConstraints()
+  }
+  
+  private func setupContentsStackViewLayoutConstraints() {
+    self.contentStackView.usingAutolayout()
+    
+    NSLayoutConstraint.activate([
+      self.contentStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+      self.contentStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+      self.contentStackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+      self.contentStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+    ])
+  }
 }
