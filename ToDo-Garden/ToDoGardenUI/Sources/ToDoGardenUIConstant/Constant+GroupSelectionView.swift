37743675b035090fe77f161212baf652bed4ce2d
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-03 20:58:04 +0900

    #290: TableView Container Layer 레이아웃 상수 추가
    - 컨테이너 객체의 Layer의 UI를 설정할 때 사용하는 레이아웃 상수들을 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupSelectionView.swift
@@ -21,7 +21,16 @@ extension Constant.GroupSelectionView.Layout {
 }
 
 extension Constant.GroupSelectionView.Layout.TableViewContainer {
+  public enum Layer {}
+
   public static let topMargin: CGFloat = 6
   public static let leadingMargin: CGFloat = 10
   public static let trailingMargin: CGFloat = 5
+}
+
+extension Constant.GroupSelectionView.Layout.TableViewContainer.Layer {
+  public static let shadowOpacity: Float = 0.15
+  public static let shadowOffset = CGSize(width: 0, height: 2)
+  public static let shadowRadius: CGFloat = 4
+  public static let cornerRadius: CGFloat = 9
 }
