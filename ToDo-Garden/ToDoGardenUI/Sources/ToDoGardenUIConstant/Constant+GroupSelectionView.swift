fa5e2dc50ae4b1e2cd2ba56f02724534d14bdf0c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-03 20:56:13 +0900

    #290: TableViewContainer 레이아웃 상수 추가
    - 테이블 뷰를 담는 컨테이너 객체의 레이아웃 상수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+GroupSelectionView.swift
@@ -9,8 +9,19 @@ import Foundation
 
 extension Constant.GroupSelectionView {
   public enum Animation {}
+  public enum Layout {}
 }
 
 extension Constant.GroupSelectionView.Animation {
   public static let duration: CGFloat = 0.2
+}
+
+extension Constant.GroupSelectionView.Layout {
+  public enum TableViewContainer {}
+}
+
+extension Constant.GroupSelectionView.Layout.TableViewContainer {
+  public static let topMargin: CGFloat = 6
+  public static let leadingMargin: CGFloat = 10
+  public static let trailingMargin: CGFloat = 5
 }
