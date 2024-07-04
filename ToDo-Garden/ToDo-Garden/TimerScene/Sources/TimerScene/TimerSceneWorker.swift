b3ebb4392093a29474066e442e72056928d8cc34
GangWoon <gangwoon2018@gmail.com>
2024-07-04 16:36:28 +0900

    #295: Refactor CountDownSequence to simplify time interval calculations

--- ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneWorker.swift
+++ ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneWorker.swift
@@ -17,7 +17,6 @@ extension TimerSceneWorker {
 }
 
 public struct CountDownSequence: AsyncSequence {
-  public typealias Element = Double
   let endTime: TimeInterval
   let timeInterval: UInt64
   
@@ -29,24 +28,40 @@ public struct CountDownSequence: AsyncSequence {
   public func makeAsyncIterator() -> AsyncIterator {
     AsyncIterator(endTime: endTime, timeInterval: timeInterval)
   }
+  
   public struct AsyncIterator: AsyncIteratorProtocol {
     private var endTime: TimeInterval
     private let timeInterval: UInt64
-    private var isFirst: Bool = true
     
-    init(
-      endTime: TimeInterval,
-      timeInterval: UInt64
-    ) {
+    private var targetTime: TimeInterval
+    private var timeCorrection: TimeInterval
+    
+    init(endTime: TimeInterval, timeInterval: UInt64) {
       self.endTime = endTime
       self.timeInterval = timeInterval
+      self.targetTime = Date().timeIntervalSince1970
+      self.timeCorrection = 0
     }
     
     public mutating func next() async throws -> Double? {
       guard self.endTime > 0 else { return nil }
-      try await Task.sleep(nanoseconds: timeInterval)
-      self.endTime -= 1
+      self.targetTime += 1.0
+      try await self.sleepForCorrectTime()
+      self.endTime -= 1.0
+      
       return self.endTime
+    }
+    
+    private mutating func sleepForCorrectTime() async throws {
+      let current = Date().timeIntervalSince1970
+      let sleepTime = self.targetTime - current + self.timeCorrection
+      if sleepTime > 0 {
+        try await Task.sleep(nanoseconds: UInt64(sleepTime * Double(self.timeInterval)))
+        self.timeCorrection = 0
+      } else {
+        try Task.checkCancellation()
+        self.timeCorrection = sleepTime
+      }
     }
   }
 }
