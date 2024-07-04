776d6cfd7271d1b1f4d5e3c29c17be546a977ad8
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-04 16:01:56 +0900

    #294: DiffableDataSource 아이템 추가
    - 수정가능한 그룹 목록을 TableView에 표시할때 사용되는 아이템을 추가하였습니다.
    - DiffableDatSource에서 아이템으로 사용할 수 있도록 Hashable 프로토콜을 채택하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/EditableGroupSection+Item.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/EditableGroupSection+Item.swift
@@ -10,3 +10,25 @@ import UIKit.UIColor
 enum EditableGroupSection {
   case main
 }
+
+public struct EditableGroupItem {
+  let groupId: Int
+  let groupName: String
+  let groupColor: UIColor
+
+  public init(
+    groupId: Int,
+    groupName: String,
+    groupColor: UIColor
+  ) {
+    self.groupId = groupId
+    self.groupName = groupName
+    self.groupColor = groupColor
+  }
+}
+
+extension EditableGroupItem: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.groupId)
+  }
+}
