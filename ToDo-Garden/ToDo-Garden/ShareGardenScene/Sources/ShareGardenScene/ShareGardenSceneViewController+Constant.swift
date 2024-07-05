e40c0ef7ae80d79c378a5e1bb2eac9082493ed2a
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-05 20:58:22 +0900

    #298: 상수관리 파일 추가

---
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/ShareGardenSceneViewController+Constant.swift
@@ -0,0 +1,13 @@
+//
+//  ShareGardenSceneViewController+Constant.swift
+//
+//
+//  Created by Noah on 7/5/24.
+//
+
+import Foundation
+
+extension ShareGardenSceneViewController {
+  enum Constant {
+  }
+}
