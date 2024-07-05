749d2211d72d7398f97964304dd946d4eba55bc9
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-05 16:49:46 +0900

    #298: HeaderView 파일 추가

---
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/ShareGardenSceneViewController+Header.swift
@@ -0,0 +1,14 @@
+//
+//  ShareGardenSceneViewController+Header.swift
+//
+//
+//  Created by Noah on 7/5/24.
+//
+
+import UIKit
+
+extension ShareGardenSceneViewController {
+  final class Header: UIView {
+    
+  }
+}
