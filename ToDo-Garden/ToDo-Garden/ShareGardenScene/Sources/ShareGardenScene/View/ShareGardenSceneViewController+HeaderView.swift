30483d53c6d3b4bc5ffcfe71b887d16afb2fb31d
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-05 19:09:03 +0900

    #298: spacing 설정
    - bounds가 결정된 시점 이후 spacing이 한번만 설정되도록 하였습니다.

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/ShareGardenSceneViewController+HeaderView.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/ShareGardenSceneViewController+HeaderView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import TDUtility
+
 import ToDoGardenUIComponent
 import ToDoGardenUIResource
 
@@ -34,6 +36,11 @@ extension ShareGardenSceneViewController {
       
       return shareButton
     }()
+    
+    // MARK: - Properties
+    
+    @ExecuteOnce private var configureSpacingOnce: (() -> Void)?
+    
     // MARK: - Object life cycle
     
     init() {
@@ -45,6 +52,16 @@ extension ShareGardenSceneViewController {
     required init(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - View life cycle
+    
+    override func layoutSubviews() {
+      super.layoutSubviews()
+      self.configureSpacingOnce = {
+        self.setupSpacing()
+      }
+    }
+  }
 }
 
 // MARK: - Setup
@@ -85,5 +102,9 @@ extension ShareGardenSceneViewController.HeaderView {
   private func addSubviews() {
     self.addArrangedSubview(self.titleLabel)
     self.addArrangedSubview(self.shareButton)
+  }
+  
+  private func setupSpacing() {
+    self.spacing = self.bounds.width * (194 / 323)
   }
 }
