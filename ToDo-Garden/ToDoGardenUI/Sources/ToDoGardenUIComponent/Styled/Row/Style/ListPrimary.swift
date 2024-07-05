edfa2b6bbf0c066f320f5e3cf3f6e291a4c4746d
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-05 13:36:21 +0900

    #287: ColorView 레이아웃 설정 함수 추가
    - 리뷰를 반영하여 기존의 ColorView 레이아웃을 설정하는 로직을 별도의 함수로 분리하였습니다.
    - 관련 코드리뷰 링크: https://github.com/ToDo-Garden/ToDo-Garden/pull/289#discussion_r1665834551

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Style/ListPrimary.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Style/ListPrimary.swift
@@ -18,22 +18,26 @@ extension Styled.Row {
     stack.addArrangedSubview(label)
     stack.addSpacing()
     let colorView = self.buildColorView(stack: stack, color: model.color)
+    stack.addArrangedSubview(colorView)
+    self.setupColorViewLayout(colorView)
     self.buildRightView(stack: stack, views: views)
     self.bindingGroupNameState(label: label)
     self.bindingGroupColorState(colorView: colorView)
   }
-  
+
   private func buildColorView(stack: UIStackView, color: UIColor) -> UIView {
     let colorView = UIView()
     colorView.backgroundColor = color
     colorView.layer.cornerRadius = Constant.Styled.Row.ListPrimary.colorViewCornerRadius
+    return colorView
+  }
+
+  private func setupColorViewLayout(_ colorView: UIView) {
     colorView.usingAutolayout()
     NSLayoutConstraint.activate([
       colorView.widthAnchor.constraint(equalToConstant: Constant.Styled.Row.ListPrimary.colorViewSize.width),
       colorView.heightAnchor.constraint(equalToConstant: Constant.Styled.Row.ListPrimary.colorViewSize.height)
     ])
-    stack.addArrangedSubview(colorView)
-    return colorView
   }
 
   private func buildRightView(stack: UIStackView, views: [UIView]? = nil) {
