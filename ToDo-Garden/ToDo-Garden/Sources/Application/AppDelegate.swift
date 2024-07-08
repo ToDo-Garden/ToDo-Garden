bf5ad5560e4974db5f44e31626a0d07b93083032
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-08 18:17:29 +0900

    #319: AppDelegate 네비게이션 바 업데이트 함수 삭제
    - AppDelegate에 존재하던 네비게이션 바 업데이트 함수를 삭제하였습니다.

--- ToDo-Garden/ToDo-Garden/Sources/Application/AppDelegate.swift
+++ ToDo-Garden/ToDo-Garden/Sources/Application/AppDelegate.swift
@@ -16,7 +16,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
 	) -> Bool {
 		self.registerCustomFonts()
-    self.updateNavigationBarApperance()
 		return true
 	}
 }
@@ -25,29 +24,4 @@ extension AppDelegate {
 	private func registerCustomFonts() {
 		PretendardFont.register()
 	}
-}
-
-extension AppDelegate {
-  private func updateNavigationBarApperance() {
-    self.updateNavigationBarTitle()
-    self.updateNavigationBackButtonItem()
-  }
-
-  private func updateNavigationBarTitle() {
-    UINavigationBar.appearance().titleTextAttributes = [
-      NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark,
-      NSAttributedString.Key.font: UIFont.pretendardHeadBold
-    ]
-  }
-
-  private func updateNavigationBackButtonItem() {
-    UIBarButtonItem.appearance().setBackButtonBackgroundImage(
-      UIImage.backwardButtonImage,
-      for: UIControl.State.normal,
-      barMetrics: UIBarMetrics.default
-    )
-
-    UINavigationBar.appearance().backIndicatorImage = UIImage()
-    UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage()
-  }
 }
