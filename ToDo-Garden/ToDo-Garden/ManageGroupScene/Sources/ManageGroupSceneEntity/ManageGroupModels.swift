6477f33b44b2aef820e54690034cdf3ddf87ba90
SONG <geun2121@gmail.com>
2024-07-09 17:05:57 +0900

    #323: ToDoGroup 모델 추가
    - ToDoGroup을 구성하기 위한 데이터 스트럭트를 정의합니다.

--- ToDo-Garden/ToDo-Garden/ManageGroupScene/Sources/ManageGroupSceneEntity/ManageGroupModels.swift
+++ ToDo-Garden/ToDo-Garden/ManageGroupScene/Sources/ManageGroupSceneEntity/ManageGroupModels.swift
@@ -8,7 +8,19 @@
 import UIKit.UIColor
 
 public enum ManageGroup {
-  
+  public struct ToDoGroup {
+    public let id: String
+    public let groupName: String
+    public let progressColor: UIColor
+    public let progressRate: Float
+    
+    public init(id: String, groupName: String, progressColor: UIColor, progressRate: Float) {
+      self.id = id
+      self.groupName = groupName
+      self.progressColor = progressColor
+      self.progressRate = progressRate
+    }
+  }
   // MARK: Use cases
   
   public enum Something {
