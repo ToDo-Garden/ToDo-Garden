1bcd1b0e35cd4f15d4524e1da2522dde1b8ae67c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-09 15:44:56 +0900

    #322: CurrentGroupRow 추가
    - 현재 투두가 속한 그룹을 나타내는 Row를 추가하였습니다.
    - 레이아웃 설정 함수도 함께 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
@@ -11,10 +11,22 @@ import ToDoGardenUIConstant
 
 public final class GroupSelectionView: UIView {
   private let model: GroupSelectionView.Model
+  private let showGroupListButton: UIButton
+  private let currentGroupRow: Styled.Row
 
   public init(model: GroupSelectionView.Model) {
     self.model = model
+    self.showGroupListButton = UIButton()
+    self.currentGroupRow = Styled.Row(
+      configuration: Styled.Row.Configuration.listPrimary(
+        Styled.Row.Configuration.ListPrimaryModel(
+          title: Constant.GroupSelectionView.StringLiteral.defaultGroupName,
+          color: UIColor.toDoGardenYellow
+        )
+      )
+    )
     super.init(frame: CGRect.zero)
+    self.setup()
   }
   
   @available(*, unavailable)
@@ -22,6 +34,42 @@ public final class GroupSelectionView: UIView {
     fatalError("init(coder:) has not been implemented")
   }
 }
+
+// MARK: Private Functions
+
+extension GroupSelectionView {
+  private func setup() {
+    self.addSubviews()
+    self.setupSubviewsLayout()
+  }
+}
+
+// MARK: About Auto Layout
+
+extension GroupSelectionView {
+  private func addSubviews() {
+    self.addSubview(self.currentGroupRow)
+  }
+
+  private func setupSubviewsLayout() {
+    self.setupCurrentGroupRowLayout()
+  }
+
+  private func setupCurrentGroupRowLayout() {
+    self.currentGroupRow.usingAutolayout()
+
+    NSLayoutConstraint.activate(
+      [
+        self.currentGroupRow.topAnchor.constraint(equalTo: self.topAnchor),
+        self.currentGroupRow.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+        self.currentGroupRow.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        self.currentGroupRow.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor)
+      ]
+    )
+  }
+}
+
+// MARK: Model
 
 extension GroupSelectionView {
   public struct Model {
