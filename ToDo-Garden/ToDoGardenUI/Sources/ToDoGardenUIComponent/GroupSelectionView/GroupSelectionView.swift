bcfd73c3d5a90a85eea51f45e14f13ad10f9e44b
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-09 15:39:00 +0900

    #322: Model 객체 추가
    - 사용하는 쪽에서 생성시 커스텀할 수 있도록 Model 객체를 생성하였습니다.
    - 드롭다운에서 보여질 셀의 높이와 개수를 커스텀 할 수 있습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
@@ -7,6 +7,35 @@
 
 import UIKit
 
+import ToDoGardenUIConstant
+
 public final class GroupSelectionView: UIView {
+  private let model: GroupSelectionView.Model
+
+  public init(model: GroupSelectionView.Model) {
+    self.model = model
+    super.init(frame: CGRect.zero)
+  }
   
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension GroupSelectionView {
+  public struct Model {
+    let cellHeight: CGFloat
+    let visibleCellCount: Int
+
+    init(cellHeight: CGFloat, visibleCellCount: Int) {
+      self.cellHeight = cellHeight
+      self.visibleCellCount = visibleCellCount
+    }
+
+    public static let primary = Self(
+      cellHeight: Constant.GroupSelectionView.Model.Primary.cellHeight,
+      visibleCellCount: Constant.GroupSelectionView.Model.Primary.visibleCellCount
+    )
+  }
 }
