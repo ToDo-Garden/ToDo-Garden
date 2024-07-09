e55a36b827299d028fba16bce2415f3162226279
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-09 17:04:13 +0900

    #322: TableView DiffableDataSource 생성 함수 추가
    - TableView의 데이터 소스를 생성하는 함수를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/EditableGroupListTableViewDelegate.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/EditableGroupListTableViewDelegate.swift
@@ -11,11 +11,15 @@ import ToDoGardenUIConstant
 
 final class EditableGroupTableViewDelegate: NSObject {
   private let cellHeight: CGFloat
+  private var tableViewDataSource: UITableViewDiffableDataSource<
+    EditableGroupSection,
+    EditableGroupItem
+  >!
 
   init(tableView: UITableView, cellHeight: CGFloat) {
     self.cellHeight = cellHeight
     super.init()
-    tableView.delegate = self
+    self.setupTableView(tableView)
   }
 }
 
@@ -34,5 +38,33 @@ extension EditableGroupTableViewDelegate: UITableViewDelegate {
 
   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return Constant.GroupSelectionView.Layout.EditableGroupTableViewDelegate.headerViewHeight
+  }
+}
+
+// MARK: Private Functions
+
+extension EditableGroupTableViewDelegate {
+  private func setupTableView(_ tableView: UITableView) {
+    tableView.delegate = self
+    self.tableViewDataSource = self.makeDiffableDataSource(tableView: tableView)
+    tableView.dataSource = self.tableViewDataSource
+  }
+
+  private func makeDiffableDataSource(tableView: UITableView) -> UITableViewDiffableDataSource<
+    EditableGroupSection,
+    EditableGroupItem
+  > {
+    return UITableViewDiffableDataSource<
+      EditableGroupSection,
+      EditableGroupItem
+    >(tableView: tableView) { (tableView, indexPath, itemIdentifier) in
+      guard let cell = tableView.dequeueReusableCell(
+        type: EditableGroupTableViewCell.self,
+        for: indexPath
+      ) else { return UITableViewCell() }
+
+      cell.updateUI(groupItem: itemIdentifier)
+      return cell
+    }
   }
 }
