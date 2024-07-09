1bb54b3042bb106da639aad246782ab302a03945
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-09 16:39:24 +0900

    #322: EditableGroupListTableView 프로퍼티 추가
    - 선택가능한 그룹을 보여줄 때 사용되는 TableView를 추가하였습니다.
    - TableView를 담고 버튼 선택 상태에 따라 숨겨질 수 있도록 ContainerView를 추가하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/GroupSelectionView/GroupSelectionView.swift
@@ -13,6 +13,7 @@ public final class GroupSelectionView: UIView {
   private let model: GroupSelectionView.Model
   private let showGroupListButton: UIButton
   private let currentGroupRow: Styled.Row
+  private let editableGroupListTableView: UITableView
 
   public init(model: GroupSelectionView.Model) {
     self.model = model
@@ -25,6 +26,7 @@ public final class GroupSelectionView: UIView {
         )
       )
     )
+    self.editableGroupListTableView = UITableView()
     super.init(frame: CGRect.zero)
     self.setup()
   }
@@ -40,6 +42,7 @@ public final class GroupSelectionView: UIView {
 extension GroupSelectionView {
   private func setup() {
     self.setupShowGroupMenuButton()
+    self.setupEditableGroupListTableView()
     self.addSubviews()
     self.setupSubviewsLayout()
   }
@@ -65,6 +68,25 @@ extension GroupSelectionView {
       self.showGroupListButton.transform = transform
     }
   }
+
+  private func setupEditableGroupListTableView() {
+    self.editableGroupListTableView.register(
+      EditableGroupTableViewCell.self,
+      forCellReuseIdentifier: EditableGroupTableViewCell.identifier
+    )
+    self.setupEditableGroupListTableViewUI()
+  }
+
+  private func setupEditableGroupListTableViewUI() {
+    self.editableGroupListTableView.sectionHeaderTopPadding = 0
+    self.editableGroupListTableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+    let layout = Constant.GroupSelectionView.Layout.EditableGroupTableView.Layer.self
+    self.editableGroupListTableView.layer.cornerRadius = layout.cornerRadius
+    self.editableGroupListTableView.layer.maskedCorners = [
+      CACornerMask.layerMinXMaxYCorner,
+      CACornerMask.layerMaxXMaxYCorner
+    ]
+  }
 }
 
 // MARK: About Auto Layout
@@ -76,6 +98,9 @@ extension GroupSelectionView {
 
   private func setupSubviewsLayout() {
     self.setupCurrentGroupRowLayout()
+    let containerView = self.makeGroupListContainerView()
+    self.setupContainerViewLayout(containerView: containerView)
+    self.setupGroupListTableViewLayout(with: containerView)
   }
 
   private func setupCurrentGroupRowLayout() {
@@ -87,6 +112,62 @@ extension GroupSelectionView {
         self.currentGroupRow.leadingAnchor.constraint(equalTo: self.leadingAnchor),
         self.currentGroupRow.trailingAnchor.constraint(equalTo: self.trailingAnchor),
         self.currentGroupRow.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor)
+      ]
+    )
+  }
+
+  private func makeGroupListContainerView() -> UIView {
+    let containerView = UIView()
+    self.setupShadowLayer(to: containerView)
+    return containerView
+  }
+
+  private func setupShadowLayer(to view: UIView) {
+    let layerConstant = Constant.GroupSelectionView.Layout.TableViewContainer.Layer.self
+    view.layer.shadowOpacity = layerConstant.shadowOpacity
+    view.layer.shadowOffset = layerConstant.shadowOffset
+    view.layer.shadowRadius = layerConstant.shadowRadius
+    view.layer.cornerRadius = layerConstant.cornerRadius
+    view.layer.masksToBounds = false
+    view.layer.maskedCorners = [
+      CACornerMask.layerMinXMaxYCorner,
+      CACornerMask.layerMaxXMaxYCorner
+    ]
+  }
+
+  private func setupContainerViewLayout(containerView: UIView) {
+    self.addSubview(containerView)
+
+    containerView.usingAutolayout()
+    let layout = Constant.GroupSelectionView.Layout.TableViewContainer.self
+    NSLayoutConstraint.activate(
+      [
+        containerView.topAnchor.constraint(
+          equalTo: self.currentGroupRow.bottomAnchor,
+          constant: layout.topMargin
+        ),
+        containerView.leadingAnchor.constraint(
+          equalTo: self.currentGroupRow.leadingAnchor,
+          constant: layout.leadingMargin
+        ),
+        containerView.trailingAnchor.constraint(
+          equalTo: self.currentGroupRow.trailingAnchor,
+          constant: -layout.trailingMargin
+        )
+      ]
+    )
+  }
+
+  private func setupGroupListTableViewLayout(with containerView: UIView) {
+    containerView.addSubview(self.editableGroupListTableView)
+    self.editableGroupListTableView.usingAutolayout()
+
+    NSLayoutConstraint.activate(
+      [
+        self.editableGroupListTableView.topAnchor.constraint(equalTo: containerView.topAnchor),
+        self.editableGroupListTableView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+        self.editableGroupListTableView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+        self.editableGroupListTableView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
       ]
     )
   }
