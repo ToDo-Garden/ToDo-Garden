c28b0edd4abf56c56cadb632090d2aff80f991b6
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-11 15:13:13 +0900

    #271: Repetition Label 추가
    - 반복 관련 설정임을 나타내는 레이블을 추가하였습니다.
    - 레이아웃 설정 함수도 함께 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoRepetitionView.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoRepetitionView.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
+import ToDoGardenUIComponent
+
 final class EditToDoRepetitionView: UIView {
+  private let repetitionLabel: UILabel
+
   init() {
+    self.repetitionLabel = UILabel()
     super.init(frame: CGRect.zero)
+    self.setup()
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+
+extension EditToDoRepetitionView {
+  private func setup() {
+    self.setupRepetitionLabelUI()
+  }
+
+  private func setupRepetitionLabelUI() {
+    self.repetitionLabel.font = UIFont.pretendardHeadSemiBold
+    self.repetitionLabel.textColor = EditToDoSceneTheme.mainColor
+    let text = EditToDoSceneTheme.StringLiteral.ToDoScheduleView.AlarmLabel.text
+    self.repetitionLabel.text = text
+  }
+}
+
+// MARK: About Layout
+
+extension EditToDoRepetitionView {
+  private func addSubviews() {
+    self.addSubview(self.repetitionLabel)
+  }
+
+  private func setupSubviewsLayout() {
+    self.setupRepetitionLabelLayout()
+  }
+
+  private func setupRepetitionLabelLayout() {
+    self.repetitionLabel.usingAutolayout()
+
+    let layout = EditToDoViewController.Constant.Layout.EditToDoScheduleView.EditToDoRepetitionView.self
+    NSLayoutConstraint.activate(
+      [
+        self.repetitionLabel.topAnchor.constraint(equalTo: self.topAnchor),
+        self.repetitionLabel.leadingAnchor.constraint(
+          equalTo: self.leadingAnchor,
+          constant: layout.RepetitionLabel.leadingMargin
+        )
+      ]
+    )
   }
 }
