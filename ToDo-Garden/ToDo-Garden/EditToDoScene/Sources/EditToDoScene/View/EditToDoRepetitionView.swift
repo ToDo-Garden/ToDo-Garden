d4713bf3c5cf985bf345a980c22e5b3927b7a504
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-11 15:08:55 +0900

    #271: EditToDoRepetitionView 추가
    - 투두 반복 설정에 필요한 뷰를 담는 컨테이너 뷰 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoRepetitionView.swift
@@ -0,0 +1,19 @@
+//
+//  EditToDoRepetitionView.swift
+//
+//
+//  Created by Wood on 7/11/24.
+//
+
+import UIKit
+
+final class EditToDoRepetitionView: UIView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
