dd1974d855e19cc15ca25d33af350b36d1017bf4
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-11 16:43:09 +0900

    #271: EditToDoScheduleView 생성
    - 투두 알림, 반복 설정들을 담는 컨테이너 뷰 객체를 생성하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoScheduleView.swift
@@ -0,0 +1,21 @@
+//
+//  EditToDoScheduleView.swift
+//
+//
+//  Created by Wood on 7/11/24.
+//
+
+import UIKit
+
+import ToDoGardenUIComponent
+
+final class EditToDoScheduleView: UIView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
