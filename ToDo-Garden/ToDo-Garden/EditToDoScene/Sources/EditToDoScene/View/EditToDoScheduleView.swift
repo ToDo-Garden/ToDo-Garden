0ca71ba04055155d345ec54c5a7e52419181ef8b
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-11 16:45:45 +0900

    #271: AlarmSwitch 추가
    - 투두 알림을 활성화할 때 사용하는 스위치를 추가하였습니다.
    - 레이아웃 설정 함수도 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoScheduleView.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/View/EditToDoScheduleView.swift
@@ -10,12 +10,51 @@ import UIKit
 import ToDoGardenUIComponent
 
 final class EditToDoScheduleView: UIView {
+  private let alarmSwitch: ToDoGardenSwitch
+
   init() {
+    self.alarmSwitch = ToDoGardenSwitch(model: ToDoGardenSwitch.Model.primary)
     super.init(frame: CGRect.zero)
+    self.setup()
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+
+extension EditToDoScheduleView {
+  private func setup() {
+    self.addSubviews()
+    self.setupSubviewsLayout()
+  }
+
+  private func addSubviews() {
+    self.addSubview(self.alarmSwitch)
+  }
+
+  private func setupSubviewsLayout() {
+    self.setupAlarmSwitchLayout()
+  }
+
+  private func setupAlarmSwitchLayout() {
+    self.alarmSwitch.usingAutolayout()
+
+    let layout = EditToDoViewController.Constant.Layout.EditToDoScheduleView.AlarmSwitch.self
+    NSLayoutConstraint.activate(
+      [
+        self.alarmSwitch.topAnchor.constraint(
+          equalTo: self.topAnchor,
+          constant: layout.topMargin
+        ),
+        self.alarmSwitch.trailingAnchor.constraint(
+          equalTo: self.trailingAnchor,
+          constant: -layout.trailingMargin
+        )
+      ]
+    )
   }
 }
