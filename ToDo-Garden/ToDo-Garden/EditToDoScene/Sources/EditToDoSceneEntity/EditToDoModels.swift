bbee55d85eb2d42d2e67eebd5fc175105b6a1185
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-11 13:16:33 +0900

    #333: ToDo 데이터 추가
    - ToDo의 정보를 가지는 도메인 객체를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoSceneEntity/EditToDoModels.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoSceneEntity/EditToDoModels.swift
@@ -5,7 +5,7 @@
 //  Created by Wood on 6/29/24.
 //  Copyright (c) 2024 ToDoGarden. All rights reserved.
 
-import Foundation
+import UIKit.UIColor
 
 public enum EditToDo {
   
@@ -20,6 +20,61 @@ public enum EditToDo {
     }
     public struct ViewModel {
       public init() { }
+    }
+  }
+}
+
+extension EditToDo {
+  public struct ToDo {
+    public let name: String
+    public let groupData: Group
+    public var alarm: ToDoAlarm
+    public var repetition: ToDoRepetition
+
+    public init(
+      name: String,
+      groupData: Group,
+      alarm: ToDoAlarm,
+      repetition: ToDoRepetition
+    ) {
+      self.name = name
+      self.groupData = groupData
+      self.alarm = alarm
+      self.repetition = repetition
+    }
+  }
+
+  public struct Group {
+    public let id: Int
+    public let name: String
+    public let color: UIColor
+
+    public init(id: Int, name: String, color: UIColor) {
+      self.id = id
+      self.name = name
+      self.color = color
+    }
+  }
+
+  public struct ToDoAlarm {
+    public let isAlarmOn: Bool
+    public var alarmTime: Date?
+
+    public init(isAlarmOn: Bool, alarmTime: Date?) {
+      self.isAlarmOn = isAlarmOn
+      self.alarmTime = alarmTime
+    }
+  }
+
+  public struct ToDoRepetition {
+    public var isOnlyToday: Bool
+    public var startDate: Date?
+    public var endDate: Date?
+
+    public init(isOnlyToday: Bool, startDate: Date?, endDate: Date?) {
+      self.isOnlyToday = isOnlyToday
+      self.startDate = startDate
+      self.endDate = endDate
     }
   }
 }
