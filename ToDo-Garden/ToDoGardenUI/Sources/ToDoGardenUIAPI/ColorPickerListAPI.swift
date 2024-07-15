75648bad9f18080216579978a677b834dfa12b59
SONG <geun2121@gmail.com>
2024-07-15 16:22:31 +0900

    #348: 명세 수정
    - 초기화문 제거

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIAPI/ColorPickerListAPI.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIAPI/ColorPickerListAPI.swift
@@ -11,5 +11,4 @@ import UIKit.UIColor
 public protocol ColorPickerListAPI {
   var colors: [UIColor] { get }
   var selected: CurrentValueSubject<Int?, Never> { get }
-  init(colors: [UIColor], itemsPerRow: Int, selected: CurrentValueSubject<Int?, Never>)
 }
