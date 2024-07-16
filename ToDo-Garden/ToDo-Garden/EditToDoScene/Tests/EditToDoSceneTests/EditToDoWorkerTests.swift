791499941ae6d4e012120fdc347f09795be5db73
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-16 19:10:29 +0900

    #356: Seconds > Date 테스트 코드 추가
    - 사용자가 입력한 시간 초를 Date타입으로 변경하는 테스트 코드를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoWorkerTests.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoWorkerTests.swift
@@ -11,5 +11,23 @@ import Testing
 @testable import EditToDoScene
 
 struct EditToDoWorkerTests {
-  
+  private let editToDoWorker = EditToDoWorker(calendar: TestData.defaultCalendar)
+
+  @Test(
+    "make dates from valid seconds",
+    arguments: TestData.EditToDoWorkerTests.seconds
+  ) func test_make_dates_from_valid_seconds(
+    secondData: TestData.EditToDoWorkerTests.SecondData
+  ) throws {
+    let second = secondData.input
+    let expectedResultHour = secondData.hour
+    let expectedResultMinute = secondData.minute
+
+    let date = try #require(self.editToDoWorker.makeDate(from: second))
+    let hour = TestData.defaultCalendar.component(Calendar.Component.hour, from: date)
+    let minute = TestData.defaultCalendar.component(Calendar.Component.minute, from: date)
+
+    #expect(hour == expectedResultHour)
+    #expect(minute == expectedResultMinute)
+  }
 }
