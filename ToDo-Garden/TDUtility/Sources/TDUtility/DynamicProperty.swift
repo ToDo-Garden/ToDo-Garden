f22f65cc6c8bd23cae54c0956e343bb88d23f1b2
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-07-16 16:55:02 +0900

    #350: 클로저가 실행될 특성 명시
    - 속성과 별개로 클로져 내부에서 UI관련 작업을 진행할 것이므로 MainActor에서 실행될 것임을 명시하였습니다.

--- ToDo-Garden/TDUtility/Sources/TDUtility/DynamicProperty.swift
+++ ToDo-Garden/TDUtility/Sources/TDUtility/DynamicProperty.swift
@@ -37,7 +37,7 @@ import Foundation
 public final class DynamicUIProperty<Value> {
   private var value: Value
   // TODO: changeHandler가 여러개가 되어야 할 경우 관리 자료구조 추가 혹은 관리 비용이 커질 경우 Combine등 다른 프레임워크 고려
-  private var changeHandler: ((Value) -> Void)?
+  private var changeHandler: (@MainActor @Sendable (Value) -> Void)?
   
   public var wrappedValue: Value {
     get { self.value }
@@ -48,7 +48,7 @@ public final class DynamicUIProperty<Value> {
   }
   
   // TODO: 동시성 환경에서 사용될 경우 @Sendable attribute 명시
-  public var projectedValue: (@escaping (Value) -> Void) -> Void {
+  public var projectedValue: (@MainActor @Sendable @escaping (Value) -> Void) -> Void {
     return { [weak self] newHandler in
       guard let self else { return }
       
