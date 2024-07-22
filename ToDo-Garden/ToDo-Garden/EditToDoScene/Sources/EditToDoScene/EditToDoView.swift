bcd8ed4f8411fba24e2cfc91b31296f4ecb38e1c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-22 23:49:11 +0900

    #371: EditToDoView 객체 생성
    - 투두 수정과 관련된 뷰들을 담는 컨테이너 객체인 EditToDoView 파일을 생성하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoView.swift
@@ -0,0 +1,19 @@
+//
+//  EditToDoView.swift
+//
+//
+//  Created by Wood on 7/22/24.
+//
+
+import UIKit
+
+final class EditToDoView: UIView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
