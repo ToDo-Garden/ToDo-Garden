615ce591909f7e405fbc1f4b5c536c3c744bc072
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-23 14:12:21 +0900

    #371: 투두 이름 입력 뷰 추가
    - 투두 이름을 입력하는 뷰를 추가하였습니다.
    - 레이아웃 설정함수도 함께 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoView.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoView.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
+import ToDoGardenUIAPI
+
 final class EditToDoView: UIView {
-  init() {
+  private let toDoNameInputView: TextInputViewAPI
+
+  init(textInputView: TextInputViewAPI) {
+    self.toDoNameInputView = textInputView
     super.init(frame: CGRect.zero)
+    self.setup()
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+
+extension EditToDoView {
+  private func setup() {
+    self.setupSubviewsLayout()
+  }
+}
+
+// MARK: About Layout
+
+extension EditToDoView {
+  private func setupSubviewsLayout() {
+    self.setupToDoNameInputViewLayout()
+  }
+
+  private func setupToDoNameInputViewLayout() {
+    self.addSubview(self.toDoNameInputView)
+    self.toDoNameInputView.translatesAutoresizingMaskIntoConstraints = false
+
+    let layout = EditToDoViewController.Constant.Layout.EditToDoView.ToDoNameTextInputView.self
+    NSLayoutConstraint.activate(
+      [
+        self.toDoNameInputView.topAnchor.constraint(
+          equalTo: self.topAnchor,
+          constant: layout.topMargin
+        ),
+        self.toDoNameInputView.leadingAnchor.constraint(
+          equalTo: self.leadingAnchor,
+          constant: layout.leadingMargin
+        ),
+        self.toDoNameInputView.trailingAnchor.constraint(
+          equalTo: self.trailingAnchor,
+          constant: -layout.trailingMargin
+        ),
+        self.toDoNameInputView.heightAnchor.constraint(equalToConstant: layout.height)
+      ]
+    )
   }
 }
