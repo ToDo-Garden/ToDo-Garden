0128b7d5f8527e26a94c4439c06ee57136ba5233
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-07-27 18:22:11 +0900

    #361: MockGroupWorker 그룹 목록 받아오는 함수 추가
    - MockGroupWorker 객체에 임의의 그룹 리스트 Mock Data를 가져오는 함수를 추가하였습니다.
    - 색상 데이터에 대한 명세가 없으므로, Hex String이 아닌 UIColor를 반환하도록 했습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/MockGroupWorker.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/MockGroupWorker.swift
@@ -9,6 +9,26 @@ import UIKit.UIColor
 
 import EditToDoSceneEntity
 
+/// Group Context를 가정한 Mock Worker 입니다.
 final class MockGroupWorker {
+  func fetchGroupList() throws -> [EditToDo.Group] {
+    return MockData.groupList
+  }
+}
 
+// MARK: Mock Data
+
+extension MockGroupWorker {
+  enum MockData {
+    static let groupList = [
+      EditToDo.Group(id: 001, name: "CS 지식", color: UIColor.toDoGardenBrown),
+      EditToDo.Group(id: 002, name: "영어", color: UIColor.toDoGardenRed),
+      EditToDo.Group(id: 003, name: "국어", color: UIColor.toDoGardenBlue),
+      EditToDo.Group(id: 004, name: "수학", color: UIColor.toDoGardenMint),
+      EditToDo.Group(id: 005, name: "Swift", color: UIColor.toDoGardenYellow),
+      EditToDo.Group(id: 006, name: "런닝", color: UIColor.toDoGardenPurple),
+      EditToDo.Group(id: 007, name: "지구과학", color: UIColor.toDoGardenGreenDark),
+      EditToDo.Group(id: 008, name: "물리", color: UIColor.toDoGardenOrange)
+    ]
+  }
 }
