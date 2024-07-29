1390d3b054fba0f2def2476676ca6b37fddbe3e1
SONG <geun2121@gmail.com>
2024-07-29 15:47:58 +0900

    #379: useCase 엔티티 정의
    - 컬러 수정
    - 페이로드 적용
    - 완료버튼 터치
    에 대한 요청/응답/뷰모델 구조체 정의

--- ToDo-Garden/ToDo-Garden/PostGroupScene/Sources/PostGroupSceneEntity/PostGroupModels.swift
+++ ToDo-Garden/ToDo-Garden/PostGroupScene/Sources/PostGroupSceneEntity/PostGroupModels.swift
@@ -5,21 +5,92 @@
 //  Created by SONG on 7/8/24.
 //  Copyright (c) 2024 ToDoGarden. All rights reserved.
 
-import Foundation
+import UIKit.UIColor
 
 public enum PostGroup {
   
   // MARK: Use cases
-  
-  public enum Something {
+  public enum ChangeColor {
     public struct Request {
-      public init() { }
+      public let groupColor: UIColor
+      public init(groupColor: UIColor) {
+        self.groupColor = groupColor
+      }
     }
     public struct Response {
-      public init() { }
+      public let groupID: String
+      public let groupColor: UIColor
+      public init(groupID: String, groupColor: UIColor) {
+        self.groupID = groupID
+        self.groupColor = groupColor
+      }
     }
     public struct ViewModel {
-      public init() { }
+      public let groupColor: UIColor
+      public init(groupColor: UIColor) {
+        self.groupColor = groupColor
+      }
+    }
+  }
+  
+  public enum SetPayload {
+    
+    public struct Response {
+      public let groupName: String
+      public let groupColor: UIColor?
+      public let isDoneBottomButtonEnable: Bool
+      public init(
+        groupName: String,
+        groupColor: UIColor?,
+        isDoneBottomButtonEnable: Bool
+      ) {
+        self.groupName = groupName
+        self.groupColor = groupColor
+        self.isDoneBottomButtonEnable = isDoneBottomButtonEnable
+      }
+    }
+    public struct ViewModel {
+      public let groupName: String
+      public let groupColor: UIColor?
+      public let isDoneBottomButtonEnable: Bool
+      public init(
+        groupName: String,
+        groupColor: UIColor?,
+        isDoneBottomButtonEnable: Bool
+      ) {
+        self.groupName = groupName
+        self.groupColor = groupColor
+        self.isDoneBottomButtonEnable = isDoneBottomButtonEnable
+      }
+    }
+  }
+  
+  public enum TouchDoneButton {
+    public struct Request {
+      public let groupName: String
+      public let groupColor: UIColor
+      public init(groupName: String, groupColor: UIColor) {
+        self.groupName = groupName
+        self.groupColor = groupColor
+      }
+    }
+    public struct Response {
+      public let groupID: String
+      public let groupName: String
+      public let groupColor: UIColor
+      public init(
+        groupID: String,
+        groupName: String,
+        groupColor: UIColor
+      ) {
+        self.groupID = groupID
+        self.groupName = groupName
+        self.groupColor = groupColor
+      }
+    }
+    public struct ViewModel {
+      public init() {
+      }
     }
   }
 }
