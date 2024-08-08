f7f2e33416c7b61e658d9c813d6b536d517f1eb0
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 14:43:03 +0900

    #405: 앱 정보 및 지원 뷰 객체 파일 추가
    - 앱 정보 및 지원에 사용되는 버튼들을 담는 뷰를 선언한 파일을 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/AppSupportView.swift
@@ -0,0 +1,19 @@
+//
+//  AppSupportView.swift
+//
+//
+//  Created by Wood on 8/8/24.
+//
+
+import UIKit
+
+final class AppSupportView: UIView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
