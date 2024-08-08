8f446500f57c317dda48f0ab8b9729bd18127ed8
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 22:10:52 +0900

    #414: EditToDoPresentationLogic Spy 객체 추가
    - present 메서드의 호출 여부와 response의 값을 검증하는 Spy 객체를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoInteractorTests.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoInteractorTests.swift
@@ -7,3 +7,45 @@
 
 @testable import EditToDoScene
 @testable import EditToDoSceneEntity
+
+class EditToDoPresentationLogicSpy: EditToDoPresentationLogic {
+  var fetchResult: Result<EditToDo.FetchToDo.Response.FetchedToDo, Error>?
+  var isPresentFetchedToDoCalled = false
+
+  var editResult: Result<Void, Error>?
+  var isPresentEditResultCalled = false
+
+  var deleteResult: Result<Void, Error>?
+  var isPresentDeleteResultCalled = false
+
+  var isPresentAlarmActivationCalled = false
+
+  var repetitionViewState: EditToDo.EditToDoRepetitionViewState?
+  var isPresentChangedRepetitionCalled = false
+
+  func presentFetchedToDo(response: EditToDo.FetchToDo.Response) {
+    self.fetchResult = response.fetchResult
+    self.isPresentFetchedToDoCalled = true
+  }
+
+  func presentEditResult(response: EditToDo.CompleteEditToDo.Response) {
+    self.editResult = response.editResult
+    self.isPresentEditResultCalled = true
+  }
+
+  func presentDeleteResult(response: EditToDo.DeleteToDo.Response) {
+    self.isPresentDeleteResultCalled = true
+    self.deleteResult = response.deleteResult
+  }
+
+  func presentAlarmActivation(response: EditToDo.ChangeAlarmActivation.Response) {
+    self.isPresentAlarmActivationCalled = true
+  }
+
+  func presentChangedRepetition(response: EditToDo.ChangeRepetition.Response) {
+    self.repetitionViewState = response.editToDoRepetitionViewState
+    self.isPresentChangedRepetitionCalled = true
+  }
+
+  func presentSomething(response: EditToDo.Something.Response) {}
+}
