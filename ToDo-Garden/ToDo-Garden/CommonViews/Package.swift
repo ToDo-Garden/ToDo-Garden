37170c1513ceea8b231c15669dba40f493f83dbf
GangWoon <gangwoon2018@gmail.com>
2024-08-08 17:48:13 +0900

    #410: Add common views package

---
+++ ToDo-Garden/ToDo-Garden/CommonViews/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+  name: "CommonViews",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "CommonViews",
+      targets: ["CommonViews"]
+    )
+  ],
+  dependencies: [
+    .package(path: "../ToDoGardenUI")
+  ],
+  targets: [
+    .target(
+      name: "CommonViews",
+      dependencies: [
+        .product(name: "ToDoGardenUIComponent", package: "ToDoGardenUI")
+      ]
+    )
+  ]
+)
