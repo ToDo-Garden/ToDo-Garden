a2d2f4e3442ebdf16f7fb0aed6f73e492babeaa5
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 13:27:32 +0900

    #405: SettingButtonStackView 추가
    - 여러개의 설정 버튼을 담는 스택 뷰를 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/SettingButtonStackView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingButtonStackView.swift
+//
+//
+//  Created by Wood on 8/8/24.
+//
+
+import UIKit
+
+public final class SettingButtonStackView: UIStackView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
