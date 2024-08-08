9f510d4e07c75633b88749ee8ae8c541a01093e4
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 14:06:42 +0900

    #405: 이용가이드 버튼 UI 설정 함수 추가
    - 이용가이드 UI 구성에 필요한 이미지 뷰와 레이블을 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/UserGuideButton.swift
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/UserGuideButton.swift
@@ -12,10 +12,75 @@ import ToDoGardenUIResource
 final class UserGuideButton: UIStackView {
   init() {
     super.init(frame: CGRect.zero)
+    self.setupUI()
   }
 
   @available(*, unavailable)
   required init(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: Private Functions
+extension UserGuideButton {
+  private func setupUI() {
+    self.setupStackViewUI()
+    self.setupEditIconImageView()
+    self.setupToDoGardenLogoImageView()
+    self.setupUserGuideLabel()
+    self.setupRightForwardImage()
+  }
+
+  private func setupStackViewUI() {
+    self.isLayoutMarginsRelativeArrangement = true
+    self.layoutMargins = UIEdgeInsets(top: 7, left: 15, bottom: 7, right: 8)
+    self.spacing = 5
+    self.backgroundColor = UIColor.toDoGardenGreenBackground
+    self.layer.cornerRadius = 10
+    self.layer.borderWidth = 1.0
+    self.layer.borderColor = UIColor.toDoGardenGreenGray.cgColor
+  }
+
+  private func setupEditIconImageView() {
+    let editIconImageView = UIImageView()
+    editIconImageView.image = UIImage.editIconImage.withTintColor(UIColor.toDoGardenGreenDark)
+    editIconImageView.setContentHuggingPriority(
+      UILayoutPriority.defaultHigh,
+      for: NSLayoutConstraint.Axis.horizontal
+    )
+    self.addArrangedSubview(editIconImageView)
+  }
+
+  private func setupToDoGardenLogoImageView() {
+    let userGuideImageView = UIImageView()
+    userGuideImageView.image = UIImage.toDoGardenLogoImage
+    userGuideImageView.setContentHuggingPriority(
+      UILayoutPriority.defaultHigh,
+      for: NSLayoutConstraint.Axis.horizontal
+    )
+    self.addArrangedSubview(userGuideImageView)
+  }
+
+  private func setupUserGuideLabel() {
+    let userGuideLabel = UILabel()
+    userGuideLabel.font = UIFont.pretendardDetailLight
+    userGuideLabel.textColor = UIColor.toDoGardenGreenDark
+    userGuideLabel.text = "이용 가이드"
+    userGuideLabel.textAlignment = NSTextAlignment.left
+    userGuideLabel.setContentHuggingPriority(
+      UILayoutPriority.defaultLow,
+      for: NSLayoutConstraint.Axis.horizontal
+    )
+    self.addArrangedSubview(userGuideLabel)
+  }
+
+  private func setupRightForwardImage() {
+    let rightForwardImageView = UIImageView()
+    rightForwardImageView.image = UIImage.forwardButtonImage
+    rightForwardImageView.setContentHuggingPriority(
+      UILayoutPriority.defaultHigh,
+      for: NSLayoutConstraint.Axis.horizontal
+    )
+    self.addArrangedSubview(rightForwardImageView)
   }
 }
