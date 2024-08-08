d34f5b8de122adbf3022ae2d185915b5f24bbabc
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 22:15:12 +0900

    #414: 테스트 데이터 추가
    - 테스트에 사용되는 데이터들을 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoSceneTestData.swift
@@ -0,0 +1,34 @@
+//
+//  EditToDoSceneTestData.swift
+//
+//
+//  Created by Wood on 8/8/24.
+//
+
+import UIKit
+
+@testable import EditToDoSceneEntity
+
+enum EditToDoSceneTestData {
+  enum Interactor {
+    static let toDo = EditToDo.ToDo(
+      name: "영단어 30개 외우기",
+      groupData: EditToDo.Group(id: 002, name: "영어", color: UIColor.toDoGardenRed),
+      alarm: EditToDo.ToDoAlarm(
+        isAlarmOn: false,
+        alarmTime: nil
+      ),
+      repetition: EditToDo.ToDoRepetition(
+        isOnlyToday: true,
+        isRepeatEveryday: false,
+        startDate: nil,
+        endDate: nil
+      )
+    )
+    static let groupList: [EditToDo.Group] = [
+      EditToDo.Group(id: 001, name: "CS 지식", color: UIColor.toDoGardenBrown),
+      EditToDo.Group(id: 002, name: "영어", color: UIColor.toDoGardenRed),
+      EditToDo.Group(id: 003, name: "국어", color: UIColor.toDoGardenBlue)
+    ]
+  }
+}
