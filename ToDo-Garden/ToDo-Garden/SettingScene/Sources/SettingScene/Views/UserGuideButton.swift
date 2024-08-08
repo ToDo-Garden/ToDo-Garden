97a40c06346fed9084dde3dceb015fa291f2f0d8
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 12:46:02 +0900

    #405 이용가이드 버튼 객체 생성
    - 이용가이드 버튼으로 사용되는 뷰를 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/UserGuideButton.swift
@@ -0,0 +1,21 @@
+//
+//  UserGuideButton.swift
+//
+//
+//  Created by Wood on 8/8/24.
+//
+
+import UIKit
+
+import ToDoGardenUIResource
+
+final class UserGuideButton: UIStackView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
