149fac2b6dea54380d5a3836a87df4085c96bd35
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-08 15:37:52 +0900

    #405: 버전 정보 뷰 파일 추가
    - 버전 정보를 표시하는 뷰 파일을 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/Views/VersionInfoView.swift
@@ -0,0 +1,19 @@
+//
+//  VersionInfoView.swift
+//  
+//
+//  Created by Wood on 8/8/24.
+//
+
+import UIKit
+
+final class VersionInfoView: UIView {
+  init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
