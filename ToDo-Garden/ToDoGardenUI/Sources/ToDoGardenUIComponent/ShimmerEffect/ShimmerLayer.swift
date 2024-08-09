5314def8ddc27b59d19735d777c47cfa1d0e4432
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-08-09 19:57:14 +0900

    #408: Color값 추가
    - ShimmerLayer에서 사용할 color값을 정의하였습니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ShimmerEffect/ShimmerLayer.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerLayer.swift
+//  ToDoGardenUI
+//
+//  Created by Noah on 8/9/24.
+//
+
+import UIKit
+
+struct ShimmerLayer {
+}
+
+extension ShimmerLayer {
+  struct Color {
+    static let gradientDarkGrey = UIColor(red: 239 / 255.0, green: 241 / 255.0, blue: 241 / 255.0, alpha: 1)
+    static let gradientLightGrey = UIColor(red: 201 / 255.0, green: 201 / 255.0, blue: 201 / 255.0, alpha: 1)
+  }
+}
