0892f28ce0dedb0e7a81c51cb0e14510649cc81d
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-08-10 15:41:29 +0900

    #408: startShimmering 메서드 테스트 코드 작성

--- ToDo-Garden/ToDoGardenUI/Tests/ShimmerEffectTests/ShimmerEffectTests.swift
+++ ToDo-Garden/ToDoGardenUI/Tests/ShimmerEffectTests/ShimmerEffectTests.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Noah on 8/10/24.
 //
+@testable import ToDoGardenUIComponent
 
 import Testing
 import UIKit
@@ -23,5 +24,23 @@ struct ShimmerEffectTests {
     
     view.isShimmering = false
     #expect(view.isShimmering == false)
+  }
+  
+  @Test
+  private func startShimmeringTests() {
+    let rootView = UIView()
+    let subview = UIView()
+    subview.isShimmering = true
+    
+    let subview2 = UIView()
+    subview2.isShimmering = true
+    
+    rootView.addSubview(subview)
+    rootView.addSubview(subview2)
+    
+    rootView.startShimmering()
+    
+    #expect(subview._shimmerLayer != nil)
+    #expect(subview2._shimmerLayer != nil)
   }
 }
