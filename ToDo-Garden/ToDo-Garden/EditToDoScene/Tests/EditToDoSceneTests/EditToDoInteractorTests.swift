f8706a64cb7504876910c69f0355a45326f13858
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-08-12 16:09:04 +0900

    #414: Mock 객체 Spy 로직 삭제
    - 리뷰를 반영하여 불필요한 코드 로직을 생성해야하는 Spy 로직을 제거했습니다.
    - Presenter 내부에 검증문 로직을 주입받을 수 있도록 수정하였습니다.
    - 관련 코드리뷰 링크: https://github.com/ToDo-Garden/ToDo-Garden/pull/415#discussion_r1711295493

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoInteractorTests.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Tests/EditToDoSceneTests/EditToDoInteractorTests.swift
@@ -5,285 +5,97 @@
 //  Created by Wood on 8/8/24.
 //
 
-import Testing
-import UIKit.UIColor
+import XCTest
 
 @testable import EditToDoScene
 @testable import EditToDoSceneEntity
 
-@Suite("EditToDoInteractor tests") struct EditToDoInteractorTest {
-  let mockToDoWorker: MockToDoWorkLogicSpy
-  let mockGroupWorker: MockGroupWorkLogicSpy
-  let interactor: EditToDoInteractor
-  let mockPresenter: EditToDoPresentationLogicSpy
+class MockToDoPresenter: EditToDoPresentationLogic {
+  var presentFetchToDoValidationClosure: ((
+    _ fetchedToDo: EditToDo.FetchToDo.Response.FetchedToDo?,
+    _ error: Error?
+  ) -> Void)?
+  var presentEditResultValidationClosure: ((_ error: Error?) -> Void)?
+  var presentDeleteResultValidationClosure: ((_ error: Error?) -> Void)?
+  var presentChangeAlarmValidationClosure: ((_ isAlarmOn: Bool) -> Void)?
+  var presentRepetitionValidationClosure: ((_ repetition: EditToDo.EditToDoRepetitionViewState) -> Void)?
 
-  init() {
-    self.mockToDoWorker = MockToDoWorkLogicSpy()
-    self.mockGroupWorker = MockGroupWorkLogicSpy()
-    self.mockPresenter = EditToDoPresentationLogicSpy()
-    self.interactor = EditToDoInteractor(
-      someWorker: EditToDoWorker(),
-      toDoWorker: self.mockToDoWorker,
-      groupWorker: self.mockGroupWorker
-    )
-    self.interactor.presenter = self.mockPresenter
+  init(
+    presentFetchToDoValidationClosure: ((
+      _ toDo: EditToDo.FetchToDo.Response.FetchedToDo?,
+      _ error: Error?
+    ) -> Void)? = { (_, _) in XCTFail("presentFetchResult unexpectedly called") },
+    presentEditResultValidationClosure: (
+      (_ error: Error?) -> Void)? = { _ in XCTFail("presentEditResult unexpectedly called") },
+    presentDeleteResultValidationClosure: (
+      (_ error: Error?) -> Void)? = { _ in XCTFail("presentDeleteResult unexpectedly called") },
+    presentChangeAlarmValidationClosure: (
+      (_ isAlarmOn: Bool) -> Void)? = { _ in XCTFail("presentChangeAlarm unexpectedly called") },
+    presentRepetitionValidationClosure: ((
+      _ repetition: EditToDo.EditToDoRepetitionViewState
+    ) -> Void)? = { _ in XCTFail("presentChangedRepetition unexpectedly called") }
+  ) {
+    self.presentFetchToDoValidationClosure = presentFetchToDoValidationClosure
+    self.presentEditResultValidationClosure = presentEditResultValidationClosure
+    self.presentDeleteResultValidationClosure = presentDeleteResultValidationClosure
+    self.presentChangeAlarmValidationClosure = presentChangeAlarmValidationClosure
+    self.presentRepetitionValidationClosure = presentRepetitionValidationClosure
   }
 
-  @Test(
-    "투두 데이터를 불러올 때 아이디가 있으면 ToDoWorker와 GroupWorker를 호출하고 EditToDoPresenter에 투두 데이터를 전달하는가"
-  ) func test_투두_데이터를_불러올때_아이디가_있으면_ToDoWorker와_GroupWorker를_호출하고_EditToDoPresenter에_투두데이터를_전달하는가() throws {
-    let request = EditToDo.FetchToDo.Request()
-    self.interactor.toDoId = 100
-    let expectedRepetitionViewState: EditToDo.EditToDoRepetitionViewState = .repeatOnlyToday
-
-    self.interactor.fetchToDo(request: request)
-
-    #expect(self.mockToDoWorker.isFetchToDoCalled)
-    #expect(self.mockGroupWorker.isFetchGroupListCalled)
-    let fetchResult = try #require(self.mockPresenter.fetchResult)
-    switch fetchResult {
+  func presentFetchedToDo(response: EditToDoSceneEntity.EditToDo.FetchToDo.Response) {
+    switch response.fetchResult {
     case .success(let fetchedToDo):
-      let toDo = try #require(self.interactor.toDo)
-      // TODO: fetchedToDo가 올바른지 20개의 변수가 올바른지 확인?
-      #expect(toDo == fetchedToDo.toDo)
+      self.presentFetchToDoValidationClosure?(fetchedToDo, nil)
     case .failure(let error):
-      #expect(Bool(false), "투두 데이터를 받아오는데 실패했습니다. error: \(error)")
+      self.presentFetchToDoValidationClosure?(nil, error)
     }
-    #expect(self.mockPresenter.isPresentFetchedToDoCalled)
   }
 
-  @Test(
-    "투두 데이터를 불러올 때 아이디가 없으면 ToDoWorker와 GroupWorker를 호출하지 않고 EditToDoPresenter에 에러를 전달하는가"
-  ) func test_투두_데이터를_불러올때_아이디가_없으면_ToDoWorker와_GroupWorker를_호출하지_않고_EditToDoPresenter에_에러를_전달하는가() throws {
-    let request = EditToDo.FetchToDo.Request()
-
-    self.interactor.fetchToDo(request: request)
-
-    #expect(self.mockToDoWorker.isFetchToDoCalled == false)
-    #expect(self.mockGroupWorker.isFetchGroupListCalled == false)
-    let fetchResult = try #require(mockPresenter.fetchResult)
-    switch fetchResult {
+  func presentEditResult(response: EditToDoSceneEntity.EditToDo.CompleteEditToDo.Response) {
+    switch response.editResult {
     case .success:
-      #expect(Bool(false), "투두 아이디가 존재하지 않는데 데이터를 불러왔습니다.")
+      self.presentEditResultValidationClosure?(nil)
     case .failure(let error):
-      let interactorError = try #require(error as? EditToDoInteractor.EditToDoInteractorError)
-      #expect(interactorError == EditToDoInteractor.EditToDoInteractorError.toDoDataNotExisted)
+      self.presentEditResultValidationClosure?(error)
     }
-    #expect(self.mockPresenter.isPresentFetchedToDoCalled)
   }
 
-  @Test(
-    "투두 수정 요청시 ToDoWorker를 호출하고 EditToDoPresenter에 성공을 전달하는가"
-  ) func test_투두_수정_요청시_ToDoWorker를_호출하고_EditToDoPresenter에_성공을_전달하는가() throws {
-    self.interactor.toDo = EditToDoSceneTestData.Interactor.toDo
-    let request = EditToDo.CompleteEditToDo.Request(
-      toDoName: "영어독해",
-      displayedGroup: EditToDo.CompleteEditToDo.Request.DisplayedGroup(id: 005, name: "국어", color: UIColor.white)
-    )
-
-    self.interactor.editToDo(request: request)
-
-    #expect(self.mockToDoWorker.isEditToDoCalled)
-    let editResult = try #require(self.mockPresenter.editResult)
-    switch editResult {
+  func presentDeleteResult(response: EditToDoSceneEntity.EditToDo.DeleteToDo.Response) {
+    switch response.deleteResult {
     case .success:
-      #expect(true)
+      self.presentDeleteResultValidationClosure?(nil)
     case .failure(let error):
-      #expect(Bool(false), "정상적인 투두 수정 요청이 실패했습니다. error: \(error)")
+      self.presentDeleteResultValidationClosure?(error)
     }
-    #expect(self.mockPresenter.isPresentEditResultCalled)
   }
 
-  @Test(
-    "투두 수정 요청시 투두가 존재하지 않으면 ToDoWorker를 호출하지 않고 EditToDoPresenter에 에러를 전달하는가"
-  ) func test_투두_수정_요청시_투두가_존재하지_않으면_ToDoWorker를_호출하지_않고_EditToDoPresenter에_에러를_전달하는가() throws {
-    let request = EditToDo.CompleteEditToDo.Request(
-      toDoName: "영어독해",
-      displayedGroup: EditToDo.CompleteEditToDo.Request.DisplayedGroup(id: 005, name: "국어", color: UIColor.white)
-    )
-
-    self.interactor.editToDo(request: request)
-
-    #expect(self.mockToDoWorker.isEditToDoCalled == false)
-    let editResult = try #require(self.mockPresenter.editResult)
-    switch editResult {
-    case .success:
-      #expect(Bool(false), "투두 데이터가 존재하지 않음에도 불구하고 수정 요청이 성공했습니다.")
-    case .failure(let someError):
-      let error = try #require(someError as? EditToDoInteractor.EditToDoInteractorError)
-      #expect(error == EditToDoInteractor.EditToDoInteractorError.toDoDataNotExisted)
-    }
-    #expect(self.mockPresenter.isPresentEditResultCalled)
+  func presentAlarmActivation(response: EditToDoSceneEntity.EditToDo.ChangeAlarmActivation.Response) {
+    self.presentChangeAlarmValidationClosure?(response.isAlarmOn)
   }
 
-  @Test(
-    "투두 삭제 요청시 ToDoWorker를 호출하고 EditToDoPresenter에 성공을 전달하는가"
-  ) func test_투두_삭제_요청시_ToDoWorker를_호출하고_EditToDoPresenter에_성공을_전달하는가() throws {
-    self.interactor.toDoId = 100
-    let request = EditToDo.DeleteToDo.Request()
-
-    self.interactor.deleteToDo(request: request)
-
-    #expect(self.mockToDoWorker.isDeleteToDoCalled)
-    let editResult = try #require(self.mockPresenter.deleteResult)
-    switch editResult {
-    case .success:
-      #expect(true)
-    case .failure(let error):
-      #expect(Bool(false), "정상적인 투두 삭제 요청이 실패했습니다. error: \(error)")
-    }
-    #expect(self.mockPresenter.isPresentDeleteResultCalled)
+  func presentChangedRepetition(response: EditToDoSceneEntity.EditToDo.ChangeRepetition.Response) {
+    self.presentRepetitionValidationClosure?(response.editToDoRepetitionViewState)
   }
 
-  @Test(
-    "투두 삭제 요청시 투두 ID가 존재하지 않으면 ToDoWorker를 호출하지 않고 EditToDoPresenter에 에러를 전달하는가"
-  ) func test_투두_삭제_요청시_투두_ID가_존재하지_않으면_ToDoWorker를_호출하지_않고_EditToDoPresenter에_에러를_전달하는가() throws {
-    let request = EditToDo.DeleteToDo.Request()
-
-    self.interactor.deleteToDo(request: request)
-
-    #expect(self.mockToDoWorker.isDeleteToDoCalled == false)
-    let editResult = try #require(self.mockPresenter.deleteResult)
-    switch editResult {
-    case .success:
-      #expect(Bool(false), "투두 ID가 존재하지 않음에도 불구하고 삭제 요청이 성공했습니다.")
-    case .failure(let someError):
-      let error = try #require(someError as? EditToDoInteractor.EditToDoInteractorError)
-      #expect(error == EditToDoInteractor.EditToDoInteractorError.toDoDataNotExisted)
-    }
-    #expect(self.mockPresenter.isPresentDeleteResultCalled)
-  }
-
-  @Test(
-    "투두 알림을 활성화 하면 EditToDoPresenter를 호출하는가"
-  ) func test_투두_알림을_활성화_하면_EditToDoPresenter를_호출하는가() throws {
-    var toDo = EditToDoSceneTestData.Interactor.toDo
-    toDo.alarm.isAlarmOn = false
-    self.interactor.toDo = toDo
-    let request = EditToDo.ChangeAlarmActivation.Request()
-
-    self.interactor.changeAlarmActivation(request: request)
-
-    let toDoData = try #require(self.interactor.toDo)
-    #expect(toDoData.alarm.isAlarmOn)
-    #expect(self.mockPresenter.isPresentAlarmActivationCalled)
-  }
-
-  @Test(
-    "투두를 매일 반복하도록 설정하면 EditToDoPresenter를 호출하는가"
-  ) func test_투두를_매일_반복하도록_설정하면_EditToDoPresenter를_호출하는가() throws {
-    var toDo = EditToDoSceneTestData.Interactor.toDo
-    toDo.repetition.isRepeatEveryday = false
-    self.interactor.toDo = toDo
-    let request = EditToDo.ChangeRepetition.Request(isOnlyToday: false, isEveryday: true)
-
-    self.interactor.changeReptition(request: request)
-
-    let toDoData = try #require(self.interactor.toDo)
-    #expect(toDoData.repetition.isRepeatEveryday)
-    #expect(self.mockPresenter.isPresentChangedRepetitionCalled)
-    #expect(self.mockPresenter.repetitionViewState == .repeatEveryday)
-  }
-
-  @Test(
-    "투두를 오늘만 반복하도록 설정하면 EditToDoPresenter를 호출하는가"
-  ) func test_투두를_오늘만_반복하도록_설정하면_EditToDoPresenter를_호출하는가() throws {
-    var toDo = EditToDoSceneTestData.Interactor.toDo
-    toDo.repetition.isOnlyToday = false
-    self.interactor.toDo = toDo
-    let request = EditToDo.ChangeRepetition.Request(isOnlyToday: true, isEveryday: nil)
-
-    self.interactor.changeReptition(request: request)
-
-    let toDoData = try #require(self.interactor.toDo)
-    #expect(toDoData.repetition.isOnlyToday)
-    #expect(self.mockPresenter.isPresentChangedRepetitionCalled)
-    #expect(self.mockPresenter.repetitionViewState == .repeatOnlyToday)
-  }
-
-  @Test(
-    "투두를 일정 기간동안 반복하도록 설정하면 EditToDoPresenter를 호출하는가"
-  ) func test_투두를_일정_기간동안_반복하도록_설정하면_EditToDoPresenter를_호출하는가() throws {
-    var toDo = EditToDoSceneTestData.Interactor.toDo
-    toDo.repetition.isRepeatEveryday = true
-    self.interactor.toDo = toDo
-    let request = EditToDo.ChangeRepetition.Request(isOnlyToday: false, isEveryday: false)
-
-    self.interactor.changeReptition(request: request)
-
-    let toDoData = try #require(self.interactor.toDo)
-    #expect(toDoData.repetition.isRepeatEveryday == false)
-    #expect(self.mockPresenter.isPresentChangedRepetitionCalled)
-    #expect(self.mockPresenter.repetitionViewState == .repeatInRange)
-  }
+  func presentSomething(response: EditToDoSceneEntity.EditToDo.Something.Response) {}
 }
 
-class EditToDoPresentationLogicSpy: EditToDoPresentationLogic {
-  var fetchResult: Result<EditToDo.FetchToDo.Response.FetchedToDo, Error>?
-  var isPresentFetchedToDoCalled = false
-
-  var editResult: Result<Void, Error>?
-  var isPresentEditResultCalled = false
-
-  var deleteResult: Result<Void, Error>?
-  var isPresentDeleteResultCalled = false
-
-  var isPresentAlarmActivationCalled = false
-
-  var repetitionViewState: EditToDo.EditToDoRepetitionViewState?
-  var isPresentChangedRepetitionCalled = false
-
-  func presentFetchedToDo(response: EditToDo.FetchToDo.Response) {
-    self.fetchResult = response.fetchResult
-    self.isPresentFetchedToDoCalled = true
-  }
-
-  func presentEditResult(response: EditToDo.CompleteEditToDo.Response) {
-    self.editResult = response.editResult
-    self.isPresentEditResultCalled = true
-  }
-
-  func presentDeleteResult(response: EditToDo.DeleteToDo.Response) {
-    self.isPresentDeleteResultCalled = true
-    self.deleteResult = response.deleteResult
-  }
-
-  func presentAlarmActivation(response: EditToDo.ChangeAlarmActivation.Response) {
-    self.isPresentAlarmActivationCalled = true
-  }
-
-  func presentChangedRepetition(response: EditToDo.ChangeRepetition.Response) {
-    self.repetitionViewState = response.editToDoRepetitionViewState
-    self.isPresentChangedRepetitionCalled = true
-  }
-
-  func presentSomething(response: EditToDo.Something.Response) {}
-}
-
-class MockGroupWorkLogicSpy: GroupWorkLogic {
-  var isFetchGroupListCalled: Bool = false
-
-  func fetchGroupList() throws -> [EditToDoSceneEntity.EditToDo.Group] {
-    self.isFetchGroupListCalled = true
-    return EditToDoSceneTestData.Interactor.groupList
-  }
-}
-
-class MockToDoWorkLogicSpy: ToDoWorkLogic {
-  var isFetchToDoCalled: Bool = false
-  var isEditToDoCalled: Bool = false
-  var isDeleteToDoCalled: Bool = false
-
-  func fetchToDo(id: Int?) throws -> EditToDo.ToDo {
-    self.isFetchToDoCalled = true
+struct MockToDoWorker: ToDoWorkLogic {
+  func fetchToDo(id: Int?) throws -> EditToDoSceneEntity.EditToDo.ToDo {
     return EditToDoSceneTestData.Interactor.toDo
   }
 
-  func editToDo(_ toDo: EditToDo.ToDo) throws {
-    self.isEditToDoCalled = true
+  func deleteToDo(id: Int?) throws {
+
   }
 
-  func deleteToDo(id: Int?) throws {
-    self.isDeleteToDoCalled = true
+  func editToDo(_ toDo: EditToDoSceneEntity.EditToDo.ToDo) throws {
+
+  }
+}
+
+struct MockGroupWorker: GroupWorkLogic {
+  func fetchGroupList() throws -> [EditToDoSceneEntity.EditToDo.Group] {
+    return EditToDoSceneTestData.Interactor.groupList
   }
 }
