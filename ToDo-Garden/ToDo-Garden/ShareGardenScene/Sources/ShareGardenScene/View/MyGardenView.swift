aaccdc1b4d15bf0a677c8838526eff1f39bdfaea
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-08-13 13:40:55 +0900

    #298: Stackview 속성 설정

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/MyGardenView.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/MyGardenView.swift
@@ -12,11 +12,25 @@ extension ShareGardenSceneViewController {
     
     init() {
       super.init(frame: CGRect.zero)
+      self.setup()
     }
     
     @available(*, unavailable)
     required init(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
     }
+  }
+}
+
+extension ShareGardenSceneViewController.MyGardenView {
+  private func setup() {
+    self.setupStackView()
+  }
+  
+  private func setupStackView() {
+    self.spacing = 14
+    self.distribution = UIStackView.Distribution.fillEqually
+    self.axis = NSLayoutConstraint.Axis.vertical
+    self.alignment = UIStackView.Alignment.center
   }
 }
