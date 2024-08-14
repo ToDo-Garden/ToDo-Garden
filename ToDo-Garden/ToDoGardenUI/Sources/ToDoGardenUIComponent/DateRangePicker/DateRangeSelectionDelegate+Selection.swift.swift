97cd4085475d1761713d9affbeea9037f590b4a2
SONG <geun2121@gmail.com>
2024-08-14 17:32:27 +0900

    #400: updateSelectionState() 정의
    - 시작점, 끝점에 날짜가 할당되어있는지 여부로 현재 범위 선택 상태를 결정합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+Selection.swift.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+Selection.swift.swift
@@ -7,5 +7,15 @@
 import Foundation
 
 extension DateRangeSelectionDelegate {
-  
+  func updateSelectionState() {
+    if startDate != nil {
+      if endDate != nil {
+        self.currentSelectionState = RangeSelectionState.startAndEnd
+      } else {
+        self.currentSelectionState = RangeSelectionState.startOnly
+      }
+    } else {
+      self.currentSelectionState = RangeSelectionState.empty
+    }
+  }
 }
