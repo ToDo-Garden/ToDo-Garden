7361972aee44a5e74af47d6c98c4ec45dd3ba43c
SONG <geun2121@gmail.com>
2024-08-14 17:14:14 +0900

    #400: RangeSelectionState 파일 추가
    - RangeSelectionState 열거형 파일을 추가합니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/RangeSelectionState.swift
@@ -0,0 +1,14 @@
+//
+//  RangeSelectionState.swift
+//
+//
+//  Created by SONG on 8/9/24.
+//
+
+import Foundation
+
+enum RangeSelectionState {
+  case empty
+  case startOnly
+  case startAndEnd
+}
