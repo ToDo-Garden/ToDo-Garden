5a9810ebb57488b8918fdbaf32a5c57e9d1097f3
SONG <geun2121@gmail.com>
2024-08-14 17:31:33 +0900

    #400: getIndexPaths() 정의
    - CalendarItem의 date를 기준으로 같은 날짜를 가진 cell의 인덱스패스를 탐색합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+IndexPathUtils.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+IndexPathUtils.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 extension DateRangeSelectionDelegate {
-
+  func getIndexPaths(of item: CalendarItem) -> [IndexPath] {
+    var foundIndexPaths: [IndexPath] = []
+    
+    for section in 0..<self.collectionView.numberOfSections {
+      for itemIndex in 0..<self.collectionView.numberOfItems(inSection: section) {
+        let indexPath = IndexPath(item: itemIndex, section: section)
+        if let calendarItem = self.collectionViewDataSource.itemIdentifier(for: indexPath),
+          Calendar.current.isDate(calendarItem.date, inSameDayAs: item.date) {
+          foundIndexPaths.append(indexPath)
+          
+          if foundIndexPaths.count == 2 {
+            return foundIndexPaths
+          }
+        }
+      }
+    }
+    return foundIndexPaths
+  }
 }
