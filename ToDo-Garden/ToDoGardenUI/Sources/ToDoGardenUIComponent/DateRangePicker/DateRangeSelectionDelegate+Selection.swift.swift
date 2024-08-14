aa2a07d4ec7c30a3d37e8c541cad01c9bd98d42e
SONG <geun2121@gmail.com>
2024-08-14 17:34:13 +0900

    #400: updateVisibleSelection() 정의
    - 각 섹션별로 "선택" 상태로 표시될 Cell에 UI업데이트를 진행합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+Selection.swift.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/DateRangePicker/DateRangeSelectionDelegate+Selection.swift.swift
@@ -18,4 +18,107 @@ extension DateRangeSelectionDelegate {
       self.currentSelectionState = RangeSelectionState.empty
     }
   }
+  
+  func updateVisibleSelection(isAfterReload: Bool = false) {
+    guard let startItem = startDate else { return }
+    
+    let visibleCells = self.getVisibleCells(isAfterReload: isAfterReload)
+    let startIndexPaths = self.getIndexPaths(of: startItem)
+    
+    if let endItem = endDate {
+      self.updateRangeSelection(
+        visibleCells: visibleCells,
+        startItem: startItem,
+        endItem: endItem,
+        startIndexPaths: startIndexPaths
+      )
+    } else {
+      self.updateSingleSelection(visibleCells: visibleCells, startIndexPaths: startIndexPaths)
+    }
+  }
+  private func getVisibleCells(isAfterReload: Bool) -> [DateRangeCollectionViewCell] {
+    if isAfterReload {
+      let newSection = 3
+      let numberOfItems = self.collectionView.numberOfItems(inSection: newSection)
+      return (0..<numberOfItems).compactMap { index in
+        self.collectionView.cellForItem(
+          at: IndexPath(item: index, section: newSection)
+        ) as? DateRangeCollectionViewCell
+      }
+    } else {
+      return self.collectionView.visibleCells.compactMap { $0 as? DateRangeCollectionViewCell }
+    }
+  }
+  
+  private func updateSingleSelection(
+    visibleCells: [DateRangeCollectionViewCell],
+    startIndexPaths: [IndexPath]
+  ) {
+    for cell in visibleCells {
+      guard let indexPath = self.collectionView.indexPath(for: cell) else { continue }
+      if startIndexPaths.contains(indexPath) {
+        self.collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+        cell.selectionType = DateRangeCollectionViewCell.SelectionType.none
+      } else {
+        self.collectionView.deselectItem(at: indexPath, animated: false)
+        cell.selectionType = DateRangeCollectionViewCell.SelectionType.none
+      }
+    }
+  }
+  
+  private func updateRangeSelection(
+    visibleCells: [DateRangeCollectionViewCell],
+    startItem: CalendarItem,
+    endItem: CalendarItem,
+    startIndexPaths: [IndexPath]
+  ) {
+    let endIndexPaths = self.getIndexPaths(of: endItem)
+    let selectionInfo = SelectionInfo(
+      startItem: startItem,
+      endItem: endItem,
+      startIndexPaths: startIndexPaths,
+      endIndexPaths: endIndexPaths
+    )
+    
+    for cell in visibleCells {
+      guard let indexPath = self.collectionView.indexPath(for: cell),
+        let calendarItem = self.collectionViewDataSource.itemIdentifier(for: indexPath) else { continue }
+      
+      self.updateCellForRangeSelection(
+        cell: cell,
+        indexPath: indexPath,
+        calendarItem: calendarItem,
+        selectionInfo: selectionInfo
+      )
+    }
+  }
+  
+  private func updateCellForRangeSelection(
+    cell: DateRangeCollectionViewCell,
+    indexPath: IndexPath,
+    calendarItem: CalendarItem,
+    selectionInfo: SelectionInfo
+  ) {
+    if calendarItem.date >= selectionInfo.startItem.date
+      && calendarItem.date <= selectionInfo.endItem.date {
+      self.collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+      if selectionInfo.startIndexPaths.contains(indexPath) {
+        cell.selectionType = DateRangeCollectionViewCell.SelectionType.right
+      } else if selectionInfo.endIndexPaths.contains(indexPath) {
+        cell.selectionType = DateRangeCollectionViewCell.SelectionType.left
+      } else {
+        cell.selectionType = DateRangeCollectionViewCell.SelectionType.full
+      }
+    } else {
+      self.collectionView.deselectItem(at: indexPath, animated: false)
+      cell.selectionType = DateRangeCollectionViewCell.SelectionType.none
+    }
+  }
+  
+  private struct SelectionInfo {
+    let startItem: CalendarItem
+    let endItem: CalendarItem
+    let startIndexPaths: [IndexPath]
+    let endIndexPaths: [IndexPath]
+  }
 }
