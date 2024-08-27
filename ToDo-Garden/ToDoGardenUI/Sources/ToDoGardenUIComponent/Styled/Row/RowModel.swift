ac069cad4953ceb7c8ab372653d2af9aee445dd4
GangWoon <gangwoon2018@gmail.com>
2024-08-27 14:56:54 +0900

    #439: Implement different profile image size

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/RowModel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/RowModel.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 extension Styled.Row {
@@ -50,7 +51,8 @@ extension Styled.Row.Configuration {
         titleFont: UIFont.pretendardHeadBold,
         description: description,
         descriptionFont: UIFont.pretendardDetailLight,
-        axis: NSLayoutConstraint.Axis.vertical
+        axis: NSLayoutConstraint.Axis.vertical,
+        profileSize: Constant.Styled.Row.Profile.shareProfileSize
       )
     }
     
@@ -65,7 +67,8 @@ extension Styled.Row.Configuration {
         titleFont: UIFont.pretendardBodySemiBold,
         description: description,
         descriptionFont: UIFont.pretendardBodyMedium,
-        axis: NSLayoutConstraint.Axis.horizontal
+        axis: NSLayoutConstraint.Axis.horizontal,
+        profileSize: Constant.Styled.Row.Profile.friendListProfileSize
       )
     }
     var image: UIImage = UIImage.defaultFriendProfileImage
@@ -74,6 +77,7 @@ extension Styled.Row.Configuration {
     var description: String
     var descriptionFont: UIFont
     var axis: NSLayoutConstraint.Axis
+    var profileSize: CGSize
   }
   
   public struct ListPrimaryModel: Equatable {
