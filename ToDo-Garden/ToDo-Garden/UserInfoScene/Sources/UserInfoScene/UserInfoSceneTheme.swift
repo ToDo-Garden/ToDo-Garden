e709f32a7b59005adbfea4193487bb9ae553792c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-09-02 17:06:03 +0900

    #453: UserInfoSceneTheme 추가
    - 유저 정보화면에서 동적으로 변하는 값들을 담는 테마 레이어를 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/UserInfoScene/Sources/UserInfoScene/UserInfoSceneTheme.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoSceneTheme.swift
+//
+//
+//  Created by Wood on 9/2/24.
+//
+
+import Foundation
+
+enum UserInfoSceneTheme {
+  enum StringLiteral {}
+}
+
+extension UserInfoSceneTheme.StringLiteral {
+  static let title = "프로필"
+}
