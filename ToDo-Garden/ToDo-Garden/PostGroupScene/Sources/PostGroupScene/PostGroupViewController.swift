5f555e4c8c53b892bdb3b247998888c571236025
SONG <geun2121@gmail.com>
2024-09-05 15:53:55 +0900

    #463: 라이프사이클 관련 메서드 변경
    - 관련리뷰 : https://github.com/ToDo-Garden/ToDo-Garden/pull/465#discussion_r1744881182

--- ToDo-Garden/ToDo-Garden/PostGroupScene/Sources/PostGroupScene/PostGroupViewController.swift
+++ ToDo-Garden/ToDo-Garden/PostGroupScene/Sources/PostGroupScene/PostGroupViewController.swift
@@ -56,8 +56,8 @@ final class PostGroupViewController: UIViewController, PostGroupViewControllable
   
   // MARK: - View lifecycle
   
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+  override func viewIsAppearing(_ animated: Bool) {
+    super.viewIsAppearing(animated)
     self.loadGroupData()
   }
   
