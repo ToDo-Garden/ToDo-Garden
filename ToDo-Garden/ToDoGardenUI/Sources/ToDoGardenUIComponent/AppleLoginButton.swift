9eed7ed0652d42848e3209ddc4619e4c8e5b83ab
SONG <geun2121@gmail.com>
2024-09-09 15:29:04 +0900

    #473: 파일 추가
    - AppleLoginButton파일을 추가합니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AppleLoginButton.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+import ToDoGardenUIConstant
+import ToDoGardenUIResource
+
+public final class AppleLoginButton: UIButton {
+  
+}
+
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview {
+  return AppleLoginButton()
+}
+#endif
