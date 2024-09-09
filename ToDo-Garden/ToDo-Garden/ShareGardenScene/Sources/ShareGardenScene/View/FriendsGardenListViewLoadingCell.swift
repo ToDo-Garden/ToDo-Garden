26095ef960df63bc56b8206701031f6e838612dd
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-09-09 19:24:37 +0900

    #298: available attribute 추가
    - 팀 합의로 사용하지 않을 init에 attribute를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/FriendsGardenListViewLoadingCell.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/FriendsGardenListViewLoadingCell.swift
@@ -62,6 +62,7 @@ extension ShareGardenSceneViewController.FriendsGardenView {
       self.setup()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
     }
