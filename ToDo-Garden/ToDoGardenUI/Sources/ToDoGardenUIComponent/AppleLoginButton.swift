40e61eeb91fd29fb39f1b00428668b83bea73a8c
SONG <geun2121@gmail.com>
2024-09-09 15:29:54 +0900

    #473: 초기화문 정의
    - init() 을 정의합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AppleLoginButton.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AppleLoginButton.swift
@@ -4,7 +4,19 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 public final class AppleLoginButton: UIButton {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setupButton()
+  }
   
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    fatalError()
+  }
+  
+  private func setupButton() {
+  }
 }
 
 #if DEBUG
