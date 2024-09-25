a4814bf99ecee6424eda5edb9e6b97be43e579c7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-09-25 15:22:03 +0900

    #441: ApplicationServiceWorker 버전 업데이트 가능 여부 메서드 주석 변경
    - 업데이트 확인시 서버가 아닌 네트워크 통신의 필요성으로 주석을 변경하였습니다.
    - 관련 코드리뷰 링크: https://github.com/ToDo-Garden/ToDo-Garden/pull/494#discussion_r1773490478

--- ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/ApplicationServiceWorker.swift
+++ ToDo-Garden/ToDo-Garden/SettingScene/Sources/SettingScene/ApplicationServiceWorker.swift
@@ -15,7 +15,7 @@ public struct ApplicationServiceWorker {
   }
 
   /// 버전 업데이트 필요 여부를 반환하는 메서드입니다.
-  /// 서버 요청이 필요하므로, Network Service 레이어 구현이 완료되면 추가 구현할 예정입니다.
+  /// 네트워크 통신이 필요하므로, Network Service 레이어 구현이 완료되면 추가 구현할 예정입니다.
   func isUpdateAvailable() async -> Bool {
     return false
   }
