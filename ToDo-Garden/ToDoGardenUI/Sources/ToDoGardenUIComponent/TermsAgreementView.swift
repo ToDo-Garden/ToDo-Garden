d73334ee9ce595d159b791f85ca1823b1ba8a7ac
SONG <geun2121@gmail.com>
2024-09-25 16:00:40 +0900

    #493: init() 및 프로퍼티 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TermsAgreementView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TermsAgreementView.swift
@@ -9,6 +9,34 @@ import UIKit
 import ToDoGardenUIConstant
 
 public final class TermsAgreementView: UIView {
-
+  // MARK: - Properties
+  private let agreeToAllRow: TermsAgreementViewRow
+  private let termsAndPoliciesAgreementRow: TermsAgreementViewRow
+  private let privacyPolicyRow: TermsAgreementViewRow
+  private let eventAndPromotionalInformationRow: TermsAgreementViewRow
+  
+  private let doneButton: ToDoGardenBoxButton
+  private var doneButtonCompletion: ((Bool) -> Void)?
+  
+  // MARK: - Initialization
+  public init() {
+    self.agreeToAllRow = TermsAgreementViewRow(chevronButtonIsHidden: true)
+    self.termsAndPoliciesAgreementRow = TermsAgreementViewRow(chevronButtonIsHidden: false)
+    self.privacyPolicyRow = TermsAgreementViewRow(chevronButtonIsHidden: false)
+    self.eventAndPromotionalInformationRow = TermsAgreementViewRow(chevronButtonIsHidden: false)
+    self.doneButton = ToDoGardenBoxButton(
+      title: Constant.TermsAgreementView.StringLiteral.done,
+      buttonType: ToDoGardenBoxButton.Configuration.tertiaryRoundRectButton
+    )
+    
+    super.init(frame: CGRect.zero)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  override public var intrinsicContentSize: CGSize {
+    return Constant.TermsAgreementView.Layout.size
+  }
 }
-
--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TermsAgreementViewRow.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TermsAgreementViewRow.swift
@@ -11,6 +11,39 @@ import ToDoGardenUIConstant
 import ToDoGardenUIResource
 
 final class TermsAgreementViewRow: UIView {
-
+  // MARK: - Properties
+  let checkButton: UIButton
+  private let textLabel: UILabel
+  private let chevronButton: UIButton
+  
+  var isSelected: Bool {
+    get { return self.checkButton.isSelected }
+    set { self.checkButton.isSelected = newValue }
+  }
+  
+  var chevronAction: (() -> Void)?
+  
+  override public var intrinsicContentSize: CGSize {
+    return Constant.TermsAgreementViewRow.Layout.size
+  }
+  
+  // MARK: - Initialization
+  init(chevronButtonIsHidden: Bool) {
+    self.checkButton = UIButton(type: UIButton.ButtonType.custom)
+    self.textLabel = UILabel()
+    self.chevronButton = UIButton(type: UIButton.ButtonType.system)
+    
+    super.init(frame: CGRect.zero)
+    self.setupViews()
+    self.chevronButton.isHidden = chevronButtonIsHidden
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  // MARK: - UISetup
+  private func setupViews() {
+  }
 }
-
