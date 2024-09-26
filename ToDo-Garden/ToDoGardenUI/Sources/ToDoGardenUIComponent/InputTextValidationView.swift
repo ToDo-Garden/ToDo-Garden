55c4f3932f48c8c48d29e323ad38d2881cfb4ac3
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-09-26 15:15:02 +0900

    #502: Model 객체 추가
    - 입력할 텍스트 종류를 설정할 수 있도록 Model 객체를 생성하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/InputTextValidationView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/InputTextValidationView.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+import ToDoGardenUIConstant
+
 public final class InputTextValidationView: UIView {
-  public init() {
+  private let model: Model
+
+  public init(model: Model) {
+    self.model = model
     super.init(frame: CGRect.zero)
   }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+public extension InputTextValidationView {
+  struct Model {
+    public let inputText: TextInputView.Model
+    public let validationText: String
+
+    public init(inputText: String, validationText: String) {
+      self.inputText = TextInputView.Model(inputText: inputText)
+      self.validationText = validationText
+    }
+
+    public static let id = Self(
+      inputText: TextInputView.Model.userId.inputText,
+      validationText: Constant.InputTextValidationView.StringLiteral.ValidationText.invalidID
+    )
+    public static let nickname = Self(
+      inputText: TextInputView.Model.userNickname.inputText,
+      validationText: Constant.InputTextValidationView.StringLiteral.ValidationText.invalidNickname
+    )
+    public static let introduction = Self(
+      inputText: TextInputView.Model.userDescription.inputText,
+      validationText: Constant.InputTextValidationView.StringLiteral.ValidationText.invalidIntroduction
+    )
   }
 }
