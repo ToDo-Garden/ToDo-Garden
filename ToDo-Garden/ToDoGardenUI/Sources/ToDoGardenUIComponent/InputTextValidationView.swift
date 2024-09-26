faaffee7f3aa549a7a9dbb5e0b47407c1d556271
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-09-26 14:28:54 +0900

    #502: InputTextValidationView 파일 추가
    - TextInputView와 validation 문구를 지닌 컴포넌트를 구현한 파일을 생성합니다.
    - 관련 코드리뷰 링크: https://github.com/ToDo-Garden/ToDo-Garden/pull/504#pullrequestreview-2330088842

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/InputTextValidationView.swift
@@ -0,0 +1,19 @@
+//
+//  InputTextValidationView.swift
+//
+//
+//  Created by Wood on 9/26/24.
+//
+
+import UIKit
+
+public final class InputTextValidationView: UIView {
+  public init() {
+    super.init(frame: CGRect.zero)
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
