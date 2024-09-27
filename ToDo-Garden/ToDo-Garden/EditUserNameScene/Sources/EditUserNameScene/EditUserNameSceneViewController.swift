2c54cc2f14ffc029defa428376b2aabdbc2afa22
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-09-27 17:51:56 +0900

    #500: 수정 완료 추가
    - 수정 완료 Navigation BarButtonItem을 추가하였습니다.
    - Figma 디자인에 맞게 UI를 설정하였습니다.

--- ToDo-Garden/ToDo-Garden/EditUserNameScene/Sources/EditUserNameScene/EditUserNameSceneViewController.swift
+++ ToDo-Garden/ToDo-Garden/EditUserNameScene/Sources/EditUserNameScene/EditUserNameSceneViewController.swift
@@ -15,16 +15,16 @@ protocol EditUserNameSceneDisplayLogic: AnyObject {
   func displaySomething(viewModel: EditUserNameScene.Something.ViewModel)
 }
 
-class EditUserNameSceneViewController: UIViewController, EditUserNameSceneViewControllable {
-  
-  // MARK: - VIP Properties
-  
+final class EditUserNameSceneViewController: UIViewController, EditUserNameSceneViewControllable {
+  private let editUserNameButton: UIBarButtonItem
+
   var interactor: EditUserNameSceneBusinessLogic?
   var router: (EditUserNameSceneRoutingLogic & EditUserNameSceneDataPassing)?
   
   // MARK: - Object lifecycle
   
   init() {
+    self.editUserNameButton = UIBarButtonItem()
     super.init(nibName: nil, bundle: nil)
   }
   
@@ -63,12 +63,30 @@ extension EditUserNameSceneViewController {
 
 extension EditUserNameSceneViewController {
   private func setupUI() {
-    setupMainView()
+    self.setupMainView()
+    self.setupEditUserNameButton()
   }
 
   private func setupMainView() {
     self.title = Constant.StringLiteral.title
     self.view.backgroundColor = UIColor.toDoGardenWhite
+  }
+
+  private func setupEditUserNameButton() {
+    let horizontalOffset = Constant.Layout.EditUserNameButton.titleHorizontalOffset
+    self.editUserNameButton.setTitlePositionAdjustment(
+      UIOffset(horizontal: -horizontalOffset, vertical: 0),
+      for: UIBarMetrics.default
+    )
+    self.editUserNameButton.setTitleTextAttributes(
+      [
+        NSAttributedString.Key.foregroundColor: EditUserNameSceneTheme.mainColor,
+        NSAttributedString.Key.font: UIFont.pretendardBodyRegular
+      ],
+      for: UIControl.State.normal
+    )
+    self.editUserNameButton.title = Constant.StringLiteral.EditUserNameButton.title
+    self.navigationItem.rightBarButtonItem = self.editUserNameButton
   }
 }
 
--- ToDo-Garden/ToDo-Garden/EditUserNameScene/Sources/EditUserNameScene/Views/EditUserNameSceneViewController+Constant.swift
+++ ToDo-Garden/ToDo-Garden/EditUserNameScene/Sources/EditUserNameScene/Views/EditUserNameSceneViewController+Constant.swift
@@ -12,9 +12,20 @@ extension EditUserNameSceneViewController {
 }
 
 extension EditUserNameSceneViewController.Constant {
+  enum Layout {}
   enum StringLiteral {}
 }
 
+extension EditUserNameSceneViewController.Constant.Layout {
+  enum EditUserNameButton {
+    static let titleHorizontalOffset: CGFloat = 10
+  }
+}
+
 extension EditUserNameSceneViewController.Constant.StringLiteral {
+  enum EditUserNameButton {
+    static let title = "완료"
+  }
+
   static let title = "닉네임 변경"
 }
