d1aac0cdaf5bf65907926f58f890ed67dbefc6d1
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-09-30 18:53:46 +0900

    #299: AsyncSequence 순회 코드 추가

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/ShareGardenSceneInteractor.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/ShareGardenSceneInteractor.swift
@@ -19,15 +19,18 @@ protocol ShareGardenSceneBusinessLogic {
 final class ShareGardenSceneInteractor: ShareGardenSceneDataStore {
   var presenter: ShareGardenScenePresentationLogic?
   private let shareGardenSceneWorker: ShareGardenSceneWorkable
-  
+  private let friendsGardenStore: FriendsGardenDataStore
 
   private var tasks: [TaskKey: Task<Void, Never>] = [:]
 
   enum TaskKey {
+    case observeFriendsGardenStoreStream
   }
 
   init(shareGardenSceneWorker: ShareGardenSceneWorkable) {
     self.shareGardenSceneWorker = shareGardenSceneWorker
+    self.friendsGardenStore = FriendsGardenDataStore()
+    self.observeFriendsGardenStoreStream()
   }
 }
 
@@ -38,5 +41,19 @@ extension ShareGardenSceneInteractor: ShareGardenSceneBusinessLogic {
 
 extension ShareGardenSceneInteractor: FriendsGardenStore {
   func fetch(by id: ShareGardenSceneEntity.ShareGardenScene.FriendsGarden.ID) -> ShareGardenScene.FriendsGarden? {
+
+extension ShareGardenSceneInteractor {
+  private func observeFriendsGardenStoreStream() {
+    self.tasks[TaskKey.observeFriendsGardenStoreStream] = Task { [weak self] in
+      guard let stream = await self?.friendsGardenStore.stream
+      else { return }
+      defer { self?.tasks[TaskKey.observeFriendsGardenStoreStream] = nil }
+
+      for await friendsGardens in stream {
+        // stream에서 받은 값을 기반으로 뷰 업데이트
+        // let response = ShareGardenScene.RequestFriendsGardenList.Response(friendsGardenList: friendsGardens)
+        // await self?.presenter?.presentFriendsGardens(response: response)
+      }
+    }
   }
 }
