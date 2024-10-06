4096f3e1315490273331ba36d4a342f0942f2014
SONG <geun2121@gmail.com>
2024-10-06 18:32:47 +0900

    #539: VIP 템플릿 추가

---
+++ ToDo-Garden/ToDo-Garden/InputIDScene/Sources/InputIDScene/InputIDRouter.swift
@@ -0,0 +1,46 @@
+//
+//  InputIDRouter.swift
+//  
+//
+//  Created by SONG on 10/6/24.
+//  Copyright (c) 2024 ToDoGarden. All rights reserved.
+
+import Foundation
+
+import InputIDSceneAPI
+
+protocol InputIDRoutingLogic {
+  func routeToSomewhere()
+}
+
+protocol InputIDDataPassing {
+  var dataStore: InputIDDataStore? { get }
+}
+
+class InputIDRouter: InputIDDataPassing {
+  weak var viewController: InputIDViewController?
+  var dataStore: InputIDDataStore?
+  private let nextSceneBuilder: NextSceneBuildable
+  
+  init(nextSceneBuilder: NextSceneBuildable) {
+    self.nextSceneBuilder = nextSceneBuilder
+  }
+}
+
+// MARK: - Routing
+
+extension InputIDRouter: InputIDRoutingLogic {
+  func routeToSomewhere() {
+    let destinationViewController = self.nextSceneBuilder.build(with: NextScenePayload())
+    
+    self.viewController?.present(destinationViewController, animated: true)
+  }
+}
+
+// MARK: - Declare Payload for scene
+
+extension InputIDRouter {
+  struct NextScenePayload: NextScenePayloadable {
+    // var name: String
+  }
+}
---
+++ ToDo-Garden/ToDo-Garden/InputIDScene/Sources/InputIDScene/InputIDWorker.swift
@@ -0,0 +1,15 @@
+//
+//  InputIDWorker.swift
+//  
+//
+//  Created by SONG on 10/6/24.
+//  Copyright (c) 2024 ToDoGarden. All rights reserved.
+
+import Foundation
+
+import InputIDSceneAPI
+
+struct InputIDWorker: InputIDWorkable {
+  func doSomeWork() {
+  }
+}
