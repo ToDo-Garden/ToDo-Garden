d1a7a889bc364d4e768aa90cdf8018443c0ac688
SONG <geun2121@gmail.com>
2024-10-07 19:17:52 +0900

    #533: AppleLoginManager 파일 추가
    - ASAuthorizationController와 ASAuthorizationControllerDelegate 등을 다루는 객체를 생성하여 추가합니다.

--- ToDo-Garden/ToDo-Garden/LoginScene/Sources/LoginScene/AppleLoginManager.swift
+++ ToDo-Garden/ToDo-Garden/LoginScene/Sources/LoginScene/AppleLoginManager.swift
@@ -1,8 +1,55 @@
 //
-//  File.swift
-//  
+//  AppleLoginManager.swift
 //
-//  Created by SONG on 10/7/24.
+//
+//  Created by SONG on 10/4/24.
 //
 
+import AuthenticationServices
 import Foundation
+
+protocol AppleLoginManagerDelegate: AnyObject {
+  func appleLoginDidComplete(with result: Result<ASAuthorizationAppleIDCredential, Error>)
+}
+
+final class AppleLoginManager: NSObject {
+  private var authController: ASAuthorizationController?
+  private weak var presentationContextProvider: ASAuthorizationControllerPresentationContextProviding?
+  weak var delegate: AppleLoginManagerDelegate?
+  
+  init(presentationContextProvider: ASAuthorizationControllerPresentationContextProviding) {
+    super.init()
+    self.presentationContextProvider = presentationContextProvider
+    self.setupAuthController()
+  }
+  
+  private func setupAuthController() {
+    let provider = ASAuthorizationAppleIDProvider()
+    let request = provider.createRequest()
+    
+    request.requestedScopes = [ASAuthorization.Scope.email]
+    
+    self.authController = ASAuthorizationController(authorizationRequests: [request])
+    self.authController?.delegate = self
+    self.authController?.presentationContextProvider = self.presentationContextProvider
+  }
+  
+  func performAppleLogin() {
+    self.authController?.performRequests()
+  }
+}
+
+extension AppleLoginManager: ASAuthorizationControllerDelegate {
+  func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithAuthorization authorization: ASAuthorization
+  ) {
+    if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+      self.delegate?.appleLoginDidComplete(with: .success(appleIDCredential))
+    }
+  }
+  
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    self.delegate?.appleLoginDidComplete(with: .failure(error))
+  }
+}
