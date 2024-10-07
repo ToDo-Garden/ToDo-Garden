61e69bd5ee0426704d4d31b8181b3ea3be4a07d7
SONG <geun2121@gmail.com>
2024-10-07 16:44:46 +0900

    #541: UI세팅 메서드 정의
    - setupStackView()
    - setupTitles() 를 정의합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AnimatedMultiLinesTitleView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AnimatedMultiLinesTitleView.swift
@@ -1,5 +1,5 @@
 //
-//  DynamicInfoInputView.swift
+//  AnimatedMultiLinesTitleView.swift
 //
 //
 //  Created by SONG on 10/6/24.
@@ -31,10 +31,48 @@ final class AnimatedMultiLinesTitleView: UIStackView {
     self.secondLineText = secondLineText
     self.thirdLineText = thirdLineText
     super.init(frame: CGRect.zero)
+    self.setupStackView()
+    self.setupTitles()
   }
   
   @available(*, unavailable)
   required init(coder: NSCoder) {
     fatalError()
   }
+extension AnimatedMultiLinesTitleView {
+  private func setupStackView() {
+    self.axis = NSLayoutConstraint.Axis.vertical
+    self.spacing = 4.0
+    self.alignment = UIStackView.Alignment.leading
+    
+    self.addArrangedSubview(self.mainTitleLabelFirst)
+    self.addArrangedSubview(self.mainTitleLabelSecond)
+    self.addArrangedSubview(self.subTitleLabel)
+  }
+
+  private func setupTitles() {
+    self.setupMainTitle(text: self.firstLineText, at: self.mainTitleLabelFirst)
+    self.setupMainTitle(text: self.secondLineText, at: self.mainTitleLabelSecond)
+    self.setupSubTitle(text: self.thirdLineText)
+  }
+  
+  private func setupMainTitle(text: String, at mainTitleLabel: UILabel) {
+    mainTitleLabel.numberOfLines = 1
+    mainTitleLabel.attributedText = text.applyTextAttributes(attributes: [
+      NSAttributedString.Key.font: UIFont.pretendardHeadBold,
+      NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark
+    ])
+    mainTitleLabel.text = ""
+  }
+  
+  private func setupSubTitle(text: String) {
+    self.subTitleLabel.numberOfLines = 1
+    self.subTitleLabel.attributedText = text.applyTextAttributes(attributes: [
+      NSAttributedString.Key.font: UIFont.pretendardDetailRegular12,
+      NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark
+    ])
+    self.subTitleLabel.text = ""
+  }
+}
+
 }
