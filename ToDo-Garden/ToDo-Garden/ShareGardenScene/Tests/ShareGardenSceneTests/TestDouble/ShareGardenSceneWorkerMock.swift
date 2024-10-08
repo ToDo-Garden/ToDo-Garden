e5989e31a89904f939c00d049654c4918ae056c1
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-08 19:12:04 +0900

    #299: ShareGardenSceneWorkerMock 파일 추가

---
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/TestDouble/ShareGardenSceneWorkerMock.swift
@@ -0,0 +1,20 @@
+// swiftlint:disable all
+//
+//  ShareGardenSceneWorkerMock.swift
+//  ShareGardenScene
+//
+//  Created by Noah on 10/8/24.
+//
+
+import Foundation
+
+import ToDoGardenUIComponent
+
+import ShareGardenScene
+import ShareGardenSceneAPI
+import ShareGardenSceneEntity
+
+actor ShareGardenSceneWorkerMock {
+}
+
+// swiftlint:enable all
