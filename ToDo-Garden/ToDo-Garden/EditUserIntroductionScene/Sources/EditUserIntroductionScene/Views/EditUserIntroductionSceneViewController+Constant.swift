92341f863ecd63c70b8d62c5e98b0bda6c6b3bb3
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-10-08 13:27:26 +0900

    #545: Constant 파일 추가
    - 상수, StringLiteral 값들을 담는 Constant  파일을 추가하였습니다.

---
+++ ToDo-Garden/ToDo-Garden/EditUserIntroductionScene/Sources/EditUserIntroductionScene/Views/EditUserIntroductionSceneViewController+Constant.swift
@@ -0,0 +1,12 @@
+//
+//  EditUserIntroductionSceneViewController+Constant.swift
+//
+//
+//  Created by Wood on 10/8/24.
+//
+
+import Foundation
+
+extension EditUserIntroductionSceneViewController {
+  enum Constant {}
+}
