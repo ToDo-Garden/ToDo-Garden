fed37673be519f84a35e23dd6b1a1a0a3e117895
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-08 19:09:30 +0900

    #299: asyncStream 추가
    - displayLogic 프로토콜 메서드 응답 시 값을 반환할 asyncStream을 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/TestDouble/ShareGardenSceneViewControllerStub.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/TestDouble/ShareGardenSceneViewControllerStub.swift
@@ -11,4 +11,31 @@ import Foundation
 import ShareGardenSceneEntity
 
 final class ShareGardenSceneViewControllerStub {
+  // MARK: - My Garden 관련 스트림
+  let (myGardenStream, myGardenStreamContinuation) = AsyncStream.makeStream(
+    of: ShareGardenScene.RequestMyGarden.ViewModel.self,
+    bufferingPolicy: AsyncStream.Continuation.BufferingPolicy.bufferingNewest(1)
+  )
+  
+  let (myGardenRequestErrorStream, myGardenRequestErrorStreamContinuation) = AsyncStream.makeStream(
+    of: Void.self,
+    bufferingPolicy: AsyncStream.Continuation.BufferingPolicy.bufferingNewest(1)
+  )
+  
+  // MARK: - Friends Garden 관련 스트림
+  let (friendsGardenListStream, friendsGardenListStreamContinuation) = AsyncStream.makeStream(
+    of: ShareGardenScene.RequestFriendsGardenList.ViewModel.self,
+    bufferingPolicy: AsyncStream.Continuation.BufferingPolicy.bufferingNewest(1)
+  )
+  
+  let (friendsGardenListRequestErrorStream, friendsGardenListRequestErrorStreamContinuation) = AsyncStream.makeStream(
+    of: Void.self,
+    bufferingPolicy: AsyncStream.Continuation.BufferingPolicy.bufferingNewest(1)
+  )
+  
+  // MARK: - Shimmering 관련 스트림
+  let (shimmeringStopStream, shimmeringStopStreamContinuation) = AsyncStream.makeStream(
+    of: Void.self,
+    bufferingPolicy: AsyncStream.Continuation.BufferingPolicy.bufferingNewest(1)
+  )
 }
