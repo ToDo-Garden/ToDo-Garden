378ba1c016aeaafabb38b88ce75585662ac71176
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-08 19:08:24 +0900

    #299: ShareGardenSceneViewControllerStub 파일 추가

---
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/TestDouble/ShareGardenSceneViewControllerStub.swift
@@ -0,0 +1,14 @@
+//
+//  ShareGardenSceneViewControllerStub.swift
+//  ShareGardenScene
+//
+//  Created by Noah on 10/8/24.
+//
+
+import Foundation
+
+@testable import ShareGardenScene
+import ShareGardenSceneEntity
+
+final class ShareGardenSceneViewControllerStub {
+}
