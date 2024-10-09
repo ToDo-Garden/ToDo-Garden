5722f521de259f8d7a602aba38e9c0f62639375e
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-10-09 16:46:00 +0900

    #563: EditUserIntroductionSceneBuilder 네이밍 수정
    - 네이밍에 Scene이 중첩되어 있어 수정하였습니다.

--- ToDo-Garden/ToDo-Garden/EditUserIntroductionScene/Sources/EditUserIntroductionScene/EditUserIntroductionSceneBuilder.swift
+++ ToDo-Garden/ToDo-Garden/EditUserIntroductionScene/Sources/EditUserIntroductionScene/EditUserIntroductionSceneBuilder.swift
@@ -10,7 +10,7 @@ import Foundation
 import EditUserIntroductionSceneAPI
 import EditUserIntroductionSceneEntity
 
-public struct EditUserIntroductionSceneSceneBuilder {
+public struct EditUserIntroductionSceneBuilder {
   /// 컴파일 타임에 필요한 의존성을 선언한 구조체입니다.
   public struct Dependency {
     let someWorker: EditUserIntroductionSceneWorkable
@@ -29,7 +29,7 @@ public struct EditUserIntroductionSceneSceneBuilder {
   }
 }
 
-extension EditUserIntroductionSceneSceneBuilder: EditUserIntroductionSceneBuildable {
+extension EditUserIntroductionSceneBuilder: EditUserIntroductionSceneBuildable {
   ///  VIP Cycle, 런타임 의존성이 설정된 ViewController 인스턴스를 반환하는 함수입니다.
   /// - Parameter payload: 런타임에 전달받아야 하는 의존성입니다.
   /// - Returns: 런타임 의존성, VIP Cycle이 설정된 ViewController를 반환합니다.
@@ -43,7 +43,7 @@ extension EditUserIntroductionSceneSceneBuilder: EditUserIntroductionSceneBuilda
   }
 }
 
-extension EditUserIntroductionSceneSceneBuilder {
+extension EditUserIntroductionSceneBuilder {
   /// VIP Cycle을 설정합니다.
   /// - Parameter viewController: VIPCycle을 설정할 viewController입니다.
   /// - Returns: VIP Cycle 설정이 완료된 `ViewControllable` 프로토콜을 준수한 `ViewController` 인스턴스를 반환합니다.
