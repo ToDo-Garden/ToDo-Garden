af82d269a158d18c1424bf0d9ab42c088ec6e163
SONG <geun2121@gmail.com>
2024-10-10 17:02:15 +0900

    #568: 프로퍼티 및 초기화문 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SignUpInputView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SignUpInputView.swift
@@ -7,6 +7,44 @@
 
 import UIKit
 
-class SignUpInputView: UIView {
+import ToDoGardenUIConstant
+
+public class SignUpInputView: UIView {
+  private let titleView: AnimatedMultiLinesTitleView
   
+  public let textInputView: InputTextValidationView
+  
+  public init(
+    firstTitle: String,
+    secondTitle: String,
+    thirdTitle: String,
+    textFieldTitle: String,
+    placeholderText: String,
+    validationText: String
+  ) {
+    self.titleView = AnimatedMultiLinesTitleView(
+      firstLineText: firstTitle,
+      secondLineText: secondTitle,
+      thirdLineText: thirdTitle
+    )
+    self.textInputView = InputTextValidationView(
+      inputText: textFieldTitle,
+      placeholderText: placeholderText,
+      validationText: validationText
+    )
+    super.init(frame: CGRect.zero)
+    self.backgroundColor = UIColor.white
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  public override var intrinsicContentSize: CGSize {
+    return CGSize(
+      width: UIScreen.main.bounds.width,
+      height: UIScreen.main.bounds.height * Constant.SignUpInputView.heightMultiplier
+    )
+  }
 }
