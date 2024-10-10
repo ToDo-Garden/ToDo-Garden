5efa856bf5fd9729b3b6cd2e1293e093877032d0
SONG <geun2121@gmail.com>
2024-10-10 16:56:17 +0900

    #568: 파일추가
    - SignUpInputView를 추가합니다.
    - Constant 파일을 추가합니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SignUpInputView.swift
@@ -0,0 +1,12 @@
+//
+//  SignUpInputView.swift
+//
+//
+//  Created by SONG on 10/10/24.
+//
+
+import UIKit
+
+class SignUpInputView: UIView {
+  
+}
---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+SignUpInputView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by SONG on 10/10/24.
+//
+
+import Foundation
