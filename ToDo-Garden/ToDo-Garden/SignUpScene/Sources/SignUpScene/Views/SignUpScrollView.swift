af288c2951369645f7fe99c8d4ddd94b5e874c74
SONG <geun2121@gmail.com>
2024-10-11 16:23:44 +0900

    #567: setupInputViews() 정의

--- ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/Views/SignUpScrollView.swift
+++ ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/Views/SignUpScrollView.swift
@@ -30,10 +30,44 @@ final class SignUpScrollView: UIScrollView {
     self.inputViews = []
     self.contentView = UIView()
     super.init(frame: frame)
+    self.setupInputViews()
   }
   
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
+  // swiftlint:disable function_body_length
+  private func setupInputViews() {
+    let constants = Constant.ScrollView.SignUpInputView.StringLiteral.self
+    
+    self.inputViews = [
+      SignUpInputView(
+        firstTitle: constants.idFirstTitle,
+        secondTitle: constants.idSecondTitle,
+        thirdTitle: constants.idThirdTitle,
+        textFieldTitle: constants.idTextFieldTitle,
+        placeholderText: constants.idPlaceholderText,
+        validationText: constants.conditionsForIDGenerationWarning
+      ),
+      SignUpInputView(
+        firstTitle: constants.introductionFirstTitle,
+        secondTitle: constants.introductionSecondTitle,
+        thirdTitle: constants.introductionThirdTitle,
+        textFieldTitle: constants.introductionTextFieldTitle,
+        placeholderText: constants.introductionPlaceholderText,
+        validationText: ""
+      ),
+      SignUpInputView(
+        firstTitle: constants.nicknameFirstTitle,
+        secondTitle: constants.nicknameSecondTitle,
+        thirdTitle: constants.nicknameThirdTitle,
+        textFieldTitle: constants.nicknameTextFieldTitle,
+        placeholderText: constants.nicknamePlaceholderText,
+        validationText: ""
+      )
+    ]
+  }
+  // swiftlint:enable function_body_length
 }
