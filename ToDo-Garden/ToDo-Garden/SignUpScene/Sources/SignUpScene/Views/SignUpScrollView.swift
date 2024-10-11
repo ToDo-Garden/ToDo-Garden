20e4656188084cabbc0169ba51b42c6f7559eaa8
SONG <geun2121@gmail.com>
2024-10-11 16:20:59 +0900

    #567: SignUpScrollView init정의
    - 프로퍼티를 선언하고 init()을 정의합니다.

--- ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/Views/SignUpScrollView.swift
+++ ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/Views/SignUpScrollView.swift
@@ -1,4 +1,31 @@
+//
+//  SignUpScrollView.swift
+//
+//
+//  Created by SONG on 10/7/24.
+//  Copyright (c) 2024 ToDoGarden. All rights reserved.
+
 import UIKit
 
 import TDUtility
 import ToDoGardenUIComponent
+
+final class SignUpScrollView: UIScrollView {
+  private(set) var currentPageIndex: Int {
+    didSet {
+      self.didChangePage()
+    }
+  }
+  private var inputViews: [SignUpInputView]
+  
+  override init(frame: CGRect) {
+    self.currentPageIndex = Int.zero
+    self.inputViews = []
+    super.init(frame: frame)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
