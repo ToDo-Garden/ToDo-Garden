a3a41c016d20e91f410773061a3125ec2dd2351d
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-14 14:45:42 +0900

    #299: 속성 초기화
    - 생성자를 추가하여 테스트에 필요한 속성을 초기화하였습니다.

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/ShareGardenSceneTests.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/ShareGardenSceneTests.swift
@@ -5,4 +5,31 @@
 //  Created by Noah on 10/8/24.
 //
 
-import Foundation
+import Testing
+import UIKit
+
+import ToDoGardenUIComponent
+
+@testable import ShareGardenScene
+import ShareGardenSceneAPI
+import ShareGardenSceneEntity
+
+@MainActor
+final class ShareGardenSceneTests {
+  private let window: UIWindow
+  private let shareGardenScene: ShareGardenSceneViewController
+  private let shareGardenSceneWorkerMock: ShareGardenSceneWorkerMock
+  private let interactor: ShareGardenSceneInteractor
+  private let presenter: ShareGardenScenePresenter
+  
+  private let sut: ShareGardenSceneViewControllerStub
+  
+  init() {
+    self.window = UIWindow()
+    self.shareGardenSceneWorkerMock = ShareGardenSceneWorkerMock()
+    self.interactor = ShareGardenSceneInteractor(shareGardenSceneWorker: self.shareGardenSceneWorkerMock)
+    self.shareGardenScene = ShareGardenSceneViewController(friendsGardenStore: self.interactor)
+    self.presenter = ShareGardenScenePresenter()
+    self.sut = ShareGardenSceneViewControllerStub()
+  }
+}
