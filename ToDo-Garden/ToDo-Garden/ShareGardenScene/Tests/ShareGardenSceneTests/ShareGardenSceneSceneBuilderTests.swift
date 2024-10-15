88044c6d433cd0b7907e9443dd083b9cedac1831
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-15 17:08:17 +0900

    #296: build test 추가
    - SceneBuilder가 올바른 타입과 VIP Cycle이 구성된 ViewController를 반환하는지 검증하였습니다.

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/ShareGardenSceneSceneBuilderTests.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Tests/ShareGardenSceneTests/ShareGardenSceneSceneBuilderTests.swift
@@ -5,3 +5,27 @@
 //  Created by Noah on 10/15/24.
 //
 
+import Testing
+
+@testable import ShareGardenScene
+
+@MainActor
+struct ShareGardenSceneSceneBuilderTests {
+  private let sut: ShareGardenSceneSceneBuilder
+  
+  init() {
+    self.sut = ShareGardenSceneSceneBuilder(dependency: .preview)
+  }
+  
+  @Test
+  private func buildTest() throws {
+    // When
+    let builderResult = self.sut.build()
+    
+    // Then
+    let shareGardenScene = try #require(builderResult as? ShareGardenSceneViewController)
+    let interactor = try #require(shareGardenScene.interactor as? ShareGardenSceneInteractor)
+    _ = try #require(interactor.presenter as? ShareGardenScenePresenter)
+    _ = try #require(shareGardenScene.router as? ShareGardenSceneRouter)
+  }
+}
