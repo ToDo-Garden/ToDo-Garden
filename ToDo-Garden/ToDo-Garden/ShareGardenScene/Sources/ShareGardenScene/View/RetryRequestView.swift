8b9fd7653adc3613d1c26ac5fc343b5616569ad7
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-10-15 19:50:27 +0900

    #588: 다시 시도하기 버튼 추가

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/RetryRequestView.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/View/RetryRequestView.swift
@@ -12,3 +12,58 @@ import ToDoGardenUIComponent
 @MainActor
 final class RetryRequestView {
 }
+
+extension RetryRequestView {
+  private final class RetryButton: UIButton {
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      self.configureAppearance()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureAppearance() {
+      let title = "다시 시도하기"
+      let configuration = UIButton.Configuration.plain()
+      self.configuration = configuration
+      
+      self.configurationUpdateHandler = { button in
+        guard var updatedConfiguration = button.configuration
+        else { return }
+        let isHighlighted = button.isHighlighted
+        let highlightedColor = UIColor.toDoGardenGreenDark.withAlphaComponent(0.5)
+        let normalColor = UIColor.toDoGardenGreenDark
+        let titleColor: UIColor = isHighlighted ? highlightedColor : normalColor
+        if let attributedTitle = self.createAttributedButtonTitle(title: title, with: titleColor) {
+          updatedConfiguration.attributedTitle = AttributedString(attributedTitle)
+        }
+        
+        self.setConfiguration(updatedConfiguration)
+      }
+    }
+    
+    private func createAttributedButtonTitle(title: String, with color: UIColor) -> NSAttributedString? {
+      let attributedTitle = title.applyTextAttributes(
+        attributes: [
+          NSAttributedString.Key.font: UIFont.pretendardBodyBold,
+          NSAttributedString.Key.foregroundColor: color
+        ]
+      )
+      return NSMutableAttributedString(attributedString: attributedTitle)
+        .addUnderline(with: UIColor.toDoGardenGreenDark, from: 0, to: title.count)
+    }
+    
+    private func setConfiguration(_ configuration: UIButton.Configuration, duration: Double = 0.3) {
+      UIView.transition(
+        with: self,
+        duration: duration,
+        options: .transitionCrossDissolve
+      ) {
+        self.configuration? = configuration
+      }
+    }
+  }
+}
