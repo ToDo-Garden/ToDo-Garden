fcc45526e5bdd9d2cff85605f61042d759eee391
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-10-17 21:54:19 +0900

    #596: UserInfoSceneRouter 소개 수정화면 라우팅 로직 추가
    - UserInfoScene > EditUserIntroductionScene으로 라우팅하는 로직을 추가하였습니다.
    - payload를 이용해 데이터를 전달하는 로직을 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/UserInfoScene/Sources/UserInfoScene/UserInfoSceneRouter.swift
+++ ToDo-Garden/ToDo-Garden/UserInfoScene/Sources/UserInfoScene/UserInfoSceneRouter.swift
@@ -35,9 +35,23 @@ extension UserInfoSceneRouter: UserInfoSceneRoutingLogic {
   func routeToLoginScene() {
     // TODO: LoginSceneBuilder가 구현되면 해당 화면으로 라우팅할 예정입니다.
   }
+
+  func routeToEditUserIntroductionScene() {
+    guard let editUserIntroductionScene = self.editUserIntroductionSceneBuilder?.build(
+      with: EditUserIntroductionScenePayload(userIntroduction: self.dataStore?.userIntroduction)
+    ) else { return }
+
+    self.viewController?.navigationController?.pushViewController(
+      editUserIntroductionScene,
+      animated: true
+    )
+  }
 }
 
 // MARK: - Declare Payload for scene
 
 extension UserInfoSceneRouter {
+  struct EditUserIntroductionScenePayload: EditUserIntroductionScenePayloadable {
+    var userIntroduction: String?
+  }
 }
