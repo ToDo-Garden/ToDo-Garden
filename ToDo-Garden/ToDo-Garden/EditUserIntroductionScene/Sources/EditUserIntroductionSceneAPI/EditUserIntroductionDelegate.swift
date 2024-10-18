84382b64c3746b13b9a5cba6148f7a3190564f06
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2024-10-18 19:45:30 +0900

    #599: EditUserIntroductionDelegate 주석 추가
    - Delegate 객체에 문자열이 Optional 타입인 이유를 주석으로 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/EditUserIntroductionScene/Sources/EditUserIntroductionSceneAPI/EditUserIntroductionDelegate.swift
+++ ToDo-Garden/ToDo-Garden/EditUserIntroductionScene/Sources/EditUserIntroductionSceneAPI/EditUserIntroductionDelegate.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 public protocol EditUserIntroductionDelegate: AnyObject {
+  /// 소개 수정 요청 성공시, Delegate를 채택한 객체에 수정된 소개를 전달합니다.
+  /// - Parameter introduction: 수정된 소개 문자열 값입니다.
+  /// - 유저가 소개를 빈 상태로 설정한 경우, nil을 전달합니다.
   func userIntroductionDidEdited(new introduction: String?)
 }
