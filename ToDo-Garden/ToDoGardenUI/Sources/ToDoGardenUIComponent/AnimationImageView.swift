e64de0ee982f18302bd91d7edccc2b55a166dbdd
SONG <geun2121@gmail.com>
2024-10-30 20:46:11 +0900

    #641: AnimationImageView 정의
    - json을 가져와서 표시합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AnimationImageView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/AnimationImageView.swift
@@ -1,8 +1,86 @@
 //
-//  File.swift
-//  
+//  AnimationImageView.swift
 //
-//  Created by SONG on 10/30/24.
+//
+//  Created by SONG on 10/29/24.
 //
 
-import Foundation
+import UIKit
+
+import Lottie
+import ToDoGardenUIResource
+
+public class AnimationImageView: UIView {
+  private var animationView: LottieAnimationView
+  private var animationTask: Task<Void, Never>?
+  
+  public init(jsonName: String) {
+    self.animationView = LottieAnimationView()
+    super.init(frame: .zero)
+    self.setupAnimation(jsonName: jsonName)
+    
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupAnimation(jsonName: String) {
+    self.animationTask = Task {
+      defer {
+        self.animationTask = nil
+      }
+      
+      guard let bundle = Bundle.toDoGardenUIResource else {
+        // Bundle load 실패
+        return
+      }
+      
+      guard let jsonURL = bundle.url(
+        forResource: jsonName,
+        withExtension: "json",
+        subdirectory: "LottieJsons"
+      ) else {
+        // JSON URL을 찾지 못함
+        return
+      }
+      
+      await self.loadAnimation(from: jsonURL)
+      self.setupAnimationView()
+    }
+  }
+  
+  private func loadAnimation(from url: URL) async {
+    if let animation = await LottieAnimation.loadedFrom(url: url) {
+      self.animationView = LottieAnimationView(animation: animation)
+    } else {
+      // Animation load 실패
+      return
+    }
+  }
+  
+  private func setupAnimationView() {
+    self.animationView.usingAutolayout()
+    self.addSubview(self.animationView)
+    
+    NSLayoutConstraint.activate([
+      self.animationView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+      self.animationView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+      self.animationView.topAnchor.constraint(equalTo: self.topAnchor),
+      self.animationView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+    ])
+    
+    self.animationView.contentMode = UIView.ContentMode.scaleAspectFit
+    self.animationView.loopMode = LottieLoopMode.loop
+    self.startAnimation()
+  }
+  
+  public func startAnimation() {
+    self.animationView.play()
+  }
+  
+  public func pauseAnimation() {
+    self.animationView.pause()
+  }
+}
