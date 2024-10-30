660adab35c9a2729cdd4e1fd1e47ab9fd2e723a4
SONG <geun2121@gmail.com>
2024-10-30 20:45:13 +0900

    #641: static 으로 번들 선언
    - Bundle.module을 리턴합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIResource/Bundle+ToDoGarden.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIResource/Bundle+ToDoGarden.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  Bundle+ToDoGarden.swift
 //
-//  Created by SONG on 10/30/24.
+//
+//  Created by SONG on 10/29/24.
 //
 
 import Foundation
+
+extension Bundle {
+  public static var toDoGardenUIResource: Bundle? {
+    return Bundle.module
+  }
+}
