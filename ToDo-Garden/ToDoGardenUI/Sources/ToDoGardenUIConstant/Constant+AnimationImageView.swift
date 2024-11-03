68a060cec4794c773185415211382f185e786074
SONG <geun2121@gmail.com>
2024-11-03 20:17:18 +0900

    #641: 상수 정의

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+AnimationImageView.swift
@@ -0,0 +1,15 @@
+//
+//  Constant+AnimationImageView.swift
+//
+//
+//  Created by SONG on 11/3/24.
+//
+
+import Foundation
+
+extension Constant.AnimationImageView {
+  public enum StringLiteral {
+    public static let fileFormat = "json"
+    public static let subDirectory = "LottieJsons"
+  }
+}
