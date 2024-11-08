18dd3361f140451740d4c5ad486cdd7cedc7d4fa
SONG <geun2121@gmail.com>
2024-11-08 15:29:56 +0900

    #646: 파일추가
    - swift 파일을 추가합니다.

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel.swift
@@ -0,0 +1,12 @@
+//
+//  BubbleLabel.swift
+//
+//
+//  Created by SONG on 11/8/24.
+//
+
+import UIKit
+
+public final class BubbleLabel: UIView {
+  
+}
---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LeafLabel.swift
@@ -0,0 +1,12 @@
+//
+//  LeafLabel.swift
+//  
+//
+//  Created by SONG on 11/8/24.
+//
+
+import UIKit
+
+public final class LeafLabel: UIView {
+  
+}
