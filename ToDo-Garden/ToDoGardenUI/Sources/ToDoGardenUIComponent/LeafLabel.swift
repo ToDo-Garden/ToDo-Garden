8c6fea05aa683adbe3aa2078561cb4d8ae8a80e6
SONG <geun2121@gmail.com>
2024-11-08 23:04:53 +0900

    #646: 메서드 구현
    - view들에 대한 setup을 합니다.
    - 레이아웃 제약을 설정합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LeafLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LeafLabel.swift
@@ -1,11 +1,14 @@
 //
 //  LeafLabel.swift
-//  
+//
 //
 //  Created by SONG on 11/8/24.
 //
 
 import UIKit
+
+import ToDoGardenUIConstant
+import ToDoGardenUIResource
 
 public final class LeafLabel: UIView {
   private let leafImageView: UIImageView
@@ -21,6 +24,12 @@ public final class LeafLabel: UIView {
     self.descriptionLabel = UILabel()
     super.init(frame: CGRect.zero)
     self.backgroundColor = UIColor.clear
+    self.setupImageView()
+    self.setupLabels(
+      titleText: titleText,
+      descriptionText: descriptionText
+    )
+    self.setupConstraints()
   }
   
   @available(*, unavailable)
@@ -34,4 +43,60 @@ public final class LeafLabel: UIView {
       height: Constant.LeafLabel.height
     )
   }
+  
+  private func setupImageView() {
+    self.leafImageView.usingAutolayout()
+    self.leafImageView.contentMode = .scaleAspectFit
+    self.addSubview(self.leafImageView)
+  }
+  
+  private func setupLabels(titleText: String, descriptionText: String) {
+    self.titleLabel.usingAutolayout()
+    self.descriptionLabel.usingAutolayout()
+    
+    self.titleLabel.numberOfLines = 1
+    self.descriptionLabel.numberOfLines = 1
+    
+    self.titleLabel.attributedText = titleText.applyTextAttributes(
+      attributes:
+        [
+          NSAttributedString.Key.font: UIFont.pretendardBodyBold,
+          NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark
+        ]
+    )
+    
+    self.descriptionLabel.attributedText = descriptionText.applyTextAttributes(
+      attributes:
+        [
+          NSAttributedString.Key.font: UIFont.pretendardDetailRegular12,
+          NSAttributedString.Key.foregroundColor: UIColor.toDoGardenGreenDark
+        ]
+    )
+    
+    self.addSubview(titleLabel)
+    self.addSubview(descriptionLabel)
+  }
+  
+  private func setupConstraints() {
+    NSLayoutConstraint.activate([
+      self.leafImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+      self.leafImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      self.leafImageView.widthAnchor.constraint(equalToConstant: Constant.LeafLabel.logoLength),
+      self.leafImageView.heightAnchor.constraint(equalToConstant: Constant.LeafLabel.logoLength),
+
+      self.titleLabel.leadingAnchor.constraint(
+        equalTo: self.leafImageView.trailingAnchor,
+        constant: Constant.LeafLabel.labelLeading
+      ),
+      self.titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+      
+      self.descriptionLabel.leadingAnchor.constraint(
+        equalTo: self.leafImageView.trailingAnchor,
+        constant: Constant.LeafLabel.labelLeading
+      ),
+      self.descriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+    ])
+  }
+}
+
 }
