455162e94845bb8d0a311702ab72f4aa11a88dab
SONG <geun2121@gmail.com>
2024-11-08 23:03:23 +0900

    #646: 초기화문 작성

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LeafLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LeafLabel.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 public final class LeafLabel: UIView {
+  private let leafImageView: UIImageView
+  private let titleLabel: UILabel
+  private let descriptionLabel: UILabel
   
-  public init() {
+  public init(
+    titleText: String,
+    descriptionText: String
+  ) {
+    self.leafImageView = UIImageView(image: UIImage.leafImage)
+    self.titleLabel = UILabel()
+    self.descriptionLabel = UILabel()
     super.init(frame: CGRect.zero)
+    self.backgroundColor = UIColor.clear
   }
   
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  public override var intrinsicContentSize: CGSize {
+    return CGSize(
+      width: UIView.noIntrinsicMetric,
+      height: Constant.LeafLabel.height
+    )
   }
 }
