a61b1bd8102e09c3849a43f6d1b2d7c5f1423f36
SONG <geun2121@gmail.com>
2024-11-09 16:11:52 +0900

    #648: BubbleLabel init() 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleLabel.swift
@@ -9,3 +9,32 @@ import UIKit
 
 import ToDoGardenUIConstant
 import ToDoGardenUIResource
+public final class BubbleLabel: UIView {
+  private let bubbleTextBox: BubbleTextBox
+  private let tailView: TailView
+  private let tailPosition: BubbleLabel.TailPosition
+  public weak var delegate: BubbleLabelDelegate?
+  
+  public enum TailPosition {
+    case left
+    case right
+  }
+  
+  public init(
+    tailPosition: BubbleLabel.TailPosition,
+    iconImage: UIImage,
+    text: String
+  ) {
+    self.tailPosition = tailPosition
+    self.bubbleTextBox = BubbleTextBox(iconImage: iconImage, text: text)
+    self.tailView = TailView()
+    super.init(frame: CGRect.zero)
+    self.setupView()
+    self.backgroundColor = UIColor.clear
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
