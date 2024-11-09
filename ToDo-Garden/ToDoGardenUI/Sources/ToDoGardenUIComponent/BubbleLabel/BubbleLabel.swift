6bf3b662be18eb1a6265b2787e82e4b27e42a4f7
SONG <geun2121@gmail.com>
2024-11-09 16:12:36 +0900

    #648: BubbleLabel setupView() 정의
    - 서브뷰 삽입
    - 레이아웃 제약 세팅

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleLabel.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleLabel.swift
@@ -37,4 +37,67 @@ public final class BubbleLabel: UIView {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+
+  private func setupView() {
+    self.backgroundColor = UIColor.white
+    self.addSubviews()
+    self.setupCancelButtonAction()
+    self.setupConstraints()
+  }
+  
+  private func addSubviews() {
+    self.addSubview(self.bubbleTextBox)
+    self.addSubview(self.tailView)
+  }
+  
+  private func setupCancelButtonAction() {
+    self.bubbleTextBox.cancelButton.addAction(
+      UIAction { _ in
+        self.cancelButtonTapped()
+    },
+      for: UIControl.Event.touchUpInside
+    )
+  }
+  
+  private func setupConstraints() {
+    self.bubbleTextBox.usingAutolayout()
+    self.tailView.usingAutolayout()
+    
+    self.setupBubbleTextBoxConstraints()
+    self.setupTailViewInitialConstraints()
+    self.setupTailViewConstraints()
+  }
+  
+  private func setupBubbleTextBoxConstraints() {
+    let margin = Constant.BubbleLabel.commonMargin
+    
+    NSLayoutConstraint.activate([
+      self.bubbleTextBox.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: margin),
+      self.bubbleTextBox.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -margin),
+      self.bubbleTextBox.topAnchor.constraint(equalTo: self.topAnchor, constant: margin),
+      self.bubbleTextBox.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -margin)
+    ])
+    
+    self.sizeToFit()
+  }
+  
+  private func setupTailViewInitialConstraints() {
+    let margin = Constant.BubbleLabel.commonMargin
+    
+    NSLayoutConstraint.activate([
+      self.tailView.widthAnchor.constraint(equalToConstant: 2 * margin),
+      self.tailView.heightAnchor.constraint(equalToConstant: margin),
+      self.tailView.bottomAnchor.constraint(equalTo: self.bubbleTextBox.topAnchor)
+    ])
+  }
+  
+  private func setupTailViewConstraints() {
+    let margin = Constant.BubbleLabel.tailPositionMargin
+    switch self.tailPosition {
+    case .left:
+      self.tailView.centerXAnchor.constraint(equalTo: self.leadingAnchor, constant: margin).isActive = true
+    case .right:
+      self.tailView.centerXAnchor.constraint(equalTo: self.trailingAnchor, constant: -margin).isActive = true
+    }
+  }
 }
