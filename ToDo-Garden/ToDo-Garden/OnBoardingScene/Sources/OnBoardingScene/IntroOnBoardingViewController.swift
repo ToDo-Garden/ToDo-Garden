6c82b9f6edc0a265e13fd268e2401e55f87a3f2c
SONG <geun2121@gmail.com>
2024-11-09 16:57:24 +0900

    #650: 파일 추가
    - OnBoardingScene 패키지를 추가합니다.

---
+++ ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/IntroOnBoardingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingIntroViewController.swift
+//
+//
+//  Created by SONG on 11/9/24.
+//
+
+import UIKit
+
+import ToDoGardenUIComponent
+
+class OnBoardingIntroViewController: UIViewController {
+  
+}
