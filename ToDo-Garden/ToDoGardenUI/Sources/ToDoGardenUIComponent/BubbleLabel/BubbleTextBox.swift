bab4f3042730ef72a48327337b58bdea1aa1a781
SONG <geun2121@gmail.com>
2024-11-09 16:10:13 +0900

    #648: BubbleTextBox 정의
    - init() 정의
    - intrinsicContentSize 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleTextBox.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/BubbleLabel/BubbleTextBox.swift
@@ -9,3 +9,30 @@ import UIKit
 
 import ToDoGardenUIConstant
 import ToDoGardenUIResource
+
+final class BubbleTextBox: UIView {
+  private let textLabel: UILabel
+  let cancelButton: UIButton
+  
+  init(iconImage: UIImage, text: String) {
+    self.textLabel = UILabel()
+    self.cancelButton = UIButton()
+    super.init(frame: CGRect.zero)
+    self.backgroundColor = UIColor.white
+    self.layer.borderWidth = 2
+    self.layer.borderColor = UIColor.toDoGardenGreenDark.cgColor
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    let verticalMargin = Constant.BubbleLabel.BubbleTextBox.commonMargin
+    return CGSize(
+      width: UIView.noIntrinsicMetric,
+      height: self.textLabel.intrinsicContentSize.height + verticalMargin
+    )
+  }
+}
