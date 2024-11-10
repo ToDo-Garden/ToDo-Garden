c475beef1f01c0b82271ed9903ce55bd209e80d4
SONG <geun2121@gmail.com>
2024-11-10 16:31:17 +0900

    #652: setupViews() 정의

--- ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/IntroOnBoardingViewController.swift
+++ ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/IntroOnBoardingViewController.swift
@@ -26,6 +26,8 @@ public class IntroOnBoardingViewController: UIViewController {
     )
     super.init(nibName: nil, bundle: nil)
     self.view.backgroundColor = UIColor.toDoGardenBeige
+    self.setupViews()
+    self.setupConstraints()
   }
   
   public override func viewWillAppear(_ animated: Bool) {
@@ -36,5 +38,71 @@ public class IntroOnBoardingViewController: UIViewController {
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension IntroOnBoardingViewController {
+  private func setupViews() {
+    self.addMainImageView()
+    self.addStackView()
+    self.addStartButton()
+  }
+  
+  private func addMainImageView() {
+    self.view.addSubview(self.mainImageView)
+  }
+  
+  private func addStackView() {
+    self.view.addSubview(self.stackView)
+  }
+  
+  private func addStartButton() {
+    self.view.addSubview(self.startButton)
+  }
+  
+  private func setupConstraints() {
+    self.setupMainImageViewConstraints()
+    self.setupStackViewConstraints()
+    self.setupStartButtonConstraints()
+  }
+  
+  private func setupMainImageViewConstraints() {
+    self.mainImageView.usingAutolayout()
+    let multiplier = Constant.Layout.multiplier
+    let aspectRatio = Constant.Layout.aspectRatio
+    
+    NSLayoutConstraint.activate([
+      self.mainImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+      self.mainImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+      self.mainImageView.heightAnchor.constraint(equalTo: self.mainImageView.widthAnchor, multiplier: aspectRatio),
+      self.mainImageView.centerYAnchor.constraint(
+        equalTo: self.view.centerYAnchor,
+        constant: -self.view.bounds.height * multiplier
+      )
+    ])
+  }
+  
+  private func setupStackViewConstraints() {
+    self.stackView.usingAutolayout()
+    
+    NSLayoutConstraint.activate([
+      self.stackView.centerYAnchor.constraint(
+        equalTo: self.view.centerYAnchor,
+        constant: self.view.bounds.height * Constant.Layout.multiplier
+      ),
+      self.stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+    ])
+  }
+  
+  private func setupStartButtonConstraints() {
+    self.startButton.usingAutolayout()
+    
+    NSLayoutConstraint.activate([
+      self.startButton.bottomAnchor.constraint(
+        equalTo: self.view.safeAreaLayoutGuide.bottomAnchor,
+        constant: -Constant.Layout.space
+      ),
+      self.startButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+    ])
   }
 }
