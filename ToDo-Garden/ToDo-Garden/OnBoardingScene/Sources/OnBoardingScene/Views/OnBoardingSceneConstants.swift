0a39f903c8d6b3c832d048a2684103c3908fdeeb
SONG <geun2121@gmail.com>
2024-11-10 16:28:27 +0900

    #652: 상수 추가

--- ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/Views/OnBoardingSceneConstants.swift
+++ ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/Views/OnBoardingSceneConstants.swift
@@ -1,8 +1,28 @@
 //
-//  File 2.swift
-//  
+//  OnBoardingSceneConstants.swift
+//
 //
 //  Created by SONG on 11/10/24.
 //
 
 import Foundation
+
+enum Constant {
+  enum Layout {
+    static let space: CGFloat = 26.0
+    static let multiplier: CGFloat = 0.2
+    static let aspectRatio: CGFloat = 456 / 375
+  }
+  enum StringLiteral {
+    static let buttonTitle: String = "시작하기"
+    
+    static let firstLineTitle: String = "뽀모도로 타이머로 시간 측정하기"
+    static let firstLineDescription: String = "뽀모도로 타이머를 이용해 집중력을 향상시킬 수 있어요"
+    
+    static let secondLineTitle: String = "완료한 투두 기록하기"
+    static let secondLineDescription: String = "매일 투두를 완료하고 기록을 갱신해요"
+    
+    static let thirdLineTitle: String = "열정을 불태우기"
+    static let thirdLineDescription: String = "투두가든을 통해 당신의 열정을 불태우세요!"
+  }
+}
