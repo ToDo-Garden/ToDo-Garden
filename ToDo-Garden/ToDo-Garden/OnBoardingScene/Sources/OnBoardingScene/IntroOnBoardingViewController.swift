f0f5e49954d3de7f12200158eb1ec1d2adc507b3
SONG <geun2121@gmail.com>
2024-11-11 12:56:37 +0900

    #652: viewIsAppearing,DidLoad 추가
    - vc 라이프사이클 관리 메서드를 추가하였고, 일부 코드를 옮겼습니다.

--- ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/IntroOnBoardingViewController.swift
+++ ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/IntroOnBoardingViewController.swift
@@ -25,19 +25,23 @@ public final class IntroOnBoardingViewController: UIViewController {
       buttonType: ToDoGardenBoxButton.Configuration.primaryRoundRectButton
     )
     super.init(nibName: nil, bundle: nil)
-    self.view.backgroundColor = UIColor.toDoGardenBeige
-    self.setupViews()
-    self.setupConstraints()
-  }
-  
-  public override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    self.navigationController?.navigationBar.isHidden = true
   }
   
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  public override func viewIsAppearing(_ animated: Bool) {
+    super.viewIsAppearing(animated)
+    self.navigationController?.navigationBar.isHidden = true
+  }
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.toDoGardenBeige
+    self.setupViews()
+    self.setupConstraints()
   }
 }
 
