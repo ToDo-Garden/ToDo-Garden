ab17e35f67f4a5ce1a88595d8c1f6ec9b87545e5
SONG <geun2121@gmail.com>
2024-11-11 13:55:33 +0900

    #654: 상수 추가

--- ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/Views/OnBoardingSceneConstants.swift
+++ ToDo-Garden/ToDo-Garden/OnBoardingScene/Sources/OnBoardingScene/Views/OnBoardingSceneConstants.swift
@@ -12,6 +12,10 @@ enum Constant {
     static let space: CGFloat = 26.0
     static let multiplier: CGFloat = 0.2
     static let aspectRatio: CGFloat = 456 / 375
+    
+    static let bubbleLabelMargin: CGFloat = 5.0
+    static let leftBubbleLabelLeading: CGFloat = -44.0
+    static let rightBubbleLabelTrailing: CGFloat = 39.0
   }
   enum StringLiteral {
     static let buttonTitle: String = "시작하기"
@@ -24,5 +28,8 @@ enum Constant {
     
     static let thirdLineTitle: String = "열정을 불태우기"
     static let thirdLineDescription: String = "투두가든을 통해 당신의 열정을 불태우세요!"
+    
+    static let leftBubbleTitle: String = "를 눌러 ToDo를 추가할 수 있어요"
+    static let rightBubbleTitle: String = "뽀모도로 타이머를 이용하여\n집중력을 향상시킬 수 있어요"
   }
 }
