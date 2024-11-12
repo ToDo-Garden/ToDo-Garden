47339edfd5f47f7b6268cabadeb0b71de5d75afd
SONG <geun2121@gmail.com>
2024-11-12 17:19:34 +0900

    #659: init() 정의

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LongestRecordView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LongestRecordView.swift
@@ -7,9 +7,38 @@
 
 import UIKit
 
-import ToDoGardenUIResource
 import TDFoundationExtension
+import ToDoGardenUIResource
 
 public final class LongestRecordView: UIView {
+  private let titleLabel: UILabel
+  private let labelStackView: UIStackView
   
+  public var informationButton: UIButton?
+  
+  public enum Configuration {
+    case pomo
+    case dateRange
+  }
+  
+  public init(
+    style: LongestRecordView.Configuration,
+    title: String,
+    groupName: String? = nil,
+    recordCount: Int,
+    date: [Date]
+  ) {
+    self.titleLabel = UILabel()
+    self.labelStackView = UIStackView(frame: CGRect.zero)
+    super.init(frame: CGRect.zero)
+    self.backgroundColor = UIColor.white
+    self.layer.cornerRadius = 10.0
+    self.layer.borderWidth = 1.0
+    self.layer.borderColor = UIColor.toDoGardenGreenGray.cgColor
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
