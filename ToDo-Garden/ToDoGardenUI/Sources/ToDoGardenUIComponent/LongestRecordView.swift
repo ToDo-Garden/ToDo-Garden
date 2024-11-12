e474df0b4dd730b5e3a8917f1d9d9c6e956a1959
SONG <geun2121@gmail.com>
2024-11-12 17:09:45 +0900

    #659: 파일 추가
    - LongestRecordView와 Date 익스텐션 파일 추가

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/LongestRecordView.swift
@@ -0,0 +1,15 @@
+//
+//  LongestRecordView.swift
+//
+//
+//  Created by SONG on 11/12/24.
+//
+
+import UIKit
+
+import ToDoGardenUIResource
+import TDFoundationExtension
+
+public final class LongestRecordView: UIView {
+  
+}
