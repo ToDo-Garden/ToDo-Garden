a1589655327bbae3767df380c0d340ca38dd52f2
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-13 16:25:00 +0900

    #637: HTTPClient 타입 추가
    - HTTPClientAPI 구현체가 될 구조체를 추가하였습니다.

---
+++ ToDo-Garden/TDFoundation/Sources/HTTPClient/HTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPClient.swift
+//  TDFoundation
+//
+//  Created by Noah on 11/7/24.
+//
+
+import Foundation
+
+import HTTPClientAPI
+
+public struct HTTPClient: Sendable {
+}
+
+extension HTTPClient {
+}
