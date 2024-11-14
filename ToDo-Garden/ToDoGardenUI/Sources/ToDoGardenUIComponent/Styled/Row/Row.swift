5a2678c4abfe5b20a6c165cd79b7f2414d78de84
SONG <geun2121@gmail.com>
2024-11-14 18:04:06 +0900

    #666: titleLabel numberOfLines 수정
    - 0 으로 설정합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Row.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Row.swift
@@ -111,6 +111,7 @@ extension Styled.Row {
     let label = UILabel()
     label.text = text
     label.font = font
+    label.numberOfLines = Int.zero
     label.textColor = textColor
     return label
   }
