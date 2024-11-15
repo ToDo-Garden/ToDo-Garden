af49cd9b6d505cbabf8d5c942375b704134c5692
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-15 20:23:15 +0900

    #637: middleware intercept 테스트 코드 작성
    - middleware가 HTTP 요청 수행 중 intercept를 올바르게 수행하는지에 대한 테스트 코드를 작성하였습니다.

--- ToDo-Garden/TDFoundation/Tests/HTTPClientTests/HTTPClientTests.swift
+++ ToDo-Garden/TDFoundation/Tests/HTTPClientTests/HTTPClientTests.swift
@@ -4,5 +4,55 @@
 //
 //  Created by Noah on 11/13/24.
 //
+// swiftlint:disable function_body_length
 
 import Foundation
+import Testing
+
+import HTTPClient
+import HTTPClientAPI
+
+struct HTTPClientTests {
+  private let sut: HTTPClient
+  private let transportMock: ClientTransportMock
+  private let middlewareMock: ClientMiddlewareMock
+  
+  init() {
+    self.transportMock = ClientTransportMock()
+    self.middlewareMock = ClientMiddlewareMock()
+    self.sut = HTTPClient(
+      transport: self.transportMock,
+      middlewares: [self.middlewareMock]
+    )
+  }
+  
+  @Test("is middleware intercepting request")
+  private func middlewareInterceptRequest() async throws {
+    // Given
+    let givenURL = try #require(URL(string: "test url"))
+    let expectedInterceptedRequest: HTTPRequest = HTTPRequest(
+      method: HTTPMethod.get,
+      endPoint: givenURL,
+      header: [:],
+      queryItems: [:],
+      body: nil
+    )
+    
+    // When
+    try await self.sut.send(
+      input: Void(),
+      serializer: { _ in
+        return expectedInterceptedRequest
+      },
+      deserializer: { _ in
+        return
+      }
+    )
+    
+    // Then
+    #expect(self.middlewareMock.isIntercepterCalled)
+    #expect(self.middlewareMock.interceptedRequest == expectedInterceptedRequest)
+  }
+}
+
+// swiftlint:enable function_body_length
