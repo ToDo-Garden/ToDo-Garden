4950fd3467f7e096b85bd0cc3ba4a54008a86aa5
SONG <geun2121@gmail.com>
2024-11-17 15:26:01 +0900

    #668: 상수 추가

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIConstant/Constant+MyStatsSummaryView.swift
@@ -0,0 +1,17 @@
+//
+//  Constant+MyStatsSummaryView.swift
+//
+//
+//  Created by SONG on 11/17/24.
+//
+
+import Foundation
+
+extension Constant.MyStatsSummaryView {
+  public enum Layout {
+    public static let cornerRadius = 10.0
+    public static let verticalLineMargin = 23.5
+    public static let labelMargin = 5.0
+    public static let commonMargin = 10.0
+  }
+}
