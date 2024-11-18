a2df04e16d424a2b033292a46c5d9f72b7aaa4d7
SONG <geun2121@gmail.com>
2024-11-18 23:30:29 +0900

    #678: 메서드 분리
    - Row를 배치하는 메서드를 분리합니다.
    - 기존의 Row배치 + 옵저빙의 역할을 하는 build와, Row배치만 하는 build를 정의합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/TableRow.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/TableRow.swift
@@ -2,22 +2,33 @@ import Combine
 import UIKit
 
 public class TableRow: UITableViewCell {
+  private func setupRow(configuration: Styled.Row.Configuration) -> Styled.Row {
+    let newRow = Styled.Row(configuration: configuration)
+    newRow.usingAutolayout()
+    self.contentView.addSubview(newRow)
+    
+    NSLayoutConstraint.activate([
+      newRow.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+      newRow.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+      newRow.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+      newRow.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+    ])
+    
+    self.selectionStyle = UITableViewCell.SelectionStyle.none
+    return newRow
+  }
+  
+  public func build(configuration: Styled.Row.Configuration) -> Self {
+    _ = self.setupRow(configuration: configuration)
+    return self
+  }
+  
   public func build<T>(
     configuration: Styled.Row.Configuration,
     keyPath: KeyPath<Styled.Row.Configuration, T>
-  ) -> AnyPublisher<T, Never> {
-    selectionStyle = UITableViewCell.SelectionStyle.none
-    let row = Styled.Row(configuration: configuration)
-    row.usingAutolayout()
-    contentView.addSubview(row)
-    NSLayoutConstraint.activate([
-      row.topAnchor.constraint(equalTo: contentView.topAnchor),
-      row.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-      row.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-      row.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-    ])
-    
-    return row.$configuration
+  ) -> AnyPublisher<T, Never>? {
+    let setupRow = self.setupRow(configuration: configuration)
+    return setupRow.$configuration
       .map(keyPath)
       .eraseToAnyPublisher()
   }
