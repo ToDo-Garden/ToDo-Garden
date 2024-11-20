78566a3f8e95ff595025a93263a79c571d681979
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-20 18:13:45 +0900

    #682: UIViewControllerContextTransitioning 프로토콜 구현
    - ViewController의 index를 기반으로 하여 페이지를 쓸어 넘기는 듯한 애니메이션을 구현하였습니다.

--- ToDo-Garden/TDFoundation/Sources/TDFoundation/PageTransition.swift
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/PageTransition.swift
@@ -18,6 +18,55 @@ public final class PageTransition: NSObject, UIViewControllerAnimatedTransitioni
     self.transitionDuration = transitionDuration
     self.viewControllers = viewControllers
   }
+  
+  public func transitionDuration(using transitionContext: (any UIViewControllerContextTransitioning)?) -> TimeInterval {
+    return self.transitionDuration
+  }
+  
+  // swiftlint:disable function_body_length large_tuple
+  public func animateTransition(using transitionContext: any UIViewControllerContextTransitioning) {
+    guard
+      let (fromViewController, fromView, fromIndex) = self.getTransitionInfo(
+        for: UITransitionContextViewControllerKey.from,
+        context: transitionContext
+      ),
+      let (_, toView, toIndex) = self.getTransitionInfo(
+        for: UITransitionContextViewControllerKey.to,
+        context: transitionContext
+      )
+    else {
+      transitionContext.completeTransition(false)
+      return
+    }
+    
+    let frame = transitionContext.initialFrame(for: fromViewController)
+    var toFrameStart = frame
+    let offsetX = frame.width * 0.1
+    toFrameStart.origin.x = toIndex > fromIndex
+      ? frame.origin.x + offsetX
+      : frame.origin.x - offsetX
+    toView.frame = toFrameStart
+    transitionContext.containerView.addSubview(toView)
+    transitionContext.containerView.addSubview(fromView)
+    
+    UIView.animate(
+      withDuration: self.transitionDuration,
+      delay: 0,
+      usingSpringWithDamping: 0.8,
+      initialSpringVelocity: 0.7,
+      options: [UIView.AnimationOptions.curveEaseInOut],
+      animations: {
+        fromView.layer.opacity = 0
+        toView.frame = frame
+        toView.layer.opacity = 1
+      },
+      completion: { success in
+        fromView.removeFromSuperview()
+        transitionContext.completeTransition(success)
+      }
+    )
+  }
+}
 
 extension PageTransition {
   private func getTransitionInfo(
@@ -31,6 +80,7 @@ extension PageTransition {
     
     return (viewController, view, index)
   }
+  // swiftlint:enable function_body_length large_tuple
   
   private func getIndex(for targetViewController: UIViewController) -> Int? {
     for (index, viewController) in self.viewControllers.enumerated() where viewController == targetViewController {
