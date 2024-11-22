f4f36a0ce447635bcd7dabb125fa64e6510ccc03
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-22 12:49:54 +0900

    #675: Supported platform 추가
    - 프로젝트 iOS 최소요구 버전사항에 맞게 supported platform을 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/RootTabBar/Package.swift
+++ ToDo-Garden/ToDo-Garden/RootTabBar/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "RootTabBar",
+  platforms: [SupportedPlatform.iOS(SupportedPlatform.IOSVersion.v15)],
   products: [
     Product.library(
       name: "RootTabBar",
