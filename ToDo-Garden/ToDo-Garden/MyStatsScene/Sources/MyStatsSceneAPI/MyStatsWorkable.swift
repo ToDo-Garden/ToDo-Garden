e2348a804a53cbe867cd2cc1eddef04682aa81fa
SONG <geun2121@gmail.com>
2024-11-25 18:17:00 +0900

    #690: MyStatsWorkable 명세작성

--- ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsSceneAPI/MyStatsWorkable.swift
+++ ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsSceneAPI/MyStatsWorkable.swift
@@ -5,8 +5,12 @@
 //  Created by SONG on 11/13/24.
 //  Copyright (c) 2024 ToDoGarden. All rights reserved.
 
-import Foundation
+import UIKit
+
+import MyStatsSceneEntity
 
 public protocol MyStatsWorkable {
-  func doSomeWork()
+  func fetchProfileViewData() async -> MyStats.FetchedProfileViewData
+  func fetchLongestRecordsViewData() async -> MyStats.FetchedLongestRecordViewData
+  func fetchSummaryViewData() async -> MyStats.FetchedSummaryViewData
 }
