c3edaf2cb003d4daf6e23a8af0f9107406a3aef0
SONG <geun2121@gmail.com>
2024-11-25 18:19:11 +0900

    #690: MyStatsWorker 메서드 정의
    - async로 동작하는 메서드를 정의합니다.

--- ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsScene/MyStatsWorker.swift
+++ ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsScene/MyStatsWorker.swift
@@ -8,7 +8,34 @@
 import Foundation
 
 import MyStatsSceneAPI
+import MyStatsSceneEntity
 
-struct MyStatsWorker: MyStatsWorkable {
-  func doSomeWork() { }
+public struct MyStatsWorker: MyStatsWorkable {
+  public init() {}
+  
+  public func fetchProfileViewData() async -> MyStats.FetchedProfileViewData {
+    return MyStats.FetchedProfileViewData(
+      continuousRecordCount: 99,
+      continuousRecordStartDate: Date.distantPast,
+      continuousRecordEndDate: Date.distantFuture
+    )
+  }
+  
+  public func fetchLongestRecordsViewData() async -> MyStats.FetchedLongestRecordViewData {
+    return MyStats.FetchedLongestRecordViewData(
+      concentratedRecordGroupName: "킹왕짱그룹",
+      concentratedRecordCount: 99,
+      concentratedRecordDate: Date.now,
+      longestContinuousRecordCount: 10,
+      longestContinuousRecordStartDate: Date.distantPast,
+      longestContinuousRecordEndDate: Date.distantFuture
+    )
+  }
+  
+  public func fetchSummaryViewData() async -> MyStats.FetchedSummaryViewData {
+    return MyStats.FetchedSummaryViewData(
+      concentratedTime: 3660,
+      completedCount: 3.5
+    )
+  }
 }
