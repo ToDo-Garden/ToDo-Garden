563d370cdf6bbb96a6e57cbd3c78603811588d83
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-28 00:18:49 +0900

    #675: RootTabBar 추가
    - RootTabBarController의 TabBar를 추가하였습니다.

--- ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBar.swift
+++ ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBar.swift
@@ -1,2 +1,45 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  RootTabBar.swift
+//  RootTabBar
+//
+//  Created by Noah on 11/27/24.
+//
+
+import UIKit
+
+extension RootTabBarController {
+  final class RootTabBar: UITabBar {
+    private let topSeparatorLineLayer: CALayer = {
+      let topSeparatorLineLayer = CALayer()
+      topSeparatorLineLayer.backgroundColor = UIColor.toDoGardenGreenBackground.cgColor
+      return topSeparatorLineLayer
+    }()
+    
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      self.setupTabBarAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+      super.layoutSubviews()
+      self.topSeparatorLineLayer.frame.size = CGSize(width: self.bounds.width, height: 1)
+      self.adjustTabBarItemImageInsets()
+    }
+    
+    private func adjustTabBarItemImageInsets() {
+      self.items?.forEach { item in
+        item.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -3, right: 0)
+      }
+    }
+    
+    private func setupTabBarAppearance() {
+      self.tintColor = UIColor.toDoGardenGreenDark
+      self.backgroundColor = UIColor.white
+      self.layer.addSublayer(self.topSeparatorLineLayer)
+    }
+  }
+}
---
+++ ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  RootTabBarController.swift
+//  RootTabBar
+//
+//  Created by Noah on 11/26/24.
+//
+
+import UIKit
+
+public final class RootTabBarController: UITabBarController {
+  
+}
