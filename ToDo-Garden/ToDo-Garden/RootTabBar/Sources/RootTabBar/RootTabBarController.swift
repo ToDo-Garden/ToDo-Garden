ac3b580a5b9f9499684d1085ea9120e96cff754c
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-28 15:37:04 +0900

    #675: 탭 구성 단위 지정
    - RootTabBar의 구성단위를 지정하였습니다.

--- ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBarController.swift
+++ ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBarController.swift
@@ -8,5 +8,24 @@
 import UIKit
 
 public final class RootTabBarController: UITabBarController {
+  private let tabItems: [RootTab]
+  
+  public init(tabItems: [RootTab]) {
+    self.tabItems = tabItems
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension RootTabBarController {
+  public enum RootTab {
+    case home(index: Int, viewController: UIViewController)
+    case share(index: Int, viewController: UIViewController)
+    case settings(index: Int, viewController: UIViewController)
+  }
   
 }
