0e9bb96c4de0a706efab784e0456c42263d63729
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-11-28 15:49:17 +0900

    #675: 선택된 탭 아이콘 애니메이션 설정
    - 사용자 경험을 높이기 위해 선택된 탭의 scale을 조정하는 애니메이션을 설정해주었습니다.

--- ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBarController.swift
+++ ToDo-Garden/ToDo-Garden/RootTabBar/Sources/RootTabBar/RootTabBarController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 public final class RootTabBarController: UITabBarController {
   private let rootTabBar = RootTabBar()
+  private let bounceAnimation: CAKeyframeAnimation = {
+    let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+    bounceAnimation.values = [1.0, 1.4, 0.9, 1.02, 1.0]
+    bounceAnimation.duration = TimeInterval(0.3)
+    bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
+    
+    return bounceAnimation
+  }()
   private let tabItems: [RootTab]
   
   public init(tabItems: [RootTab]) {
@@ -25,6 +33,15 @@ public final class RootTabBarController: UITabBarController {
     super.loadView()
     self.setValue(self.rootTabBar, forKey: "tabBar")
   }
+  
+  public override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+    if let index = tabBar.items?.firstIndex(of: item),
+      index + 1 <= tabBar.subviews.count {
+      let tabBarButtton = tabBar.subviews[index + 1]
+      let selectedImageView = self.getSelectedTabBarImageView(in: tabBarButtton)
+      selectedImageView?.layer.add(self.bounceAnimation, forKey: nil)
+    }
+  }
 }
 
 extension RootTabBarController {
@@ -34,4 +51,12 @@ extension RootTabBarController {
     case settings(index: Int, viewController: UIViewController)
   }
   
+  
+  private func getSelectedTabBarImageView(in selectedTabBarButton: UIView) -> UIImageView? {
+    let selectedImageView = selectedTabBarButton.subviews
+      .compactMap { $0.subviews.first?.subviews.first as? UIImageView }
+      .first
+    
+    return selectedImageView
+  }
 }
