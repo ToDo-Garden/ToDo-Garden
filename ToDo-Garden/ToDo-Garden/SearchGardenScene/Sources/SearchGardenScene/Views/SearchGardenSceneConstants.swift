86bbf27447a239c568fee93bf731a8c6f83754e5
SONG <geun2121@gmail.com>
2024-11-29 17:53:30 +0900

    #699: SearchGardenView 추가
    - mockData 추가
    - 상수 추가
    - View 추가

---
+++ ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenSceneConstants.swift
@@ -0,0 +1,15 @@
+//
+//  SearchGardenSceneConstants.swift
+//
+//
+//  Created by SONG on 11/26/24.
+//
+
+import Foundation
+
+enum Constant {
+  enum SearchGardenView {
+    public static let commonMargin: CGFloat = 18.0
+    public static let textfieldHeight: CGFloat = 32.0
+  }
+}
---
+++ ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenView.swift
@@ -0,0 +1,95 @@
+//
+//  SearchGardenView.swift
+//
+//
+//  Created by SONG on 11/25/24.
+//
+
+import UIKit
+
+import ToDoGardenUIComponent
+import ToDoGardenUIResource
+
+final class SearchGardenView: UIVStackView {
+  let textField: UITextField
+  let tableView: SearchGardenTableView
+  
+  init() {
+    self.textField = UITextField().applySearchGardenStyle()
+    self.tableView = SearchGardenTableView()
+    super.init(
+      spacing: Constant.SearchGardenView.commonMargin,
+      arrangedSubviews: []
+    )
+    self.isUserInteractionEnabled = true
+    self.setupSubViews()
+  }
+}
+
+extension SearchGardenView {
+  private func setupSubViews() {
+    self.setupTextField()
+    self.setupDivider()
+    self.setupTableView()
+  }
+  
+  private func setupTextField() {
+    self.addArrangedSubview(self.textField)
+    self.textField.usingAutolayout()
+    NSLayoutConstraint.activate(
+      [
+        self.textField.heightAnchor.constraint(equalToConstant: Constant.SearchGardenView.textfieldHeight),
+        self.textField.leadingAnchor.constraint(
+          equalTo: self.leadingAnchor,
+          constant: Constant.SearchGardenView.commonMargin
+        ),
+        self.textField.trailingAnchor.constraint(
+          equalTo: self.trailingAnchor,
+          constant: -Constant.SearchGardenView.commonMargin
+        )
+      ]
+    )
+  }
+  
+  private func setupDivider() {
+    let divider = UIView()
+    self.addArrangedSubview(divider)
+    divider.backgroundColor = UIColor.toDoGardenGreenGray
+    divider.usingAutolayout()
+    
+    NSLayoutConstraint.activate(
+      [
+        divider.heightAnchor.constraint(equalToConstant: 1.0),
+        divider.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+        divider.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+      ]
+    )
+  }
+  
+  private func setupTableView() {
+    self.addArrangedSubview(self.tableView)
+    self.tableView.usingAutolayout()
+
+    NSLayoutConstraint.activate(
+      [
+        self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+        self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+      ]
+    )
+  }
+}
+
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview {
+  let view = SearchGardenView()
+  
+  view.usingAutolayout()
+  view.widthAnchor.constraint(equalToConstant: 395).isActive = true
+  view.heightAnchor.constraint(equalToConstant: 650).isActive = true
+  
+  view.tableView.updateData(with: MockData.preview)
+  
+  return view
+}
+#endif
