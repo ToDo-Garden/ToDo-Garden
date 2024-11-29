cbb749293c1d8eb2a9586b62b7c7716a922da868
SONG <geun2121@gmail.com>
2024-11-29 18:04:40 +0900

    #699: 상수 추가

--- ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenSceneConstants.swift
+++ ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenSceneConstants.swift
@@ -11,5 +11,12 @@ enum Constant {
   enum SearchGardenView {
     public static let commonMargin: CGFloat = 18.0
     public static let textfieldHeight: CGFloat = 32.0
+    public static let cellHeight: CGFloat = 59.0
+  }
+  
+  enum NavigationBar {
+    public static let height: CGFloat = 75.0
+    public static let title = "가든찾기"
+    public static let rightButtonTitle = "완료"
   }
 }
