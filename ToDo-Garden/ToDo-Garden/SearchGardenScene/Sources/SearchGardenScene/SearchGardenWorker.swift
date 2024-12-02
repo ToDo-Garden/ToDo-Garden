5686b453998a7cf637070b8cdf4c80b321756270
SONG <geun2121@gmail.com>
2024-12-03 00:26:12 +0900

    #713: Worker를 에러처리 구조 구현
    - 워커에서 HTTPClientError를 던질 구조를 만듭니다.

--- ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/SearchGardenWorker.swift
+++ ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/SearchGardenWorker.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+import HTTPClientAPI
 import SearchGardenSceneAPI
 import SearchGardenSceneEntity
 import ToDoGardenUIComponent // TODO: 제거 예정
@@ -16,13 +17,19 @@ public struct SearchGardenWorker: SearchGardenWorkable {
   public init() {
   }
   
-  public func fetchUserDataForAddingGarden(userID: String) async -> SearchGarden.FetchedUserDataForAddingGarden {
+  public func fetchUserDataForAddingGarden(userID: String) async throws -> SearchGarden.FetchedUserDataForAddingGarden {
+    
     return SearchGarden.FetchedUserDataForAddingGarden(
       userNickname: "UserNickname",
       userIntroduction: "UserIntroduction",
       userGarden: PomodoroRecordCollection(),
       isFriend: false
     )
+    // HTTPCliendError 를 던질예정
   }
   
+  public func requestToAddGarden(userID: String) async throws -> SearchGarden.ResultForAddingGarden {
+    return SearchGarden.ResultForAddingGarden(isSuccess: true)
+    // HTTPCliendError 를 던질예정 
+  }
 }
