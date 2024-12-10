7d395f0396a59b9c3003fd4d67570212b7825f0e
SONG <geun2121@gmail.com>
2024-12-11 01:36:12 +0900

    #717: Mockdata수정

--- ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenMockData.swift
+++ ToDo-Garden/ToDo-Garden/SearchGardenScene/Sources/SearchGardenScene/Views/SearchGardenMockData.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by SONG on 11/26/24.
 //
@@ -11,21 +11,21 @@ import ToDoGardenUIComponent
 
 enum MockData {
   static let preview: [SearchGardenUser] = [
-    SearchGardenUser(userNickname: "사용자1", userID: "user1", userImage: nil),
-    SearchGardenUser(userNickname: "사용자2", userID: "user2", userImage: nil),
-    SearchGardenUser(userNickname: "사용자3", userID: "user3", userImage: nil),
-    SearchGardenUser(userNickname: "사용자4", userID: "user4", userImage: nil),
-    SearchGardenUser(userNickname: "사용자5", userID: "user5", userImage: nil),
-    SearchGardenUser(userNickname: "사용자6", userID: "user6", userImage: nil),
-    SearchGardenUser(userNickname: "사용자7", userID: "user7", userImage: nil),
-    SearchGardenUser(userNickname: "사용자8", userID: "user8", userImage: nil),
-    SearchGardenUser(userNickname: "사용자9", userID: "user9", userImage: nil),
-    SearchGardenUser(userNickname: "사용자10", userID: "user10", userImage: nil),
-    SearchGardenUser(userNickname: "사용자11", userID: "user11", userImage: nil),
-    SearchGardenUser(userNickname: "사용자12", userID: "user12", userImage: nil),
-    SearchGardenUser(userNickname: "사용자13", userID: "user13", userImage: nil),
-    SearchGardenUser(userNickname: "사용자14", userID: "user14", userImage: nil),
-    SearchGardenUser(userNickname: "사용자15", userID: "user15", userImage: nil),
-    SearchGardenUser(userNickname: "사용자16", userID: "user16", userImage: nil)
+    SearchGardenUser(userNickname: "개발자김", userID: "Dev_kim", userImage: nil),
+    SearchGardenUser(userNickname: "여행작가박", userID: "Traveler_park", userImage: nil),
+    SearchGardenUser(userNickname: "요리초보", userID: "Cook_beginner", userImage: nil),
+    SearchGardenUser(userNickname: "헬스마스터", userID: "Fitness_champ", userImage: nil),
+    SearchGardenUser(userNickname: "음악덕후", userID: "Music_lover", userImage: nil),
+    SearchGardenUser(userNickname: "그림쟁이", userID: "artist_drawing", userImage: nil),
+    SearchGardenUser(userNickname: "농구스타", userID: "BasketBall_Pro", userImage: nil),
+    SearchGardenUser(userNickname: "사진작가이", userID: "photo_master", userImage: nil),
+    SearchGardenUser(userNickname: "코딩천재", userID: "code_Genius", userImage: nil),
+    SearchGardenUser(userNickname: "여행댕댕", userID: "dog_travel", userImage: nil),
+    SearchGardenUser(userNickname: "재테크고수", userID: "finance_Expert", userImage: nil),
+    SearchGardenUser(userNickname: "베이킹왕", userID: "baking_queen", userImage: nil),
+    SearchGardenUser(userNickname: "영화마니아", userID: "MOVIE_FAN", userImage: nil),
+    SearchGardenUser(userNickname: "독서광", userID: "book_lover", userImage: nil),
+    SearchGardenUser(userNickname: "게임스트리머", userID: "game_streamer", userImage: nil),
+    SearchGardenUser(userNickname: "스타트업창업자", userID: "StartUp_founder", userImage: nil)
   ]
 }
