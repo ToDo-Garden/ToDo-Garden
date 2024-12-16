e5526283f663bb3be739f0beec86dfd83ad1dddf
gangwoon <gangwoon2018@gmail.com>
2024-12-16 23:09:12 +0900

    #733: Fix styled row

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Style/ToDoListStyle.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/Styled/Row/Style/ToDoListStyle.swift
@@ -2,14 +2,15 @@ import UIKit
 
 import ToDoGardenUIConstant
 
+// swiftlint:disable function_body_length
 extension Styled.Row {
   func buildTodoListStyle(stack: UIStackView, model: Configuration.TodoListModel) {
     self.buildStack(
       stack: stack,
       edgeInsets: Constant.Styled.Row.ToDoList.stackEdgeInsets
     )
-    let (zStack, zStackHandler) = self.buildZStack(model: model)
-    let checkBox = self.buildCheckBox(color: model.foregroundColor, zStackHandler)
+    let (zStack, checkBoxAction) = self.buildZStack(model: model)
+    let checkBox = self.buildCheckBox(color: model.foregroundColor, checkBoxAction)
     stack.addArrangedSubview(checkBox)
     self.setupCheckBoxConstraints(checkBox)
     
@@ -28,20 +29,31 @@ extension Styled.Row {
     let zStack = UIStackView(arrangedSubviews: [textField])
     let (selectedView, selecetdViewUpdateAction) = buildSelectedView(model: model)
     zStack.addArrangedSubview(selectedView)
+    /// selectedView를 누르게 되면 selectedView를 숨기고 textField를 노출시킵니다.
+    selectedView.addTapGesture { [weak selectedView, weak textField] in
+      selectedView?.isHidden = true
+      textField?.isHidden = false
+      _ = textField?.becomeFirstResponder()
+    }
+    /// textField가 resign될 경우, textField의 텍스트 여부에 따라서 textField를 숨길지, selectedView를 보여줄지 결정하게 됩니다.
+    textField.resignHandler = { [weak self, weak textField, weak selectedView] in
+      textField?.isHidden = !(textField?.text?.isEmpty ?? true)
+      selectedView?.isHidden = (textField?.text?.isEmpty ?? true)
+      selecetdViewUpdateAction(textField?.text, self?.configuration.todoListModel?.isSelected ?? true)
+    }
     
     return (
       zStack,
       { [weak textField, weak selectedView] isSelected in
-        textField?.isHidden = isSelected
-        selectedView?.isHidden = !isSelected
-        if isSelected {
-          selecetdViewUpdateAction(textField?.text)
-        }
+        let flag = textField?.text?.isEmpty ?? true
+        textField?.isHidden = flag ? isSelected : true
+        selectedView?.isHidden = flag ? !isSelected : false
+        selecetdViewUpdateAction(textField?.text, isSelected)
       }
     )
   }
   
-  private func buildTextField(text: String?, color: UIColor, isSelected: Bool) -> UITextField {
+  private func buildTextField(text: String?, color: UIColor, isSelected: Bool) -> Styled.TextField {
     let textField = Styled.TextField(configuration: .groupEdit(.todoList(mainColor: color)))
     let placeholderText = " 할 일을 입력해주세요."
     textField.attributedPlaceholder = NSAttributedString(
@@ -67,7 +79,7 @@ extension Styled.Row {
     textField.widthAnchor.constraint(equalToConstant: Constant.Styled.Row.ToDoList.textFieldWidth).isActive = true
   }
   
-  private func buildSelectedView(model: Configuration.TodoListModel) -> (UIView, (String?) -> Void) {
+  private func buildSelectedView(model: Configuration.TodoListModel) -> (UIView, (String?, Bool) -> Void) {
     let strikeThroughLabel = self.buildStrikethroughLabel(text: model.text)
     let imageView = self.buildAlarmImage(hasAlarm: model.hasAlert)
     let stack = UIHStackView(arrangedSubviews: [strikeThroughLabel, imageView])
@@ -76,7 +88,7 @@ extension Styled.Row {
     return (
       stack,
       { [weak strikeThroughLabel] in
-        strikeThroughLabel?.update($0)
+        strikeThroughLabel?.update(text: $0, animated: $1)
       }
     )
   }
@@ -131,5 +143,13 @@ extension Styled.Row {
 
 @available(iOS 17.0, *)
 #Preview {
-  Styled.Row(configuration: .todoList(.init(foregroundColor: .orange, hasAlert: true)))
+  let first = Styled.Row(configuration: .todoList(.init(foregroundColor: .orange, hasAlert: true)))
+  let second = Styled.Row(configuration: .todoList(.init(foregroundColor: .toDoGardenGreenDark, hasAlert: false)))
+  let third = Styled.Row(configuration: .todoList(.init(foregroundColor: .blue, hasAlert: false)))
+  let stack = UIVStackView(arrangedSubviews: [first, second, third])
+  first.widthAnchor.constraint(equalTo: stack.widthAnchor).isActive = true
+  second.widthAnchor.constraint(equalTo: stack.widthAnchor).isActive = true
+  third.widthAnchor.constraint(equalTo: stack.widthAnchor).isActive = true
+  return stack
 }
+// swiftlint:enable function_body_length
