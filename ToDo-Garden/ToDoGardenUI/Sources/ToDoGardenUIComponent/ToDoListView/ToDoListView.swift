e10c025c32adf554242117c532694a7387af046d
SeungHyun-Hong <dev.noah0316@gmail.com>
2024-12-19 13:59:30 +0900

    #718: ToDoListView UI 구현
    - UICollectionLayoutListConfiguration을 이용해 ToDoListView UI를 구현하였습니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoListView/ToDoListView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/ToDoListView/ToDoListView.swift
@@ -10,8 +10,95 @@ import UIKit
 // MARK: - Typealias
 
 extension ToDoListView {
+  public typealias Snapshot = NSDiffableDataSourceSnapshot<ToDoSection, ToDoListView.ToDoItem>
   public typealias ToDoUIModel = Styled.Row.Configuration.TodoListModel
+  private typealias DataSource = UICollectionViewDiffableDataSource<ToDoSection, ToDoItem>
+  private typealias ToDoCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, ToDoItem>
+  private typealias ToDoGroupHeaderRegistration = UICollectionView.SupplementaryRegistration<ToDoGroupSectionHeaderView>
 }
 
 public final class ToDoListView: UIView {
+  private lazy var contentView: UICollectionView = {
+    let toDoListView = UICollectionView(
+      frame: CGRect.zero,
+      collectionViewLayout: self.makeToDoListViewLayout()
+    )
+    toDoListView.backgroundColor = UIColor.white
+    
+    return toDoListView
+  }()
+  private lazy var dataSource: DataSource = self.makeDataSource()
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setup()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Setup
+
+extension ToDoListView {
+  private func setup() {
+    self.setupLayout()
+  }
+}
+
+// MARK: - DataSource
+
+extension ToDoListView {
+  private func makeDataSource() -> DataSource {
+    let toDoCellRegistration = ToDoCellRegistration { cell, _, toDoItem in
+      cell.contentConfiguration = ToDoContentViewContentConfiguration(model: toDoItem.toDoUIModel)
+      cell.indentationLevel = Int.zero
+    }
+    
+    let toDoGroupHeaderRegistration = ToDoGroupHeaderRegistration(
+      elementKind: UICollectionView.elementKindSectionHeader
+    ) { supplementaryView, _, indexPath in
+      let snapshot = self.dataSource.snapshot()
+      let section = snapshot.sectionIdentifiers[indexPath.section]
+      
+      supplementaryView.update(with: section.headerUIModel)
+    }
+    
+    let dataSource = DataSource(collectionView: self.contentView) { collectionView, indexPath, item in
+      return collectionView.dequeueConfiguredReusableCell(
+        using: toDoCellRegistration,
+        for: indexPath,
+        item: item
+      )
+    }
+    
+    dataSource.supplementaryViewProvider = { collectionView, _, indexPath in
+      return collectionView.dequeueConfiguredReusableSupplementary(using: toDoGroupHeaderRegistration, for: indexPath)
+    }
+    
+    return dataSource
+  }
+}
+
+// MARK: - layout
+
+extension ToDoListView {
+  private func setupLayout() {
+    self.backgroundColor = UIColor.white
+    self.addSubview(self.contentView)
+    self.contentView.equalToParent()
+  }
+  
+  private func makeToDoListViewLayout() -> UICollectionViewCompositionalLayout {
+    var listConfiguration = UICollectionLayoutListConfiguration(
+      appearance: UICollectionLayoutListConfiguration.Appearance.grouped
+    )
+    listConfiguration.showsSeparators = false
+    listConfiguration.backgroundColor = UIColor.white
+    listConfiguration.headerMode = UICollectionLayoutListConfiguration.HeaderMode.supplementary
+    
+    return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+  }
 }
