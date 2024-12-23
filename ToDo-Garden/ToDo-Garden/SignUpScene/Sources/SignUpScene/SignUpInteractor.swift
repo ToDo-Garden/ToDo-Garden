33fdd9376350cfc2f66e35e86bdadc14fadc88b3
SONG <geun2121@gmail.com>
2024-12-23 22:16:31 +0900

    #745: Interactor 수정
    - 유저등록 메서드 정의

--- ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/SignUpInteractor.swift
+++ ToDo-Garden/ToDo-Garden/SignUpScene/Sources/SignUpScene/SignUpInteractor.swift
@@ -14,17 +14,25 @@ protocol SignUpDataStore {
   var isEventAndPromotionalInformationAgreed: Bool { get }
 }
 
+@MainActor
 protocol SignUpBusinessLogic {
   func checkStringValidation(request: SignUp.CheckStringValidation.Request)
+  func registerUser(request: SignUp.RegisterUser.Request)
 }
 
 class SignUpInteractor: SignUpDataStore {
   var isEventAndPromotionalInformationAgreed: Bool = false
   var presenter: SignUpPresentationLogic?
   private let signUpWorker: SignUpWorkable
+  private var tasks: [TaskKey: Task<Void, Never>] = [:]
   
   init(signUpWorker: SignUpWorkable) {
     self.signUpWorker = signUpWorker
+  }
+  
+  func cancelTask(for key: TaskKey) {
+    self.tasks[key]?.cancel()
+    self.tasks[key] = nil
   }
 }
 
@@ -51,5 +59,33 @@ extension SignUpInteractor: SignUpBusinessLogic {
         self.presenter?.presentInvalid(response: response)
       }
     }
+  }
+  
+  func registerUser(request: SignUp.RegisterUser.Request) {
+    self.tasks[TaskKey.registerUser] = Task {
+      defer { self.tasks[TaskKey.registerUser] = nil }
+      
+      do {
+        try Task.checkCancellation()
+        let response = try await self.signUpWorker.registerUser(request: request)
+        try Task.checkCancellation()
+        self.presenter?.presentUserRegistrationSuccess(response: response)
+      } catch let error {
+        self.handleError(error: error)
+      }
+    }
+  }
+}
+
+extension SignUpInteractor {
+  enum TaskKey {
+    case registerUser
+    case isExistingCustomId
+  }
+  
+  @MainActor
+  private func handleError(error: Error) {
+    debugPrint("\(error) on SignUpInteractor")
+    self.presenter?.presentError(error: error)
   }
 }
