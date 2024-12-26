a769cb1701c185f81f516f792d2d3ec9d49f4123
SONG <geun2121@gmail.com>
2024-12-27 04:10:48 +0900

    #750: TermsTextViewContoller 추가

---
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/TermsTextView/TermsTextViewContoller.swift
@@ -0,0 +1,58 @@
+//
+//  TetmsTextViewContoller.swift
+//  ToDoGardenUI
+//
+//  Created by SONG on 12/27/24.
+//
+
+import UIKit
+
+public final class TermsTextViewContoller: UIViewController {
+  private let termsTextView: TermsTextView
+  
+  public init(title: String, text: String) {
+    self.termsTextView = TermsTextView(text: text)
+    super.init(nibName: nil, bundle: nil)
+    self.setupUI(title: title)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupUI(title: String) {
+    self.title = title
+    self.view.addSubview(self.termsTextView)
+    self.termsTextView.usingAutolayout()
+    NSLayoutConstraint.activate([
+      self.termsTextView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+      self.termsTextView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+      self.termsTextView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+      self.termsTextView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+    ])
+  }
+}
+
+// swiftlint:disable all
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview {
+  let viewController = TermsTextViewContoller(
+    title: "약관제목",
+    text:
+            """
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.
+          
+          Senectus et netus et malesuada. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Neque convallis a cras semper auctor. Libero id faucibus nisl tincidunt eget. Leo a diam sollicitudin tempor id. A lacus vestibulum sed arcu non odio euismod lacinia. In tellus integer feugiat scelerisque. Feugiat in fermentum posuere urna nec tincidunt praesent. Porttitor rhoncus dolor purus non enim praesent elementum facilisis. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Et malesuada fames ac turpis egestas sed. Sit amet nisl suscipit adipiscing bibendum est ultricies. Arcu ac tortor dignissim convallis aenean et tortor at. Pretium viverra suspendisse potenti nullam ac tortor vitae purus. Eros donec ac odio tempor orci dapibus ultrices. Elementum nibh tellus molestie nunc. Et magnis dis parturient montes nascetur. Est placerat in egestas erat imperdiet. Consequat interdum varius sit amet mattis vulputate enim.
+
+          Sit amet nulla facilisi morbi tempus. Nulla facilisi cras fermentum odio eu. Etiam erat velit scelerisque in dictum non consectetur a erat. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Ut sem nulla pharetra diam. Fames ac turpis egestas maecenas. Bibendum neque egestas congue quisque egestas diam. Laoreet id donec ultrices tincidunt arcu non sodales neque. Eget felis eget nunc lobortis mattis aliquam faucibus purus. Faucibus interdum posuere lorem ipsum dolor sit.
+      """
+  )
+  
+  let navi = UINavigationController(rootViewController: viewController)
+  
+  return navi
+}
+#endif
+// swiftlint:enable all
