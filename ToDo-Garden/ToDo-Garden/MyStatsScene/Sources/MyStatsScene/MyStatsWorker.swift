7fcecd17e0ae9a1b8a4be31dd9f318de5e416e23
SONG <geun2121@gmail.com>
2025-01-08 19:16:31 +0900

    #766: 워커 수정
    - 네트워크 모듈 추가
    - rest api 연동 구현

--- ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsScene/MyStatsWorker.swift
+++ ToDo-Garden/ToDo-Garden/MyStatsScene/Sources/MyStatsScene/MyStatsWorker.swift
@@ -3,49 +3,130 @@ import Foundation
 import HTTPClientAPI
 import MyStatsSceneAPI
 import MyStatsSceneEntity
+import TDFoundation
 
 public struct MyStatsWorker: MyStatsWorkable {
-  public init() {}
+  private let httpClient: HTTPClientAPI
+  
+  public init(httpClient: HTTPClientAPI) {
+    self.httpClient = httpClient
+  }
   
   public func fetchProfileViewData() async throws -> MyStats.FetchedProfileViewData {
-    do {
-      // TODO: RESTAPI 연동
-      
-      return MyStats.FetchedProfileViewData(
-        continuousRecordCount: 99,
-        continuousRecordStartDate: Date.distantPast,
-        continuousRecordEndDate: Date.distantFuture
-      )
-    } catch let error as HTTPClientError {
-      throw error
+    let result = try await self.downloadProfileData()
+    
+    var imageData: Data?
+    if let imageUrlString = result.imageUrl {
+      imageData = try await self.downloadImage(urlString: imageUrlString)
+    } else {
+      imageData = nil
     }
+    
+    return MyStats.FetchedProfileViewData(
+      nickname: result.nickname,
+      profileImage: imageData,
+      continuousRecordCount: result.continuousRecordCount,
+      continuousRecordStartDate: result.continuousRecordStartDate,
+      continuousRecordEndDate: result.continuousRecordEndDate
+    )
   }
   
   public func fetchLongestRecordsViewData() async throws -> MyStats.FetchedLongestRecordViewData {
-    do {
-      // TODO: RESTAPI 연동
-      return MyStats.FetchedLongestRecordViewData(
-        concentratedRecordGroupName: "킹왕짱그룹",
-        concentratedRecordCount: 99,
-        concentratedRecordDate: Date.now,
-        longestContinuousRecordCount: 10,
-        longestContinuousRecordStartDate: Date.distantPast,
-        longestContinuousRecordEndDate: Date.distantFuture
-      )
-    } catch let error as HTTPClientError {
-      throw error
-    }
+    let result = try await self.downloadLongestRecordsViewData()
+    return result
   }
   
   public func fetchSummaryViewData() async throws -> MyStats.FetchedSummaryViewData {
-    do {
-      // TODO: RESTAPI 연동
-      return MyStats.FetchedSummaryViewData(
-        concentratedTime: 3660,
-        completedCount: 3.5
-      )
-    } catch let error as HTTPClientError {
-      throw error
+    let result = try await self.downloadSummaryData()
+    return result
+  }
+}
+
+extension MyStatsWorker {
+  private func downloadProfileData() async throws -> MyStats.ProfileViewDataDTO {
+    let request = HTTPRequest(
+      method: .get,
+      endPoint: URLConstants.Stats.getCurrentContinuousDays
+    )
+    
+    let result = try await self.httpClient.send(
+      input: request,
+      serializer: { $0 },
+      deserializer: { response in
+        guard response.statusCode >= 200 && response.statusCode < 400 else {
+          throw HTTPClientError.badStatusCode(response.statusCode)
+        }
+        
+        guard let data = response.body else {
+          throw HTTPClientError.deserializationError
+        }
+        
+        return try JSONDecoder().decode(MyStats.ProfileViewDataDTO.self, from: data)
+      }
+    )
+    return result
+  }
+  
+  private func downloadLongestRecordsViewData() async throws -> MyStats.FetchedLongestRecordViewData {
+    let request = HTTPRequest(method: .get, endPoint: URLConstants.Stats.getMaxRecords)
+    let result = try await self.httpClient.send(
+      input: request,
+      serializer: { $0 },
+      deserializer: { response in
+        guard response.statusCode >= 200 && response.statusCode < 400 else {
+          throw HTTPClientError.badStatusCode(response.statusCode)
+        }
+        
+        guard let data = response.body else {
+          throw HTTPClientError.deserializationError
+        }
+        
+        return try JSONDecoder().decode(MyStats.FetchedLongestRecordViewData.self, from: data)
+      }
+    )
+    return result
+  }
+  
+  private func downloadSummaryData() async throws -> MyStats.FetchedSummaryViewData {
+    let request = HTTPRequest(method: .get, endPoint: URLConstants.Stats.getSummary)
+    let result = try await self.httpClient.send(
+      input: request,
+      serializer: { $0 },
+      deserializer: { response in
+        guard response.statusCode >= 200 && response.statusCode < 400 else {
+          throw HTTPClientError.badStatusCode(response.statusCode)
+        }
+        
+        guard let data = response.body else {
+          throw HTTPClientError.deserializationError
+        }
+        
+        return try JSONDecoder().decode(MyStats.FetchedSummaryViewData.self, from: data)
+      }
+    )
+    return result
+  }
+  
+  private func downloadImage(urlString: String) async throws -> Data {
+    guard let url = URL(string: urlString) else {
+      throw NSError(domain: "Failed to create URL from \(urlString)", code: 0)
     }
+    
+    let request = HTTPRequest(method: .get, endPoint: url)
+    let result = try await self.httpClient.send(
+      input: request,
+      serializer: { $0 },
+      deserializer: { response in
+        guard response.statusCode >= 200 && response.statusCode < 400 else {
+          throw HTTPClientError.badStatusCode(response.statusCode)
+        }
+        
+        guard let data = response.body else {
+          throw HTTPClientError.deserializationError
+        }
+        return data
+      }
+    )
+    return result
   }
 }
