b5a3bf998a672337049aa3cc00fb311f2015c90d
SONG <geun2121@gmail.com>
2025-01-17 17:33:25 +0900

    #781: 임시 이미지캐시 객체 추가
    - 임시로 쓸 이미지캐기 객체입니다.
    - 지워질 코드입니다.

--- ToDo-Garden/TDFoundation/Sources/TDFoundation/ScrollPrefetcher/ScrollPrefecher.swift
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/ScrollPrefetcher/ScrollPrefecher.swift
@@ -115,3 +115,66 @@ public final class ScrollPrefecher:
     return nil
   }
 }
+
+// TODO: ⬇️ 이미지캐시 객체 교체 예정
+// swiftlint: disable all
+public final class ImageCache {
+  public static let shared = ImageCache()
+  
+  private let cache = NSCache<NSURL, UIImage>()
+  
+  private init() {
+    self.cache.totalCostLimit = 50 * 1024 * 1024
+  }
+  
+  public func setObject(_ image: UIImage, forKey key: NSURL) {
+    self.cache.setObject(image, forKey: key)
+  }
+  
+  public func object(forKey key: NSURL) -> UIImage? {
+    return self.cache.object(forKey: key)
+  }
+  
+  public func removeObject(forKey key: NSURL) {
+    self.cache.removeObject(forKey: key)
+  }
+  
+  public func removeAllObjects() {
+    self.cache.removeAllObjects()
+  }
+  
+  public func loadImage(url: URL) async throws -> UIImage {
+    let cacheKey = url as NSURL
+    
+    if let cachedImage = ImageCache.shared.object(forKey: cacheKey) {
+      return cachedImage
+    }
+    
+    guard let accessToken = try KeychainManager.shared.load(forKey: KeychainManager.KeychainKey.accessToken),
+      let accessTokenString = String(data: accessToken, encoding: .utf8) else {
+      throw KeychainError.nonExistentKey
+    }
+    
+    let apiKey = try KeyConstants.supabaseAPIKey
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    request.setValue("\(apiKey)", forHTTPHeaderField: "apiKey")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("todogarden", forHTTPHeaderField: "Accept-Profile")
+    request.setValue("Bearer \(accessTokenString)", forHTTPHeaderField: "Authorization")
+    
+    do {
+      let (data, _) = try await URLSession.shared.data(for: request)
+      
+      if let downloadedImage = UIImage(data: data) {
+        ImageCache.shared.setObject(downloadedImage, forKey: cacheKey)
+        return downloadedImage
+      } else {
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+      }
+    } catch {
+      return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+  }
+}
+// swiftlint: enable all
