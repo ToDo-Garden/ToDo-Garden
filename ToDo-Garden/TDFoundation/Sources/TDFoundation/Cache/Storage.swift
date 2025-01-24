8ef928573871461a9f0b0512ca9a76d6430b3dae
gangwoon <gangwoon2018@gmail.com>
2025-01-25 00:16:31 +0900

    #758: Add key-based caching system with eviction handling

--- ToDo-Garden/TDFoundation/Sources/TDFoundation/Cache/Storage.swift
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/Cache/Storage.swift
@@ -1,33 +1,61 @@
 import Foundation
 
-public enum StorageExpiration: Sendable {
-  var estimatedExpirationSinceNow: Date {
-    switch self {
-    case .never: return .distantFuture
-    case .seconds(let seconds): return Date().addingTimeInterval(seconds)
-    case .expired: return .distantPast
+extension Cache {
+  final class Storage<Value: AnyObject>: NSObject, NSCacheDelegate {
+    let cache = NSCache<NSString, Value>()
+    var keys = Set<String>()
+    var removedContinuations: [String: [Continuation]] = [:]
+    
+    init(totoalCostLimit: Int) {
+      self.cache.totalCostLimit = totoalCostLimit
+      super.init()
+      self.cache.delegate = self
+    }
+    
+    func object(forKey key: String) -> Value? {
+      self.cache.object(forKey: key as NSString)
+    }
+    
+    func setObject(_ obj: Value, forKey key: String, cost: Int = 0) {
+      self.cache.setObject(obj, forKey: key as NSString, cost: cost)
+      self.keys.insert(key)
+    }
+    
+    func removeObject(forKey key: String) {
+      self.cache.removeObject(forKey: key as NSString)
+      self.keys.remove(key)
+    }
+    
+    func removeAllObjects() {
+      self.cache.removeAllObjects()
+      self.keys.removeAll()
+    }
+    
+    func cache(
+      _ cache: NSCache<AnyObject, AnyObject>,
+      willEvictObject obj: Any
+    ) {
+      guard
+        let box = obj as? Box<RequestState>,
+        let loadingState = box.loadingState
+      else { return }
+      self.removedContinuations[box.key] = loadingState.continuations
+      self.keys.remove(box.key)
     }
   }
-  
-  var isExpired: Bool {
-    timeInterval <= 0
-  }
-  
-  var timeInterval: TimeInterval {
-    switch self {
-    case .never: return .infinity
-    case .seconds(let seconds): return seconds
-    case .expired: return -.infinity
-    }
-  }
-  
-  case never
-  case seconds(TimeInterval)
-  case expired
 }
 
-public enum ExpirationExtending: Sendable {
-  case none
-  case cacheTime
-  case expirationTime(expiration: StorageExpiration)
+extension Cache.Storage: Sequence {
+  func makeIterator() -> AnyIterator<(String, Value)> {
+    var iterator = self.keys.makeIterator()
+    return AnyIterator {
+      while let key = iterator.next() {
+        if let value = self.cache.object(forKey: key as NSString) {
+          return (key, value)
+        }
+      }
+      return nil
+    }
+  }
 }
+
