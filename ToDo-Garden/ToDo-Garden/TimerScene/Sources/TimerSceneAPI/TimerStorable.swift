532b22f3543703b1c928347a4c4f8e70ecac0754
SONG <geun2121@gmail.com>
2025-01-27 04:31:21 +0900

    #786: TimerStorable 추가

---
+++ ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerSceneAPI/TimerStorable.swift
@@ -0,0 +1,14 @@
+//
+//  TimerStorable.swift
+//  TimerScene
+//
+//  Created by SONG on 1/27/25.
+//
+
+import TimerSceneEntity
+
+public protocol TimerStorable: Sendable {
+  func saveCompletedTimer(groupId: String, duration: Int) throws
+  func deleteAllPomodoros() throws
+  func getAsDTO() throws -> TimerScene.FocusTimeRequestDTO
+}
