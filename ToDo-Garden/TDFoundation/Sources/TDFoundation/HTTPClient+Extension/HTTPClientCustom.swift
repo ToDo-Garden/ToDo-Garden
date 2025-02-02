133084c124f1ae7e352d07ecf2484e3dace93994
SONG <geun2121@gmail.com>
2025-02-02 21:50:14 +0900

    #798: HTTPClientCustom 추가
    - HTTPClient 모듈을 사용하는 측에서 직접 import 하지 않기 위한 조치입니다. 사실상 HTTPClient 프로바이더 역할을 합니다.

---
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/HTTPClient+Extension/HTTPClientCustom.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPClientCustom.swift
+//  TDFoundation
+//
+//  Created by SONG on 2/1/25.
+//
+
+import HTTPClient
+import HTTPClientAPI
+
+public enum HTTPClientCustom {
+  public static let live: HTTPClientCustomable = HTTPClient.live
+}
