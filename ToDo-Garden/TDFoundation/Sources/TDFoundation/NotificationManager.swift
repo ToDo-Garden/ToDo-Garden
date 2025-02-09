e1eccb4064def00ddbc5e81ca72c97d07603e2e1
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-02-09 20:27:13 +0900

    #811: 알림 권한 fetch 메서드 추가
    - 현재 사용자가 설정한 알림 권한을 가져오는 메서드를 추가하였습니다.
    - 사용자가 아직 권한 설정을 하지 않은 경우에는, 알림 권한 선택 프롬프트를 띄웁니다.

--- ToDo-Garden/TDFoundation/Sources/TDFoundation/NotificationManager.swift
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/NotificationManager.swift
@@ -10,4 +10,26 @@ import UserNotifications
 
 public final class NotificationManager {
   public init() {}
+
+  /// 현재 사용자가 설정한 알림 설정 권한을 가져오는 메서드입니다.
+  /// 권한이 허용되어있으면 true, 거부되어 있으면 false를 반환합니다.
+  public func fetchPermission() async throws -> Bool {
+    let notificationCenter = UNUserNotificationCenter.current()
+    let settings = await notificationCenter.notificationSettings()
+    switch settings.authorizationStatus {
+    case .authorized:
+      return true
+    case .denied:
+      return false
+    case .notDetermined:
+      let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+      return try await notificationCenter.requestAuthorization(options: options)
+    default:
+      throw NotificationPermissionError.unknownError
+    }
+  }
+}
+
+public enum NotificationPermissionError: Error {
+  case unknownError
 }
