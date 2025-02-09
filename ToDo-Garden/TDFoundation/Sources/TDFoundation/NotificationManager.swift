5d2f0ca57373c21a54cab43a0ca5a7b4ca1a11c0
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-02-09 20:16:19 +0900

    #811: NotificationManager 객체 추가
    - TDFoundation에 알림 생성 및 권한을 가져오는 객체를 생성하였습니다.

---
+++ ToDo-Garden/TDFoundation/Sources/TDFoundation/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//
+//
+//  Created by Wood on 2/8/25.
+//
+
+import Foundation
+import UserNotifications
+
+public final class NotificationManager {
+  public init() {}
+}
