7fc256dc3de32a805e818f083d8568e4bcaf804c
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-02-22 22:25:08 +0900

    #824: 투두 상세조회 API 호출 로직 추가

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoSceneBuilder.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoSceneBuilder.swift
@@ -9,6 +9,7 @@ import Foundation
 
 import EditToDoSceneAPI
 
+@MainActor
 public struct EditToDoSceneBuilder {
   /// 컴파일 타임에 필요한 의존성을 선언한 구조체입니다.
   public struct Dependency {
--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoWorker.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoWorker.swift
@@ -1,6 +1,6 @@
 //
 //  EditToDoWorker.swift
-//  
+//
 //
 //  Created by Wood on 6/29/24.
 //  Copyright (c) 2024 ToDoGarden. All rights reserved.
@@ -9,21 +9,72 @@ import Foundation
 
 import EditToDoSceneAPI
 import EditToDoSceneEntity
+import HTTPClientAPI
+import TDFoundation
+import TDUtility
 
 public struct EditToDoWorker: EditToDoWorkable {
+  private let httpClient: HTTPClientAPI
+
+  public init(httpClient: HTTPClientAPI) {
+    self.httpClient = httpClient
+  }
+
+  // swiftlint: disable all
   public func fetchToDo(id: UUID) async throws -> EditToDo.ToDo {
-    <#code#>
+    let fetchedToDo = try await self.httpClient.send(
+      input: EditToDo.FetchToDo.RequestDTO(id: id),
+      serializer: { data in
+        let request = HTTPRequest(
+          method: .get,
+          endPoint: URLConstants.ToDo.fetchToDoDetail,
+          queryItems: ["todo_id": data.id.uuidString]
+        )
+        debugPrint(request)
+        return request
+      },
+      deserializer: { (response: HTTPResponse) in
+        guard response.statusCode >= 200 && response.statusCode < 400 else {
+          throw HTTPClientError.badStatusCode(response.statusCode)
+        }
+
+        guard let body = response.body else {
+          throw HTTPClientError.deserializationError
+        }
+
+        return try JSONDecoder().decode(EditToDo.FetchToDo.ResponseDTO.self, from: body)
+      }
+    )
+
+    return EditToDo.ToDo(
+      name: fetchedToDo.name,
+      groupData: EditToDo.Group(
+        id: fetchedToDo.group.id,
+        name: fetchedToDo.group.name,
+        color: fetchedToDo.group.color
+      ),
+      alarm: EditToDo.ToDoAlarm(
+        isAlarmOn: fetchedToDo.isAlarmOn,
+        alarmTime: fetchedToDo.alarmTime
+      ),
+      repetition: EditToDo.ToDoRepetition(
+        isOnlyToday: fetchedToDo.isOnlyToday,
+        startDate: fetchedToDo.startDate,
+        endDate: fetchedToDo.endDate
+      )
+    )
   }
-  
+
   public func fetchGroupList() async throws -> [EditToDo.Group] {
-    <#code#>
+    return []
   }
-  
+
   public func editToDo(id: UUID) async throws {
-    <#code#>
+
   }
 
   public func deleteToDo(id: UUID) async throws {
-    <#code#>
+
   }
 }
+// swiftlint: enable all
