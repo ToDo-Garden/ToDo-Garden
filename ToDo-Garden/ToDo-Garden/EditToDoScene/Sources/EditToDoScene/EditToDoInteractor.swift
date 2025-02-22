7eff19af9516057c5ff8158716828e0e749e98ae
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2025-02-22 22:26:30 +0900

    #824: Interactor 상세 조회 로직 수정

--- ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoInteractor.swift
+++ ToDo-Garden/ToDo-Garden/EditToDoScene/Sources/EditToDoScene/EditToDoInteractor.swift
@@ -10,10 +10,12 @@ import Foundation
 import EditToDoSceneAPI
 import EditToDoSceneEntity
 
+@MainActor
 protocol EditToDoDataStore {
-  var toDoId: UUID { get set }
+  var toDoId: UUID? { get set }
 }
 
+@MainActor
 protocol EditToDoBusinessLogic {
   func changeRepetition(isOnlyToday: Bool)
   func changeReptitionRange(request: EditToDo.ChangeRepetitionRange.Request)
@@ -25,8 +27,9 @@ protocol EditToDoBusinessLogic {
   func editToDo(request: EditToDo.CompleteEditToDo.Request)
 }
 
+@MainActor
 final class EditToDoInteractor: EditToDoDataStore {
-  var toDoId: UUID
+  var toDoId: UUID?
   var toDo: EditToDo.ToDo?
 
   // MARK: VIP Objects
@@ -41,7 +44,7 @@ final class EditToDoInteractor: EditToDoDataStore {
   }
 }
 
-// MARK: - Request to worker
+// MARK: - Non-REST API Logics
 
 extension EditToDoInteractor: EditToDoBusinessLogic {
   /// 사용자가 투두 알림 스위치를 통해 활성화 여부를 변경했을 때 호출되는 메서드입니다.
@@ -100,103 +103,118 @@ extension EditToDoInteractor: EditToDoBusinessLogic {
     let response = EditToDo.ChangeRepetitionRange.Response(startDate: startDate, endDate: endDate)
     self.presenter?.presentChangedRepetitionRange(response: response)
   }
+}
 
+// MARK: REST API Logics
+import HTTPClient
+import HTTPClientAPI
+
+extension EditToDoInteractor {
   /// 서버로부터 수정할 투두의 정보를 받아오는 메서드입니다.
   func fetchToDo() {
-    let fetchResult: Result<EditToDo.FetchToDo.Response.FetchedToDo, Error>
-    do {
-      guard let toDoId = self.toDoId
-      else { throw EditToDoInteractorError.toDoDataNotExisted }
-
-      let toDo = try self.toDoWorker.fetchToDo(id: toDoId)
-      self.toDo = toDo
-      let group = try self.groupWorker.fetchGroupList()
-      let repetitionViewState = self.makeRepetitionViewState(
-        isOnlyToday: toDo.repetition.isOnlyToday,
-        isEveryday: toDo.repetition.isRepeatEveryday
-      )
-
-      let fetchedToDo = EditToDo.FetchToDo.Response.FetchedToDo(
-        toDo: toDo,
-        groupList: group,
-        repetitionViewState: repetitionViewState
-      )
-
-      fetchResult = Result.success(fetchedToDo)
-    } catch let error {
-      fetchResult = Result.failure(error)
+    guard let id = self.toDoId else {
+      self.presenter?.presentError(.network)
+      return
     }
 
-    let response = EditToDo.FetchToDo.Response(fetchResult: fetchResult)
-    self.presenter?.presentFetchedToDo(response: response)
+    self.tasks[TaskKey.fetchToDo] = Task {
+      defer { self.tasks[TaskKey.fetchToDo] = nil }
+
+      do {
+        try Task.checkCancellation()
+        let toDo = try await self.editToDoWorker.fetchToDo(id: id)
+        try Task.checkCancellation()
+        self.toDo = toDo
+        let response = EditToDo.FetchToDo.Response(toDo: toDo)
+        self.presenter?.presentFetchedToDo(response: response)
+      } catch let error as HTTPClientError {
+        debugPrint(error.underlyingError)
+        debugPrint(error.localizedDescription)
+      } catch let error {
+        debugPrint(error.localizedDescription)
+        self.presenter?.presentError(.failToFetch)
+      }
+    }
+
+//    let fetchResult: Result<EditToDo.FetchToDo.Response.FetchedToDo, Error>
+//    do {
+//      guard let toDoId = self.toDoId
+//      else { throw EditToDoInteractorError.toDoDataNotExisted }
+//
+//      let toDo = try self.editToDoWorker.fetchToDo(id: toDoId)
+//      self.toDo = toDo
+//      let group = try self.groupWorker.fetchGroupList()
+//      let repetitionViewState = self.makeRepetitionViewState(
+//        isOnlyToday: toDo.repetition.isOnlyToday,
+//        isEveryday: toDo.repetition.isRepeatEveryday
+//      )
+//
+//      let fetchedToDo = EditToDo.FetchToDo.Response.FetchedToDo(
+//        toDo: toDo,
+//        groupList: group,
+//        repetitionViewState: repetitionViewState
+//      )
+//
+//      fetchResult = Result.success(fetchedToDo)
+//    } catch let error {
+//      fetchResult = Result.failure(error)
+//    }
+//
+//    let response = EditToDo.FetchToDo.Response(fetchResult: fetchResult)
+//    self.presenter?.presentFetchedToDo(response: response)
   }
-  
+
   /// 서버에 투두의 삭제를 요청하는 메서드입니다.
   func deleteToDo() {
-    let deleteResult: Result<Void, Error>
-    do {
-      guard let toDoId = self.toDoId
-      else { throw EditToDoInteractorError.toDoDataNotExisted }
-
-      try self.toDoWorker.deleteToDo(id: toDoId)
-      deleteResult = Result.success(())
-    } catch let error {
-      deleteResult = Result.failure(error)
-    }
-
-    let response = EditToDo.DeleteToDo.Response(deleteResult: deleteResult)
-    self.presenter?.presentDeleteResult(response: response)
+//    let deleteResult: Result<Void, Error>
+//    do {
+//      guard let toDoId = self.toDoId
+//      else { throw EditToDoInteractorError.toDoDataNotExisted }
+//
+//      try self.editToDoWorker.deleteToDo(id: toDoId)
+//      deleteResult = Result.success(())
+//    } catch let error {
+//      deleteResult = Result.failure(error)
+//    }
+//
+//    let response = EditToDo.DeleteToDo.Response(deleteResult: deleteResult)
+//    self.presenter?.presentDeleteResult(response: response)
   }
 
   /// 서버에 투두의 수정을 요청하는 메서드입니다.
   func editToDo(request: EditToDo.CompleteEditToDo.Request) {
-    let editResult: Result<Void, Error>
-    do {
-      let editedToDo = try self.makeToDoForEdit(with: request)
-      try self.toDoWorker.editToDo(editedToDo)
-      editResult = Result.success(())
-    } catch let error {
-      editResult = Result.failure(error)
-    }
-
-    let response = EditToDo.CompleteEditToDo.Response(editResult: editResult)
-    self.presenter?.presentEditResult(response: response)
+//    let editResult: Result<Void, Error>
+//    do {
+//      let editedToDo = try self.makeToDoForEdit(with: request)
+//      try self.editToDoWorker.editToDo(editedToDo)
+//      editResult = Result.success(())
+//    } catch let error {
+//      editResult = Result.failure(error)
+//    }
+//
+//    let response = EditToDo.CompleteEditToDo.Response(editResult: editResult)
+//    self.presenter?.presentEditResult(response: response)
   }
 }
 
 // MARK: Private Functions
 
 extension EditToDoInteractor {
-  private func makeRepetitionViewState(
-    isOnlyToday: Bool,
-    isEveryday: Bool?
-  ) -> EditToDo.EditToDoRepetitionViewState {
-    self.toDo?.repetition.isOnlyToday = isOnlyToday
-    if isOnlyToday {
-      return EditToDo.EditToDoRepetitionViewState.repeatOnlyToday
-    }
-
-    let isRepeatEveryday = isEveryday ?? (self.toDo?.repetition.isRepeatEveryday ?? true)
-    self.toDo?.repetition.isRepeatEveryday = isRepeatEveryday
-    let state: EditToDo.EditToDoRepetitionViewState = isRepeatEveryday ? .repeatEveryday : .repeatInRange
-    return state
-  }
-
-  private func makeToDoForEdit(
-    with request: EditToDo.CompleteEditToDo.Request
-  ) throws -> EditToDo.ToDo {
-    guard var editedToDo = self.toDo
-    else { throw EditToDoInteractorError.toDoDataNotExisted }
-
-    editedToDo.name = request.toDoName
-    editedToDo.groupData = EditToDo.Group(
-      id: request.displayedGroup.id,
-      name: request.displayedGroup.name,
-      color: request.displayedGroup.color
-    )
-
-    return editedToDo
-  }
+//  private func makeToDoForEdit(
+//    with request: EditToDo.CompleteEditToDo.Request
+//  ) throws -> EditToDo.ToDo {
+//    guard var editedToDo = self.toDo
+//    else { throw EditToDoInteractorError.toDoDataNotExisted }
+//
+//    editedToDo.name = request.toDoName
+//    editedToDo.groupData = EditToDo.Group(
+//      id: request.displayedGroup.id,
+//      name: request.displayedGroup.name,
+//      color: request.displayedGroup.color
+//    )
+//
+//    return editedToDo
+//  }
 }
 
 // MARK: Errors
