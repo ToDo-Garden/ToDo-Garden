d0e0fa2061188ddd860516e2c6594d77a17fe231
SONG <geun2121@gmail.com>
2025-03-13 12:09:14 +0900

    #856: 파일명 수정
    - TimerSceneScene -> TimerScene

--- ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneBuilder.swift
+++ ToDo-Garden/ToDo-Garden/TimerScene/Sources/TimerScene/TimerSceneBuilder.swift
@@ -4,7 +4,7 @@ import TDFoundation
 import TimerSceneAPI
 
 @MainActor
-public struct TimerSceneSceneBuilder {
+public struct TimerSceneBuilder {
   /// 컴파일 타임에 필요한 의존성을 선언한 구조체입니다.
   public struct Dependency {
     let timerWorker: TimerSceneWorkable
@@ -32,7 +32,7 @@ public struct TimerSceneSceneBuilder {
   }
 }
 
-extension TimerSceneSceneBuilder: TimerSceneSceneBuildable {
+extension TimerSceneBuilder: TimerSceneBuildable {
   ///  VIP Cycle, 런타임 의존성이 설정된 ViewController 인스턴스를 반환하는 함수입니다.
   /// - Parameter payload: 런타임에 전달받아야 하는 의존성입니다.
   /// - Returns: 런타임 의존성, VIP Cycle이 설정된 ViewController를 반환합니다.
@@ -43,7 +43,7 @@ extension TimerSceneSceneBuilder: TimerSceneSceneBuildable {
   }
 }
 
-extension TimerSceneSceneBuilder {
+extension TimerSceneBuilder {
   /// VIP Cycle을 설정합니다.
   /// - Parameter viewController: VIPCycle을 설정할 viewController입니다.
   /// - Returns: VIP Cycle 설정이 완료된 `ViewControllable` 프로토콜을 준수한 `ViewController` 인스턴스를 반환합니다.
