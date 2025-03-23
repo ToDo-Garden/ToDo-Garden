cd4c7e27176e4f652c4e8908aaa07b687b910df1
SONG <geun2121@gmail.com>
2025-03-23 18:31:47 +0900

    #888: 공용 함수 추가
    - 날짜 포맷 변경 메서드 추가합니다.

--- ToDo-Garden/TDUtility/Sources/TDFoundationExtension/String+Extension.swift
+++ ToDo-Garden/TDUtility/Sources/TDFoundationExtension/String+Extension.swift
@@ -21,4 +21,44 @@ public extension String {
     formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     return formatter.date(from: self) ?? Date()
   }
+  
+  // 2025-03-27T00:00:00+00:00 -> 20250327
+  func toYYYYMMDDStringFromISO8601() -> String {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime]
+    if let date = formatter.date(from: self) {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "yyyyMMdd"
+      return dateFormatter.string(from: date)
+    }
+    return self
+  }
+  
+  // 2025-03-22 14:22:40 +0000 -> 20250322
+  func toYYYYMMDDStringFromISO8601Space() -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+    if let date = formatter.date(from: self) {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "yyyyMMdd"
+      return dateFormatter.string(from: date)
+    }
+    return self
+  }
+  
+  // 2032년 3월 -> 20320301
+  func toYYYYMMDDStringFromYYYYMM() -> String {
+    let digits = self.compactMap { $0.isNumber ? $0 : nil }
+    let numericString = String(digits)
+    
+    if numericString.count == 5 {
+      let yearPart = numericString.prefix(4)
+      let monthPart = numericString.suffix(1)
+      return "\(yearPart)0\(monthPart)01"
+    } else if numericString.count == 6 {
+      return "\(numericString)01"
+    } else {
+      return self
+    }
+  }
 }
