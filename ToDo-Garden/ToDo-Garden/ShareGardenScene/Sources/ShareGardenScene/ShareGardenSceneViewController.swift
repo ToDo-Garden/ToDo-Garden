b052b755025ee1b52ff93c7de552082f94edef81
SeungHyun-Hong <dev.noah0316@gmail.com>
2025-03-25 21:53:26 +0900

    #896: MyGardenViewDelegate 채택

--- ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/ShareGardenSceneViewController.swift
+++ ToDo-Garden/ToDo-Garden/ShareGardenScene/Sources/ShareGardenScene/ShareGardenSceneViewController.swift
@@ -124,6 +124,7 @@ extension ShareGardenSceneViewController {
   private func setupActions() {
     self.setupMyGardenViewRetryAction()
     self.setupFriendsGardenViewRetryAction()
+    self.setupShareButtonAction()
   }
 }
 
@@ -144,6 +145,15 @@ extension ShareGardenSceneViewController {
       self?.friendsGardenView.showFriendsGardenListView()
       self?.interactor?.requestFriendsGardenList()
     }
+  }
+  
+  private func setupShareButtonAction() {
+    self.myGardenView.delegate = self
+  }
+}
+
+extension ShareGardenSceneViewController: MyGardenViewDelegate {
+  func shareButtonTapped() {
   }
 }
 
