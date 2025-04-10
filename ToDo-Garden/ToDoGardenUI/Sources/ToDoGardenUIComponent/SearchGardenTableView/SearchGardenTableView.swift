a36acf9ece48841f511a6c30e584a5e63c642928
SONG <geun2121@gmail.com>
2025-04-11 01:01:02 +0900

    #957:  placeholder 변경사항 반영
    - static 형태로 계속 살아있는 객체가 아닌, 인스턴스를 반환하는 메서드를 적용합니다.

--- ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SearchGardenTableView/SearchGardenTableView.swift
+++ ToDo-Garden/ToDoGardenUI/Sources/ToDoGardenUIComponent/SearchGardenTableView/SearchGardenTableView.swift
@@ -10,6 +10,7 @@ import UIKit
 final public class SearchGardenTableView: UITableView {
   private var diffableDataSource: UITableViewDiffableDataSource<SearchGardenSection, SearchGardenUser>!
   private var snapshot: NSDiffableDataSourceSnapshot<SearchGardenSection, SearchGardenUser>!
+  private var placeHolderData: SearchGardenUser? = SearchGardenUser.placeholderData()
 
   public override init(frame: CGRect, style: UITableView.Style) {
     super.init(frame: frame, style: style)
@@ -33,10 +34,18 @@ final public class SearchGardenTableView: UITableView {
     self.snapshot.appendItems(users)
     self.diffableDataSource.apply(self.snapshot, animatingDifferences: true)
   }
+  
   public func clearItemsInMainSection(completion: (() -> Void)? = nil) {
     self.snapshot.deleteItems(snapshot.itemIdentifiers(inSection: SearchGardenSection.main))
     self.diffableDataSource.apply(self.snapshot, animatingDifferences: false)
     completion?()
+  }
+  
+  public func cleanUpDeinit() {
+    self.placeHolderData = nil
+    self.snapshot = NSDiffableDataSourceSnapshot<SearchGardenSection, SearchGardenUser>()
+    self.snapshot.appendSections([.main])
+    self.diffableDataSource.apply(self.snapshot, animatingDifferences: false)
   }
   
   public func userForCell(at indexPath: IndexPath) -> SearchGardenUser? {
@@ -49,16 +58,18 @@ final public class SearchGardenTableView: UITableView {
   }
   
   public func appendPlaceholderCell() {
-    self.snapshot.appendItems([SearchGardenUser.placeholderData])
+    guard let placeHolderData = self.placeHolderData else { return }
+    self.snapshot.appendItems([placeHolderData])
     self.diffableDataSource.apply(self.snapshot, animatingDifferences: false)
   }
   
   public func deletePlaceholderCell() {
-    guard self.snapshot.itemIdentifiers.contains(SearchGardenUser.placeholderData) else {
+    guard let placeHolderData = self.placeHolderData else { return }
+    guard self.snapshot.itemIdentifiers.contains(placeHolderData) else {
       return
     }
     
-    self.snapshot.deleteItems([SearchGardenUser.placeholderData])
+    self.snapshot.deleteItems([placeHolderData])
     self.diffableDataSource.apply(self.snapshot, animatingDifferences: false)
   }
   
